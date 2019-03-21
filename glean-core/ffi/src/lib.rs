f100747502e45a827347f1066e8f3fb42b2a9606
Jan-Erik Rediger <janerik@fnordig.de>
2019-03-21 15:29:38 +0100

    Minimal Rust code & FFI & Kotlin glue

--- glean-core/ffi/src/lib.rs
+++ glean-core/ffi/src/lib.rs
@@ -1,7 +1,4 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+#[no_mangle]
+pub extern "C" fn increment() -> u64 {
+    glean_core::increment()
 }
