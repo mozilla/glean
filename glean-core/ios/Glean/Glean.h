389bef97984b61b8f05eba230b6264b7eee22264
Jan-Erik Rediger <janerik@fnordig.de>
2019-03-21 11:47:08 +0100

    Setup iOS project
    xcode -> New Project -> Cocoa Touch Framework

---
+++ glean-core/ios/Glean/Glean.h
@@ -0,0 +1,19 @@
+//
+//  Glean.h
+//  Glean
+//
+//  Created by Jan-Erik Rediger on 21.03.19.
+//  Copyright © 2019 Jan-Erik Rediger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Glean.
+FOUNDATION_EXPORT double GleanVersionNumber;
+
+//! Project version string for Glean.
+FOUNDATION_EXPORT const unsigned char GleanVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Glean/PublicHeader.h>
+
+
