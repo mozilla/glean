829e3b433e51ca57645c6cc0eb93fcd56c6f091a
Jan-Erik Rediger <janerik@fnordig.de>
2019-03-25 19:07:47 -0300

    Import minimal glean sample app from android-components

---
+++ samples/android/app/README.md
@@ -0,0 +1,9 @@
+# Glean.rs Sample Application
+
+A simple app showcasing Glean.rs, the mobile telemetry SDK.
+
+## License
+
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at http://mozilla.org/MPL/2.0/
---
+++ samples/android/app/src/main/java/org/mozilla/samples/glean/MainActivity.kt
@@ -0,0 +1,23 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.samples.glean_rs
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import mozilla.telemetry.glean.Glean
+
+open class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Generate an event when user clicks on the button.
+        buttonGenerateData.setOnClickListener {
+            Glean.increment()
+        }
+    }
+}
