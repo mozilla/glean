db42cd3734b09051f45dc89cfb5bd653421a955a
Jan-Erik Rediger <janerik@fnordig.de>
2019-03-25 19:07:47 -0300

    Import minimal glean sample app from android-components

---
+++ samples/android/app/src/main/java/org/mozilla/samples/glean/GleanApplication.kt
@@ -0,0 +1,19 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.samples.glean_rs
+
+import android.app.Application
+import mozilla.telemetry.glean.Glean
+
+class GleanApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        // Initialize the Glean library. Ideally, this is the first thing that
+        // must be done right after enabling logging.
+        Glean.initialize()
+    }
+}
