b5a24bca9001274e67f037c6e7980a4251f68660
Jan-Erik Rediger <janerik@fnordig.de>
2019-03-28 17:43:13 -0300

    Use the existing singleton

--- samples/ios/app/glean-sample-app/ViewController.swift
+++ samples/ios/app/glean-sample-app/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
 
-        let g = Glean()
+        let g = Glean.shared
         g.inc()
     }
 
