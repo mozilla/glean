8b6b8ee5466fa2373be1830b0387f20f2833a5e3
Jan-Erik Rediger <janerik@fnordig.de>
2019-03-28 15:26:09 -0300

    Add initial sample app

---
+++ samples/ios/app/glean-sample-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  glean-sample-app
+//
+//  Created by Jan-Erik Rediger on 28.03.19.
+//  Copyright © 2019 Jan-Erik Rediger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
