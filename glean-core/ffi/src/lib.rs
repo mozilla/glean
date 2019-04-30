317240d468f341b42830bb505e851bddb9677b8c
Jan-Erik Rediger <janerik@fnordig.de>
2019-04-30 14:27:09 +0200

    Format FFI component

--- glean-core/ffi/src/lib.rs
+++ glean-core/ffi/src/lib.rs
@@ -1,21 +1,12 @@
 use std::os::raw::c_char;
 
-use lazy_static::lazy_static;
 use ffi_support::{
-    IntoFfi,
-    FfiStr,
-    ConcurrentHandleMap,
-    ExternError,
-    define_handle_map_deleter,
-    define_string_destructor,
-    call_with_output,
+    call_with_output, define_handle_map_deleter, define_string_destructor, ConcurrentHandleMap,
+    ExternError, FfiStr, IntoFfi,
 };
+use lazy_static::lazy_static;
 
-use glean_core::{
-    Glean,
-    CommonMetricData,
-    metrics::*,
-};
+use glean_core::{metrics::*, CommonMetricData, Glean};
 
 lazy_static! {
     static ref BOOLEAN_METRICS: ConcurrentHandleMap<BooleanMetric> = ConcurrentHandleMap::new();
@@ -24,14 +15,14 @@ lazy_static! {
 }
 
 #[no_mangle]
-pub extern fn glean_initialize(data_dir: FfiStr) {
+pub extern "C" fn glean_initialize(data_dir: FfiStr) {
     #[cfg(target_os = "android")]
     {
         let _ = std::panic::catch_unwind(|| {
             android_logger::init_once(
                 android_logger::Filter::default().with_min_level(log::Level::Debug),
                 Some("libglean_ffi"),
-                );
+            );
             log::debug!("Android logging should be hooked up!")
         });
     }
@@ -42,59 +33,71 @@ pub extern fn glean_initialize(data_dir: FfiStr) {
 }
 
 #[no_mangle]
-pub extern fn glean_is_initialized() -> u8 {
+pub extern "C" fn glean_is_initialized() -> u8 {
     Glean::singleton().is_initialized().into_ffi_value()
 }
 
 #[no_mangle]
-pub extern fn glean_is_upload_enabled() -> u8 {
+pub extern "C" fn glean_is_upload_enabled() -> u8 {
     Glean::singleton().is_upload_enabled().into_ffi_value()
 }
 
 #[no_mangle]
-pub extern fn glean_set_upload_enabled(flag: u8) {
+pub extern "C" fn glean_set_upload_enabled(flag: u8) {
     let flag = flag != 0;
     Glean::singleton().set_upload_enabled(flag);
 }
 
 #[no_mangle]
-pub extern fn glean_new_boolean_metric(name: FfiStr, category: FfiStr, err: &mut ExternError) -> u64 {
+pub extern "C" fn glean_new_boolean_metric(
+    name: FfiStr,
+    category: FfiStr,
+    err: &mut ExternError,
+) -> u64 {
     BOOLEAN_METRICS.insert_with_output(err, || {
         BooleanMetric::new(CommonMetricData {
             name: name.into_string(),
             category: category.into_string(),
             send_in_pings: vec!["core".into()],
-            .. Default::default()
+            ..Default::default()
         })
     })
 }
 
 #[no_mangle]
-pub extern fn glean_new_string_metric(name: FfiStr, category: FfiStr, err: &mut ExternError) -> u64 {
+pub extern "C" fn glean_new_string_metric(
+    name: FfiStr,
+    category: FfiStr,
+    err: &mut ExternError,
+) -> u64 {
     STRING_METRICS.insert_with_output(err, || {
         StringMetric::new(CommonMetricData {
             name: name.into_string(),
             category: category.into_string(),
             send_in_pings: vec!["core".into()],
-            .. Default::default()
+            ..Default::default()
         })
     })
 }
 
 #[no_mangle]
-pub extern fn glean_new_counter_metric(name: FfiStr, category: FfiStr, err: &mut ExternError) -> u64 {
+pub extern "C" fn glean_new_counter_metric(
+    name: FfiStr,
+    category: FfiStr,
+    err: &mut ExternError,
+) -> u64 {
     COUNTER_METRICS.insert_with_output(err, || {
         CounterMetric::new(CommonMetricData {
             name: name.into_string(),
             category: category.into_string(),
             send_in_pings: vec!["core".into()],
-            .. Default::default()
+            ..Default::default()
         })
     })
 }
 
 #[no_mangle]
-pub extern fn glean_counter_add(metric_id: u64, amount: u64, error: &mut ExternError) {
+pub extern "C" fn glean_counter_add(metric_id: u64, amount: u64, error: &mut ExternError) {
     COUNTER_METRICS.call_with_output(error, metric_id, |metric| {
         metric.add(amount as u32);
         ()
@@ -102,7 +105,7 @@ pub extern fn glean_counter_add(metric_id: u64, amount: u64, error: &mut ExternE
 }
 
 #[no_mangle]
-pub extern fn glean_ping_collect(ping_name: FfiStr, error: &mut ExternError) -> *mut c_char {
+pub extern "C" fn glean_ping_collect(ping_name: FfiStr, error: &mut ExternError) -> *mut c_char {
     call_with_output(error, || {
         let ping_maker = glean_core::ping::PingMaker::new();
         let ping = ping_maker.collect_string(ping_name.as_str());
