5db8c10e0cbe2c047b9d2b89bc514e6200c39570
Jan-Erik Rediger <janerik@fnordig.de>
2019-05-02 18:23:53 +0200

    Re-factor all storage code into the Inner struct

--- glean-core/src/lib.rs
+++ glean-core/src/lib.rs
@@ -80,118 +80,29 @@ impl Glean {
         self.read().upload_enabled
     }
 
-    pub(crate) fn iter_store_from<F>(&self, lifetime: Lifetime, iter_start: &str, mut transaction_fn: F)
+    pub(crate) fn iter_store_from<F>(&self, lifetime: Lifetime, iter_start: &str, transaction_fn: F)
     where
         F: FnMut(&[u8], &Metric)
     {
-        let inner = self.read();
-        let len = iter_start.len();
-
-        // Lifetime::Application data is not persisted to disk
-        if lifetime == Lifetime::Application {
-            for (key, value) in &inner.app_data {
-                if key.starts_with(iter_start) {
-                    let key = &key[len..];
-                    transaction_fn(key.as_bytes(), value);
-                }
-
-            }
-
-            return;
-        }
-
-        let rkv = inner.rkv.as_ref().unwrap();
-        let store: SingleStore = rkv.open_single(lifetime.as_str(), StoreOptions::create()).unwrap();
-        let reader = rkv.read().unwrap();
-        let mut iter = store.iter_from(&reader, &iter_start).unwrap();
-
-        while let Some(Ok((metric_name, value))) = iter.next() {
-            if !metric_name.starts_with(iter_start.as_bytes()) {
-                break;
-            }
-
-            let metric_name = &metric_name[len..];
-            let metric: Metric = match value.unwrap() {
-                rkv::Value::Blob(blob) => bincode::deserialize(blob).unwrap(),
-                _ => continue,
-            };
-            transaction_fn(metric_name, &metric);
-        }
+        self.read().iter_store_from(lifetime, iter_start, transaction_fn)
     }
 
-    pub(crate) fn write_with_store<F>(&self, store_name: Lifetime, mut transaction_fn: F)
+    pub(crate) fn write_with_store<F>(&self, store_name: Lifetime, transaction_fn: F)
     where
         F: FnMut(rkv::Writer, SingleStore),
     {
-        if store_name == Lifetime::Application {
-            panic!("Can't write with store for application-lifetime data");
-        }
-
-        let inner = self.write();
-        let rkv = inner.rkv.as_ref().unwrap();
-        let store: SingleStore = rkv.open_single(store_name.as_str(), StoreOptions::create()).unwrap();
-        let writer = rkv.write().unwrap();
-        transaction_fn(writer, store);
+        self.write().write_with_store(store_name, transaction_fn)
     }
 
     pub(crate) fn record(&self, lifetime: Lifetime, ping_name: &str, key: &str, metric: &Metric) {
-        let inner = self.write();
-
-        let encoded = bincode::serialize(&metric).unwrap();
-        let value = rkv::Value::Blob(&encoded);
-
-        let final_key = format!("{}#{}", ping_name, key);
-        let store_name = lifetime.as_str();
-        let rkv = inner.rkv.as_ref().unwrap();
-        let store = rkv.open_single(store_name, StoreOptions::create()).unwrap();
-
-        let mut writer = rkv.write().unwrap();
-        store.put(&mut writer, final_key, &value).unwrap();
-        let _ = writer.commit();
+        self.write().record(lifetime, ping_name, key, metric)
     }
 
     pub(crate) fn record_with<F>(&self, lifetime: Lifetime, ping_name: &str, key: &str, transform: F)
     where
         F: Fn(Option<Metric>) -> Metric,
     {
-        let mut inner = self.write();
-        let final_key = format!("{}#{}", ping_name, key);
-
-        if lifetime == Lifetime::Application {
-            let entry = inner.app_data.entry(final_key);
-            match entry {
-                Entry::Vacant(entry) => {
-                    entry.insert(transform(None));
-                }
-                Entry::Occupied(mut entry) => {
-                    let old_value = entry.get();
-                    entry.insert(transform(Some(old_value.clone())));
-                }
-            }
-            return;
-        }
-
-        let store_name = lifetime.as_str();
-        let rkv = inner.rkv.as_ref().unwrap();
-        let store = rkv.open_single(store_name, StoreOptions::create()).unwrap();
-
-        let mut writer = rkv.write().unwrap();
-        let new_value: Metric = {
-            let old_value = store.get(&writer, &final_key).unwrap();
-
-            match old_value {
-                Some(rkv::Value::Blob(blob)) => {
-                    let old_value = bincode::deserialize(blob).ok();
-                    transform(old_value)
-                }
-                _ => transform(None),
-            }
-        };
-
-        let encoded = bincode::serialize(&new_value).unwrap();
-        let value = rkv::Value::Blob(&encoded);
-        store.put(&mut writer, final_key, &value).unwrap();
-        let _ = writer.commit();
+        self.write().record_with(lifetime, ping_name, key, transform)
     }
 }
 
@@ -240,5 +151,114 @@ impl Inner {
         };
         log::info!("Rkv done. We are initialized!");
         rkv
+    }
+
+    pub(crate) fn iter_store_from<F>(&self, lifetime: Lifetime, iter_start: &str, mut transaction_fn: F)
+    where
+        F: FnMut(&[u8], &Metric)
+    {
+        let len = iter_start.len();
+
+        // Lifetime::Application data is not persisted to disk
+        if lifetime == Lifetime::Application {
+            for (key, value) in &self.app_data {
+                if key.starts_with(iter_start) {
+                    let key = &key[len..];
+                    transaction_fn(key.as_bytes(), value);
+                }
+
+            }
+
+            return;
+        }
+
+        let rkv = self.rkv.as_ref().unwrap();
+        let store: SingleStore = rkv.open_single(lifetime.as_str(), StoreOptions::create()).unwrap();
+        let reader = rkv.read().unwrap();
+        let mut iter = store.iter_from(&reader, &iter_start).unwrap();
+
+        while let Some(Ok((metric_name, value))) = iter.next() {
+            if !metric_name.starts_with(iter_start.as_bytes()) {
+                break;
+            }
+
+            let metric_name = &metric_name[len..];
+            let metric: Metric = match value.unwrap() {
+                rkv::Value::Blob(blob) => bincode::deserialize(blob).unwrap(),
+                _ => continue,
+            };
+            transaction_fn(metric_name, &metric);
+        }
+    }
+
+    pub(crate) fn write_with_store<F>(&self, store_name: Lifetime, mut transaction_fn: F)
+    where
+        F: FnMut(rkv::Writer, SingleStore),
+    {
+        if store_name == Lifetime::Application {
+            panic!("Can't write with store for application-lifetime data");
+        }
+
+        let rkv = self.rkv.as_ref().unwrap();
+        let store: SingleStore = rkv.open_single(store_name.as_str(), StoreOptions::create()).unwrap();
+        let writer = rkv.write().unwrap();
+        transaction_fn(writer, store);
+    }
+
+    pub(crate) fn record(&self, lifetime: Lifetime, ping_name: &str, key: &str, metric: &Metric) {
+        let encoded = bincode::serialize(&metric).unwrap();
+        let value = rkv::Value::Blob(&encoded);
+
+        let final_key = format!("{}#{}", ping_name, key);
+        let store_name = lifetime.as_str();
+        let rkv = self.rkv.as_ref().unwrap();
+        let store = rkv.open_single(store_name, StoreOptions::create()).unwrap();
+
+        let mut writer = rkv.write().unwrap();
+        store.put(&mut writer, final_key, &value).unwrap();
+        let _ = writer.commit();
+    }
+
+    pub(crate) fn record_with<F>(&mut self, lifetime: Lifetime, ping_name: &str, key: &str, transform: F)
+    where
+        F: Fn(Option<Metric>) -> Metric,
+    {
+        let final_key = format!("{}#{}", ping_name, key);
+
+        if lifetime == Lifetime::Application {
+            let entry = self.app_data.entry(final_key);
+            match entry {
+                Entry::Vacant(entry) => {
+                    entry.insert(transform(None));
+                }
+                Entry::Occupied(mut entry) => {
+                    let old_value = entry.get();
+                    entry.insert(transform(Some(old_value.clone())));
+                }
+            }
+            return;
+        }
+
+        let store_name = lifetime.as_str();
+        let rkv = self.rkv.as_ref().unwrap();
+        let store = rkv.open_single(store_name, StoreOptions::create()).unwrap();
+
+        let mut writer = rkv.write().unwrap();
+        let new_value: Metric = {
+            let old_value = store.get(&writer, &final_key).unwrap();
+
+            match old_value {
+                Some(rkv::Value::Blob(blob)) => {
+                    let old_value = bincode::deserialize(blob).ok();
+                    transform(old_value)
+                }
+                _ => transform(None),
+            }
+        };
+
+        let encoded = bincode::serialize(&new_value).unwrap();
+        let value = rkv::Value::Blob(&encoded);
+        store.put(&mut writer, final_key, &value).unwrap();
+        let _ = writer.commit();
     }
 }
