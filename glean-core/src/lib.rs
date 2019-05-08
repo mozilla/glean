5f469cdea975e7854929d7b04aa159230d8f8d1e
Jan-Erik Rediger <janerik@fnordig.de>
2019-05-08 13:37:29 +0200

    Drop the inner module

--- glean-core/src/lib.rs
+++ glean-core/src/lib.rs
@@ -1,36 +1,47 @@
+use std::path::PathBuf;
+
 use uuid::Uuid;
 
 mod common_metric_data;
 mod database;
 mod error_recording;
 mod first_run;
-mod inner;
 mod internal_metrics;
 pub mod metrics;
 pub mod ping;
 pub mod storage;
 mod util;
 
-pub use common_metric_data::{CommonMetricData, Lifetime};
-pub use error_recording::ErrorType;
-use inner::Inner;
-use ping::PingMaker;
-use database::Database;
-use internal_metrics::CoreMetrics;
+pub use crate::common_metric_data::{CommonMetricData, Lifetime};
+pub use crate::error_recording::ErrorType;
+use crate::internal_metrics::CoreMetrics;
+use crate::storage::StorageManager;
+use crate::database::Database;
+use crate::util::sanitize_application_id;
+use crate::ping::PingMaker;
 
 const GLEAN_SCHEMA_VERSION: u32 = 1;
 
 #[derive(Debug)]
 pub struct Glean {
-    inner: Inner,
+    initialized: bool,
+    upload_enabled: bool,
+    data_store: Database,
     core_metrics: CoreMetrics,
+    data_path: Option<PathBuf>,
+    application_id: Option<String>,
 }
 
 impl Glean {
     pub fn new() -> Self {
+        log::info!("Creating new glean");
         Self {
-            inner: Inner::new(),
+            initialized: false,
+            upload_enabled: true,
+            data_store: Database::new(),
             core_metrics: CoreMetrics::new(),
+            data_path: None,
+            application_id: None,
         }
     }
 
@@ -39,13 +50,12 @@ impl Glean {
     /// This will create the necessary directories and files in `data_path`.
     /// This will also initialize the core metrics.
     pub fn initialize(&mut self, data_path: &str, application_id: &str) {
-        {
-            self.write().initialize(data_path, application_id);
-
-            // drop lock before we call any metric setters
-        }
-
+        self.data_store.initialize(data_path);
         self.initialize_core_metrics(data_path);
+
+        self.data_path = Some(PathBuf::from(data_path));
+        self.application_id = Some(sanitize_application_id(application_id));
+        self.initialized = true;
     }
 
     fn initialize_core_metrics(&mut self, data_path: &str) {
@@ -53,45 +63,48 @@ impl Glean {
         self.core_metrics.client_id.generate_if_missing(self.storage());
     }
 
-    fn read(&self) -> &Inner {
-        &self.inner
-    }
-
-    fn write(&mut self) -> &mut Inner {
-        &mut self.inner
-    }
-
     /// Determine whether the global Glean object is fully initialized yet.
     pub fn is_initialized(&self) -> bool {
-        self.read().is_initialized()
+        self.initialized
     }
 
     /// Set whether upload is enabled or not.
     ///
     /// When upload is disabled, no data will be recorded.
     pub fn set_upload_enabled(&mut self, flag: bool) {
-        self.write().set_upload_enabled(flag)
+        self.upload_enabled = flag;
     }
 
     /// Determine whether upload is enabled.
     ///
     /// When upload is disabled, no data will be recorded.
     pub fn is_upload_enabled(&self) -> bool {
-        self.read().is_upload_enabled()
+        self.upload_enabled
     }
 
+    pub fn get_application_id(&self) -> &str {
+        // TODO: Error handling?
+        self.application_id.as_ref().unwrap()
+    }
+
+    pub fn get_data_path(&self) -> &PathBuf {
+        // TODO: Error handling?
+        self.data_path.as_ref().unwrap()
+    }
+
+
     pub fn storage(&self) -> &Database {
-        self.read().storage()
+        &self.data_store
     }
 
     pub fn snapshot(&mut self, store_name: &str, clear_store: bool) -> String {
-        self.write().snapshot(store_name, clear_store)
+        StorageManager.snapshot(&self.storage(), store_name, clear_store)
     }
 
     fn make_path(&self, ping_name: &str, doc_id: &str) -> String {
         format!(
             "/submit/{}/{}/{}/{}",
-            self.read().get_application_id(),
+            self.get_application_id(),
             ping_name,
             GLEAN_SCHEMA_VERSION,
             doc_id
@@ -113,7 +126,7 @@ impl Glean {
         let ping_content = ::serde_json::to_string_pretty(&ping_maker.collect(self.storage(), ping_name)).unwrap();
         ping_maker.store_ping(
             &doc_id,
-            &self.read().get_data_path(),
+            &self.get_data_path(),
             &url_path,
             &ping_content,
         )
