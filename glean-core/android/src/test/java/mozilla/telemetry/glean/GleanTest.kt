66e39a524a20688c6016743c420ad2743cc15c6c
Michael Droettboom <mdboom@gmail.com>
2019-05-09 07:15:16 -0400

    Add simple Kotlin test (#17)
    * Add simple Kotlin test
    
    * Use android docker
    
    * Change robolectric version
    
    * Fix syntax
    
    * Fix android docker name
    
    * Fix docker tag
    
    * Disable android testing on Circle
    
    * Update .circleci/config.yml
    
    Co-Authored-By: Jan-Erik Rediger <badboy@archlinux.us>

---
+++ glean-core/android/src/test/java/mozilla/telemetry/glean/GleanTest.kt
@@ -0,0 +1,18 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+* License, v. 2.0. If a copy of the MPL was not distributed with this
+* file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package mozilla.telemetry.glean
+
+import androidx.test.core.app.ApplicationProvider
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+@RunWith(RobolectricTestRunner::class)
+class GleanTest {
+    @Test
+    fun `simple smoke test`() {
+        Glean.initialize(ApplicationProvider.getApplicationContext())
+    }
+}
