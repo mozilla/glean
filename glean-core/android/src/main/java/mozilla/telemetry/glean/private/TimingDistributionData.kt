a8ba298b733f2fc2149d3b5847521768ed528d4a
Alessio Placitelli <alessio.placitelli@gmail.com>
2019-05-27 16:28:46 +0200

    Stub out the TimingDistributionMetricType

---
+++ glean-core/android/src/main/java/mozilla/telemetry/glean/private/TimingDistributionData.kt
@@ -0,0 +1,201 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package mozilla.telemetry.glean.private
+
+import androidx.annotation.VisibleForTesting
+import org.json.JSONArray
+import org.json.JSONObject
+
+import mozilla.components.support.ktx.android.org.json.tryGetInt
+import mozilla.components.support.ktx.android.org.json.tryGetLong
+import mozilla.components.support.ktx.android.org.json.tryGetString
+
+/**
+ * This class represents the structure of a timing distribution according to the pipeline schema. It
+ * is meant to help serialize and deserialize data to the correct format for transport and storage,
+ * as well as including a helper function to calculate the bucket sizes.
+ *
+ * @param category of the metric
+ * @param name of the metric
+ * @param bucketCount total number of buckets
+ * @param range an array always containing 2 elements: the minimum and maximum bucket values
+ * @param histogramType the [HistogramType] representing the bucket layout
+ * @param values a map containing the bucket index mapped to the accumulated count
+ * @param sum the accumulated sum of all the samples in the timing distribution
+ * @param timeUnit the base [TimeUnit] of the bucket values
+ */
+@VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
+data class TimingDistributionData(
+    val category: String,
+    val name: String,
+    val bucketCount: Int = DEFAULT_BUCKET_COUNT,
+    val rangeMin: Long = DEFAULT_RANGE_MIN,
+    val rangeMax: Long = DEFAULT_RANGE_MAX,
+    val histogramType: HistogramType = HistogramType.Exponential,
+    val values: MutableMap<Int, Long> = mutableMapOf(),
+    var sum: Long = 0,
+    val timeUnit: TimeUnit = TimeUnit.Millisecond
+) {
+    companion object {
+        // The following are defaults for a simple timing distribution for the default time unit
+        // of millisecond.  The values arrived at were an approximated using existing "_MS"
+        // telemetry probes as a guide.
+        const val DEFAULT_BUCKET_COUNT = 100
+        const val DEFAULT_RANGE_MIN = 0L
+        const val DEFAULT_RANGE_MAX = 60000L
+
+        /**
+         * Factory function that takes stringified JSON and converts it back into a
+         * [TimingDistributionData].  This tries to read all values and attempts to
+         * use a default where no value exists.
+         *
+         * @param json Stringified JSON value representing a [TimingDistributionData] object
+         * @return A [TimingDistributionData] or null if unable to rebuild from the string.
+         */
+        @Suppress("ReturnCount", "ComplexMethod")
+        internal fun fromJsonString(json: String): TimingDistributionData? {
+            val jsonObject: JSONObject
+            try {
+                jsonObject = JSONObject(json)
+            } catch (e: org.json.JSONException) {
+                return null
+            }
+
+            // Category can be empty so it may be possible to be a null value so try and allow this
+            // by using `orEmpty()` to fill in the value.  Other values should be present or else
+            // something is wrong and we should return null.
+            val category = jsonObject.tryGetString("category").orEmpty()
+            val name = jsonObject.tryGetString("name") ?: return null
+            val bucketCount = jsonObject.tryGetInt("bucketCount") ?: return null
+            // If 'range' isn't present, JSONException is thrown
+            val range = try {
+                val array = jsonObject.getJSONArray("range")
+                // Range must have exactly 2 values
+                if (array.length() == 2) {
+                    // The getLong() function throws JSONException if we can't convert to a Long, so
+                    // the catch should return null if either value isn't a valid Long
+                    array.getLong(0)
+                    array.getLong(1)
+                    // This returns the JSONArray to the assignment if everything checks out
+                    array
+                } else {
+                    return null
+                }
+            } catch (e: org.json.JSONException) {
+                return null
+            }
+            val rawHistogramType = jsonObject.tryGetInt("histogramType") ?: return null
+            val histogramType = HistogramType.values().getOrNull(rawHistogramType) ?: return null
+            // Attempt to parse the values map, if it fails then something is wrong and we need to
+            // return null.
+            val values = try {
+                val mapData = jsonObject.getJSONObject("values")
+                val valueMap: MutableMap<Int, Long> = mutableMapOf()
+                mapData.keys().forEach { key ->
+                    valueMap[key.toInt()] = mapData.tryGetLong(key) ?: 0L
+                }
+                valueMap
+            } catch (e: org.json.JSONException) {
+                // This should only occur if there isn't a key/value pair stored for "values"
+                return null
+            }
+            val sum = jsonObject.tryGetLong("sum") ?: return null
+            val timeUnit = TimeUnit.values()[jsonObject.tryGetInt("timeUnit")
+                ?: return null]
+
+            return TimingDistributionData(
+                category = category,
+                name = name,
+                bucketCount = bucketCount,
+                rangeMin = range.getLong(0),
+                rangeMax = range.getLong(1),
+                histogramType = histogramType,
+                values = values,
+                sum = sum,
+                timeUnit = timeUnit
+            )
+        }
+    }
+
+    // This is a calculated read-only property that returns the total count of accumulated values
+    val count: Long
+        get() = values.map { it.value }.sum()
+
+    // This is a helper property to build the correct identifier for the metric and allow for
+    // blank categories
+    internal val identifier: String = if (category.isEmpty()) { name } else { "$category.$name" }
+
+    // This is a list of limits for the buckets.  Instantiated lazily to ensure that the range and
+    // bucket counts are set first.
+    internal val buckets: List<Long> by lazy { getBuckets() }
+
+    /**
+     * Accumulates a sample to the correct bucket, using a linear search to locate the index of the
+     * bucket where the sample is less than or equal to the bucket value.  This works since the
+     * buckets are sorted in ascending order.  If a mapped value doesn't exist for this bucket yet,
+     * one is created.
+     *
+     * @param sample Long value representing the sample that is being accumulated
+     */
+    internal fun accumulate(sample: Long) {
+        for (i in buckets.indices) {
+            if (sample <= buckets[i] || i == bucketCount - 1) {
+                values[i] = (values[i] ?: 0) + 1
+                break
+            }
+        }
+
+        sum += sample
+    }
+
+    /**
+     * Helper function to build the [TimingDistributionData] into a JSONObject for serialization
+     * purposes.
+     */
+    internal fun toJsonObject(): JSONObject {
+        return JSONObject(mapOf(
+            "category" to category,
+            "name" to name,
+            "bucketCount" to bucketCount,
+            "range" to JSONArray(arrayOf(rangeMin, rangeMax)),
+            "histogramType" to histogramType.ordinal,
+            "values" to values.mapKeys { "${it.key}" },
+            "sum" to sum,
+            "timeUnit" to timeUnit.ordinal
+        ))
+    }
+
+    /**
+     * Helper function to generate the list of bucket max values used when accumulating to the
+     * correct buckets.
+     *
+     * @return List containing the bucket limits
+     */
+    private fun getBuckets(): List<Long> {
+        // This algorithm calculates the bucket sizes using a natural log approach to get
+        // `bucketCount` number of buckets, exponentially spaced between `range[MIN]` and
+        // `range[MAX]`
+        val logMax = Math.log(rangeMax.toDouble())
+        val result: MutableList<Long> = mutableListOf()
+        var current = rangeMin
+        if (current == 0L) {
+            current = 1L
+        }
+        result.add(current)
+        for (i in 2..bucketCount) {
+            val logCurrent = Math.log(current.toDouble())
+            val logRatio = (logMax - logCurrent) / (bucketCount - i)
+            val logNext = logCurrent + logRatio
+            val nextValue = Math.round(Math.exp(logNext))
+            if (nextValue > current) {
+                current = nextValue
+            } else {
+                ++current
+            }
+            result.add(current)
+        }
+        return result.sorted()
+    }
+}
---
+++ glean-core/android/src/test/java/mozilla/telemetry/glean/private/TimingDistributionMetricTypeTest.kt
@@ -0,0 +1,161 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+* file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package mozilla.telemetry.glean.private
+
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.ObsoleteCoroutinesApi
+import mozilla.telemetry.glean.resetGlean
+// import mozilla.components.service.glean.timing.TimingManager
+import org.junit.After
+// import org.junit.Assert.assertEquals
+// import org.junit.Assert.assertTrue
+// import org.junit.Assert.assertFalse
+import org.junit.Before
+import org.junit.Ignore
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import java.lang.NullPointerException
+
+@ObsoleteCoroutinesApi
+@ExperimentalCoroutinesApi
+@RunWith(RobolectricTestRunner::class)
+class TimingDistributionMetricTypeTest {
+
+    @Before
+    fun setUp() {
+        resetGlean()
+    }
+
+    @After
+    fun reset() {
+        // TimingManager.testResetTimeSource()
+    }
+
+    @Ignore("TimingManager is not implemented yet. See bug 1554689")
+    @Test
+    fun `The API saves to its storage engine`() {
+        /*
+        // Define a timing distribution metric which will be stored in "store1"
+        val metric = TimingDistributionMetricType(
+            disabled = false,
+            category = "telemetry",
+            lifetime = Lifetime.Ping,
+            name = "timing_distribution",
+            sendInPings = listOf("store1"),
+            timeUnit = TimeUnit.Millisecond
+        )
+
+        // Accumulate a few values
+        for (i in 1L..3L) {
+            TimingManager.getElapsedNanos = { 0 }
+            metric.start(this)
+            TimingManager.getElapsedNanos = { i * 1000000 } // ms to ns
+            metric.stopAndAccumulate(this)
+        }
+
+        // Check that data was properly recorded.
+        assertTrue(metric.testHasValue())
+        val snapshot = metric.testGetValue()
+        // Check the sum
+        assertEquals(6L, snapshot.sum)
+        // Check that the 1L fell into the first bucket
+        assertEquals(1L, snapshot.values[0])
+        // Check that the 2L fell into the second bucket
+        assertEquals(1L, snapshot.values[1])
+        // Check that the 3L fell into the third bucket
+        assertEquals(1L, snapshot.values[2])
+        */
+    }
+
+    @Ignore("TimingManager is not implemented yet. See bug 1554689")
+    @Test
+    fun `disabled timing distributions must not record data`() {
+        /*
+        // Define a timing distribution metric which will be stored in "store1"
+        // It's lifetime is set to Lifetime.Ping so it should not record anything.
+        val metric = TimingDistributionMetricType(
+            disabled = true,
+            category = "telemetry",
+            lifetime = Lifetime.Ping,
+            name = "timing_distribution",
+            sendInPings = listOf("store1"),
+            timeUnit = TimeUnit.Millisecond
+        )
+
+        // Attempt to store the timespan using set
+        TimingManager.getElapsedNanos = { 0 }
+        metric.start(this)
+        TimingManager.getElapsedNanos = { 1 }
+        metric.stopAndAccumulate(this)
+
+        // Check that nothing was recorded.
+        assertFalse("TimingDistributions without a lifetime should not record data.",
+            metric.testHasValue())
+        */
+    }
+
+    @Test(expected = NullPointerException::class)
+    fun `testGetValue() throws NullPointerException if nothing is stored`() {
+        // Define a timing distribution metric which will be stored in "store1"
+        val metric = TimingDistributionMetricType(
+            disabled = false,
+            category = "telemetry",
+            lifetime = Lifetime.Ping,
+            name = "timing_distribution",
+            sendInPings = listOf("store1"),
+            timeUnit = TimeUnit.Millisecond
+        )
+        metric.testGetValue()
+    }
+
+    @Ignore("TimingManager is not implemented yet. See bug 1554689")
+    @Test
+    fun `The API saves to secondary pings`() {
+        /*
+        // Define a timing distribution metric which will be stored in multiple stores
+        val metric = TimingDistributionMetricType(
+            disabled = false,
+            category = "telemetry",
+            lifetime = Lifetime.Ping,
+            name = "timing_distribution",
+            sendInPings = listOf("store1", "store2", "store3"),
+            timeUnit = TimeUnit.Millisecond
+        )
+
+        // Accumulate a few values
+        for (i in 1L..3L) {
+            TimingManager.getElapsedNanos = { 0 }
+            metric.start(this)
+            TimingManager.getElapsedNanos = { i * 1000000 } // ms to ns
+            metric.stopAndAccumulate(this)
+        }
+
+        // Check that data was properly recorded in the second ping.
+        assertTrue(metric.testHasValue("store2"))
+        val snapshot = metric.testGetValue("store2")
+        // Check the sum
+        assertEquals(6L, snapshot.sum)
+        // Check that the 1L fell into the first bucket
+        assertEquals(1L, snapshot.values[0])
+        // Check that the 2L fell into the second bucket
+        assertEquals(1L, snapshot.values[1])
+        // Check that the 3L fell into the third bucket
+        assertEquals(1L, snapshot.values[2])
+
+        // Check that data was properly recorded in the third ping.
+        assertTrue(metric.testHasValue("store3"))
+        val snapshot2 = metric.testGetValue("store3")
+        // Check the sum
+        assertEquals(6L, snapshot2.sum)
+        // Check that the 1L fell into the first bucket
+        assertEquals(1L, snapshot2.values[0])
+        // Check that the 2L fell into the second bucket
+        assertEquals(1L, snapshot2.values[1])
+        // Check that the 3L fell into the third bucket
+        assertEquals(1L, snapshot2.values[2])
+        */
+    }
+}
