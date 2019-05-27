384dd88285501efe173da8ffdd2776fcce6959a1
Jan-Erik Rediger <jrediger@mozilla.com>
2019-05-27 12:09:15 +0200

    Test static labels

--- glean-core/tests/labeled.rs
+++ glean-core/tests/labeled.rs
@@ -55,7 +55,7 @@ fn can_use_multiple_labels() {
             disabled: false,
             lifetime: Lifetime::Ping,
         },
-        Some(vec!["label1".into(), "label2".into()]),
+        None,
     );
 
     let metric = labeled.get(&glean, "label1");
@@ -74,6 +74,50 @@ fn can_use_multiple_labels() {
                 "telemetry.labeled_metric": {
                     "label1": 1,
                     "label2": 2,
+                }
+            }
+        }),
+        snapshot
+    );
+}
+
+#[test]
+fn labels_are_checked_against_static_list() {
+    let (glean, _t) = new_glean();
+    let mut labeled: LabeledMetric<CounterMetric> = LabeledMetric::new(
+        CommonMetricData {
+            name: "labeled_metric".into(),
+            category: "telemetry".into(),
+            send_in_pings: vec!["store1".into()],
+            disabled: false,
+            lifetime: Lifetime::Ping,
+        },
+        Some(vec!["label1".into(), "label2".into()]),
+    );
+
+    let metric = labeled.get(&glean, "label1");
+    metric.add(&glean, 1);
+
+    let metric = labeled.get(&glean, "label2");
+    metric.add(&glean, 2);
+
+    // All non-registed labels get mapped to the `other` label
+    let metric = labeled.get(&glean, "label3");
+    metric.add(&glean, 3);
+    let metric = labeled.get(&glean, "label4");
+    metric.add(&glean, 4);
+
+    let snapshot = StorageManager
+        .snapshot_as_json(glean.storage(), "store1", true)
+        .unwrap();
+
+    assert_eq!(
+        json!({
+            "labeled_counter": {
+                "telemetry.labeled_metric": {
+                    "label1": 1,
+                    "label2": 2,
+                    "__other__": 7,
                 }
             }
         }),
