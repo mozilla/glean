798c575144c2f941f526eae6718720e3261410d2
Alessio Placitelli <alessio.placitelli@gmail.com>
2019-05-30 16:25:42 +0200

    Port the DatetimeStorageEngineTest tests to Rust

--- glean-core/tests/datetime.rs
+++ glean-core/tests/datetime.rs
@@ -12,7 +12,7 @@ use glean_core::metrics::*;
 use glean_core::storage::StorageManager;
 use glean_core::{CommonMetricData, Glean, Lifetime};
 
-// SKIPPED from glean-ac: string deserializer should correctly parse integers
+// SKIPPED from glean-ac: datetime deserializer should correctly parse integers
 // This test doesn't really apply to rkv
 
 #[test]
@@ -53,6 +53,110 @@ fn datetime_serializer_should_correctly_serialize_datetime() {
         assert_eq!(
             json!({"datetime": {"telemetry.datetime_metric": expected_value}}),
             snapshot
+        );
+    }
+}
+
+#[test]
+fn set_value_properly_sets_the_value_in_all_stores() {
+    let (glean, _t) = new_glean();
+    let store_names: Vec<String> = vec!["store1".into(), "store2".into()];
+
+    let metric = DatetimeMetric::new(CommonMetricData {
+        name: "datetime_metric".into(),
+        category: "telemetry".into(),
+        send_in_pings: store_names.clone(),
+        disabled: false,
+        lifetime: Lifetime::Ping,
+    }, TimeUnit::Nanosecond);
+
+    // `1983-04-13T12:09:14.274+00:00` will be truncated to Minute resolution.
+    let dt = FixedOffset::east(0).ymd(1983, 4, 13).and_hms_nano(12, 9, 14, 1560274);
+    metric.set(&glean, dt);
+
+    for store_name in store_names {
+        assert_eq!(
+            "1983-04-13T12:09:14.001560274+00:00",
+            metric.test_get_value_as_string(&glean, &store_name).unwrap()
+        );
+    }
+}
+
+// SKIPPED from glean-ac: getSnapshot() returns null if nothing is recorded in the store
+// This test doesn't really apply to rkv
+
+// SKIPPED from glean-ac: getSnapshot() correctly clears the stores
+// This test doesn't really apply to rkv
+
+#[test]
+fn test_that_truncation_works() {
+    let (glean, _t) = new_glean();
+
+    // `1985-07-03T12:09:14.000560274+01:00`
+    let high_res_datetime =
+        FixedOffset::east(3600).ymd(1985, 7, 3).and_hms_nano(12, 9, 14, 1560274);
+    let store_name = "store1";
+
+    // Create an helper struct for defining the truncation cases.
+    struct TestCase {
+        case_name: String,
+        desired_resolution: TimeUnit,
+        expected_result: String,
+    }
+
+    // Define the single test cases.
+    let test_cases = vec![
+        TestCase {
+            case_name: "nano".to_string(),
+            desired_resolution: TimeUnit::Nanosecond,
+            expected_result: "1985-07-03T12:09:14.001560274+01:00".to_string(),
+        },
+        TestCase {
+            case_name: "micro".to_string(),
+            desired_resolution: TimeUnit::Microsecond,
+            expected_result: "1985-07-03T12:09:14.001560+01:00".to_string(),
+        },
+        TestCase {
+            case_name: "milli".to_string(),
+            desired_resolution: TimeUnit::Millisecond,
+            expected_result: "1985-07-03T12:09:14.001+01:00".to_string(),
+        },
+        TestCase {
+            case_name: "second".to_string(),
+            desired_resolution: TimeUnit::Second,
+            expected_result: "1985-07-03T12:09:14+01:00".to_string(),
+        },
+        TestCase {
+            case_name: "minute".to_string(),
+            desired_resolution: TimeUnit::Minute,
+            expected_result: "1985-07-03T12:09+01:00".to_string(),
+        },
+        TestCase {
+            case_name: "hour".to_string(),
+            desired_resolution: TimeUnit::Hour,
+            expected_result: "1985-07-03T12+01:00".to_string(),
+        },
+        TestCase {
+            case_name: "day".to_string(),
+            desired_resolution: TimeUnit::Day,
+            expected_result: "1985-07-03+01:00".to_string(),
+        },
+    ];
+
+    // Execute them all.
+    for t in test_cases {
+        let metric = DatetimeMetric::new(CommonMetricData {
+            name: format!("datetime_metric_{}", t.case_name).into(),
+            category: "telemetry".into(),
+            send_in_pings: vec![store_name.into()],
+            disabled: false,
+            lifetime: Lifetime::User,
+        }, t.desired_resolution);
+        metric.set(&glean, high_res_datetime);
+
+        assert_eq!(
+            t.expected_result,
+            metric.test_get_value_as_string(&glean, &store_name).unwrap()
         );
     }
 }
