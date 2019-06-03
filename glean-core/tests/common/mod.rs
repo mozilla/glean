7470f7edba3daffea637d5084d134d09c26a3ba9
Michael Droettboom <mdboom@gmail.com>
2019-06-03 16:08:20 -0400

    1554275: Port PingMakerTests.kt to the rust side (#107)
    * 1554275: Port PingMakerTests.kt to the rust side
    
    * Address comments in the PR

--- glean-core/tests/common/mod.rs
+++ glean-core/tests/common/mod.rs
@@ -7,6 +7,11 @@
 #![allow(dead_code)]
 use glean_core::Glean;
 
+use chrono;
+use chrono::offset::TimeZone;
+use iso8601;
+use iso8601::Date::YMD;
+
 pub fn tempdir() -> (tempfile::TempDir, String) {
     let t = tempfile::tempdir().unwrap();
     let name = t.path().display().to_string();
@@ -24,4 +29,19 @@ pub fn new_glean() -> (Glean, tempfile::TempDir) {
     let glean = Glean::new(&tmpname, GLOBAL_APPLICATION_ID, true).unwrap();
 
     (glean, dir)
+}
+
+/// Convert an iso8601::DateTime to a chrono::DateTime<FixedOffset>
+pub fn iso8601_to_chrono(datetime: &iso8601::DateTime) -> chrono::DateTime<chrono::FixedOffset> {
+    if let YMD { year, month, day } = datetime.date {
+        return chrono::FixedOffset::east(datetime.time.tz_offset_hours * 3600)
+            .ymd(year, month, day)
+            .and_hms_milli(
+                datetime.time.hour,
+                datetime.time.minute,
+                datetime.time.second,
+                datetime.time.millisecond,
+            );
+    };
+    panic!("Unsupported datetime format");
 }
