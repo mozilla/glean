a9124833ab5f0f4cda3ee213439ef59721346337
Jan-Erik Rediger <jrediger@mozilla.com>
2019-06-11 14:16:26 +0200

    Fail the build if any documentation on public items is missing.

--- glean-core/src/lib.rs
+++ glean-core/src/lib.rs
@@ -2,6 +2,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+#![deny(missing_docs)]
 #![allow(clippy::new_without_default)]
 #![allow(clippy::redundant_closure)]
 
