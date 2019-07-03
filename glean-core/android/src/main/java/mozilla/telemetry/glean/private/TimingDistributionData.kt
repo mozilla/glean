f27b4858c67d11b0cc0213dab0e5d54383484f4c
Jan-Erik Rediger <jrediger@mozilla.com>
2019-07-03 17:00:56 +0200

    Implement timing distribution and re-enable tests (Kotlin)

--- glean-core/android/src/main/java/mozilla/telemetry/glean/private/TimingDistributionData.kt
+++ glean-core/android/src/main/java/mozilla/telemetry/glean/private/TimingDistributionData.kt
@@ -5,47 +5,31 @@
 package mozilla.telemetry.glean.private
 
 import androidx.annotation.VisibleForTesting
-import org.json.JSONArray
 import org.json.JSONObject
 
 import mozilla.components.support.ktx.android.org.json.tryGetInt
 import mozilla.components.support.ktx.android.org.json.tryGetLong
-import mozilla.components.support.ktx.android.org.json.tryGetString
 
 /**
  * This class represents the structure of a timing distribution according to the pipeline schema. It
  * is meant to help serialize and deserialize data to the correct format for transport and storage,
  * as well as including a helper function to calculate the bucket sizes.
  *
- * @param category of the metric
- * @param name of the metric
  * @param bucketCount total number of buckets
  * @param range an array always containing 2 elements: the minimum and maximum bucket values
  * @param histogramType the [HistogramType] representing the bucket layout
  * @param values a map containing the bucket index mapped to the accumulated count
  * @param sum the accumulated sum of all the samples in the timing distribution
- * @param timeUnit the base [TimeUnit] of the bucket values
  */
 @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
 data class TimingDistributionData(
-    val category: String,
-    val name: String,
-    val bucketCount: Int = DEFAULT_BUCKET_COUNT,
-    val rangeMin: Long = DEFAULT_RANGE_MIN,
-    val rangeMax: Long = DEFAULT_RANGE_MAX,
-    val histogramType: HistogramType = HistogramType.Exponential,
-    val values: MutableMap<Int, Long> = mutableMapOf(),
-    var sum: Long = 0,
-    val timeUnit: TimeUnit = TimeUnit.Millisecond
+    val bucketCount: Int,
+    val rangeMin: Long,
+    val rangeMax: Long,
+    val values: MutableMap<Int, Long>,
+    var sum: Long
 ) {
     companion object {
-        // The following are defaults for a simple timing distribution for the default time unit
-        // of millisecond.  The values arrived at were an approximated using existing "_MS"
-        // telemetry probes as a guide.
-        const val DEFAULT_BUCKET_COUNT = 100
-        const val DEFAULT_RANGE_MIN = 0L
-        const val DEFAULT_RANGE_MAX = 60000L
-
         /**
          * Factory function that takes stringified JSON and converts it back into a
          * [TimingDistributionData].  This tries to read all values and attempts to
@@ -66,9 +50,7 @@ data class TimingDistributionData(
             // Category can be empty so it may be possible to be a null value so try and allow this
             // by using `orEmpty()` to fill in the value.  Other values should be present or else
             // something is wrong and we should return null.
-            val category = jsonObject.tryGetString("category").orEmpty()
-            val name = jsonObject.tryGetString("name") ?: return null
-            val bucketCount = jsonObject.tryGetInt("bucketCount") ?: return null
+            val bucketCount = jsonObject.tryGetInt("bucket_count") ?: return null
             // If 'range' isn't present, JSONException is thrown
             val range = try {
                 val array = jsonObject.getJSONArray("range")
@@ -86,8 +68,6 @@ data class TimingDistributionData(
             } catch (e: org.json.JSONException) {
                 return null
             }
-            val rawHistogramType = jsonObject.tryGetInt("histogramType") ?: return null
-            val histogramType = HistogramType.values().getOrNull(rawHistogramType) ?: return null
             // Attempt to parse the values map, if it fails then something is wrong and we need to
             // return null.
             val values = try {
@@ -102,19 +82,13 @@ data class TimingDistributionData(
                 return null
             }
             val sum = jsonObject.tryGetLong("sum") ?: return null
-            val timeUnit = TimeUnit.values()[jsonObject.tryGetInt("timeUnit")
-                ?: return null]
 
             return TimingDistributionData(
-                category = category,
-                name = name,
                 bucketCount = bucketCount,
                 rangeMin = range.getLong(0),
                 rangeMax = range.getLong(1),
-                histogramType = histogramType,
                 values = values,
-                sum = sum,
-                timeUnit = timeUnit
+                sum = sum
             )
         }
     }
@@ -122,80 +96,4 @@ data class TimingDistributionData(
     // This is a calculated read-only property that returns the total count of accumulated values
     val count: Long
         get() = values.map { it.value }.sum()
-
-    // This is a helper property to build the correct identifier for the metric and allow for
-    // blank categories
-    internal val identifier: String = if (category.isEmpty()) { name } else { "$category.$name" }
-
-    // This is a list of limits for the buckets.  Instantiated lazily to ensure that the range and
-    // bucket counts are set first.
-    internal val buckets: List<Long> by lazy { getBuckets() }
-
-    /**
-     * Accumulates a sample to the correct bucket, using a linear search to locate the index of the
-     * bucket where the sample is less than or equal to the bucket value.  This works since the
-     * buckets are sorted in ascending order.  If a mapped value doesn't exist for this bucket yet,
-     * one is created.
-     *
-     * @param sample Long value representing the sample that is being accumulated
-     */
-    internal fun accumulate(sample: Long) {
-        for (i in buckets.indices) {
-            if (sample <= buckets[i] || i == bucketCount - 1) {
-                values[i] = (values[i] ?: 0) + 1
-                break
-            }
-        }
-
-        sum += sample
-    }
-
-    /**
-     * Helper function to build the [TimingDistributionData] into a JSONObject for serialization
-     * purposes.
-     */
-    internal fun toJsonObject(): JSONObject {
-        return JSONObject(mapOf(
-            "category" to category,
-            "name" to name,
-            "bucketCount" to bucketCount,
-            "range" to JSONArray(arrayOf(rangeMin, rangeMax)),
-            "histogramType" to histogramType.ordinal,
-            "values" to values.mapKeys { "${it.key}" },
-            "sum" to sum,
-            "timeUnit" to timeUnit.ordinal
-        ))
-    }
-
-    /**
-     * Helper function to generate the list of bucket max values used when accumulating to the
-     * correct buckets.
-     *
-     * @return List containing the bucket limits
-     */
-    private fun getBuckets(): List<Long> {
-        // This algorithm calculates the bucket sizes using a natural log approach to get
-        // `bucketCount` number of buckets, exponentially spaced between `range[MIN]` and
-        // `range[MAX]`
-        val logMax = Math.log(rangeMax.toDouble())
-        val result: MutableList<Long> = mutableListOf()
-        var current = rangeMin
-        if (current == 0L) {
-            current = 1L
-        }
-        result.add(current)
-        for (i in 2..bucketCount) {
-            val logCurrent = Math.log(current.toDouble())
-            val logRatio = (logMax - logCurrent) / (bucketCount - i)
-            val logNext = logCurrent + logRatio
-            val nextValue = Math.round(Math.exp(logNext))
-            if (nextValue > current) {
-                current = nextValue
-            } else {
-                ++current
-            }
-            result.add(current)
-        }
-        return result.sorted()
-    }
 }
