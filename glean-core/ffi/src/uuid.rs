b13a91bf2bbbbb8b598e45c2b467cd71ec61b3eb
Jan-Erik Rediger <jrediger@mozilla.com>
2019-07-11 14:31:17 +0200

    Define uuid via macro

--- glean-core/ffi/src/uuid.rs
+++ glean-core/ffi/src/uuid.rs
@@ -2,52 +2,17 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-use std::convert::TryFrom;
 use std::os::raw::c_char;
 
-use ffi_support::{define_handle_map_deleter, ConcurrentHandleMap, FfiStr};
-use lazy_static::lazy_static;
+use ffi_support::FfiStr;
 
-use glean_core::metrics::{MetricType, UuidMetric};
-use glean_core::{CommonMetricData, Lifetime};
+use crate::{define_metric, handlemap_ext::HandleMapExtension, GLEAN};
 
-use crate::handlemap_ext::HandleMapExtension;
-use crate::{from_raw_string_array, RawStringArray, GLEAN};
-
-lazy_static! {
-    static ref UUID_METRICS: ConcurrentHandleMap<UuidMetric> = ConcurrentHandleMap::new();
-}
-define_handle_map_deleter!(UUID_METRICS, glean_destroy_uuid_metric);
-
-#[no_mangle]
-pub extern "C" fn glean_new_uuid_metric(
-    category: FfiStr,
-    name: FfiStr,
-    send_in_pings: RawStringArray,
-    send_in_pings_len: i32,
-    lifetime: i32,
-    disabled: u8,
-) -> u64 {
-    UUID_METRICS.insert_with_log(|| {
-        let send_in_pings = unsafe { from_raw_string_array(send_in_pings, send_in_pings_len) };
-        let lifetime = Lifetime::try_from(lifetime)?;
-
-        Ok(UuidMetric::new(CommonMetricData {
-            name: name.into_string(),
-            category: category.into_string(),
-            send_in_pings,
-            lifetime,
-            disabled: disabled != 0,
-        }))
-    })
-}
-
-#[no_mangle]
-pub extern "C" fn glean_uuid_should_record(glean_handle: u64, metric_id: u64) -> u8 {
-    GLEAN.call_infallible(glean_handle, |glean| {
-        UUID_METRICS.call_infallible(metric_id, |metric| metric.should_record(&glean))
-    })
-}
+define_metric!(UuidMetric => UUID_METRICS {
+    new           -> glean_new_uuid_metric(),
+    destroy       -> glean_destroy_uuid_metric,
+    should_record -> glean_uuid_should_record,
+});
 
 #[no_mangle]
 pub extern "C" fn glean_uuid_set(glean_handle: u64, metric_id: u64, value: FfiStr) {
