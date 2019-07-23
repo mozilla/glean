4ed94254bd106384baeaefea47ed3d1ea1634fd9
Alessio Placitelli <alessio.placitelli@gmail.com>
2019-07-23 10:52:32 +0200

    Fix the package for HistogramBase.kt

--- glean-core/android/src/main/java/mozilla/telemetry/glean/private/HistogramBase.kt
+++ glean-core/android/src/main/java/mozilla/telemetry/glean/private/HistogramBase.kt
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
 * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-package mozilla.components.service.glean.private
+package mozilla.telemetry.glean.private
 
 /**
  * A common interface to be implemented by all the histogram-like metric types
