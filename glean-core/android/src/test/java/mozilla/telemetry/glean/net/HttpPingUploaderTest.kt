f2fbf12a6c943203692f4b7e90a46d02f8a88c48
Alessio Placitelli <alessio.placitelli@gmail.com>
2019-07-31 10:29:59 +0200

    Bonus: fix the package name for HttpPingUploaderTest.kt

--- glean-core/android/src/test/java/mozilla/telemetry/glean/net/HttpPingUploaderTest.kt
+++ glean-core/android/src/test/java/mozilla/telemetry/glean/net/HttpPingUploaderTest.kt
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-package mozilla.components.service.glean.net
+package mozilla.telemetry.glean.net
 
 import mozilla.components.concept.fetch.Client
 import mozilla.components.concept.fetch.Request
@@ -14,7 +14,6 @@ import mozilla.telemetry.glean.config.Configuration
 import mozilla.telemetry.glean.getMockWebServer
 import mozilla.components.support.test.any
 import mozilla.components.support.test.mock
-import mozilla.telemetry.glean.net.HttpPingUploader
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
