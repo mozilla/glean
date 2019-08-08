ee2facfd39e6f02d5b68df47047a531371a17032
Alessio Placitelli <alessio.placitelli@gmail.com>
2019-08-08 16:14:55 +0200

    Use ActivityScenario to launch GleanDebugActivityTest
    This further removes Robolectric from the tests

--- glean-core/android/src/test/java/mozilla/telemetry/glean/debug/GleanDebugActivityTest.kt
+++ glean-core/android/src/test/java/mozilla/telemetry/glean/debug/GleanDebugActivityTest.kt
@@ -12,9 +12,9 @@ import org.junit.Assert.assertFalse
 import org.junit.Assert.assertNull
 import org.junit.Assert.assertTrue
 import org.junit.Before
-import org.robolectric.Robolectric
 import android.content.pm.ActivityInfo
 import android.content.pm.ResolveInfo
+import androidx.test.core.app.ActivityScenario.launch
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import mozilla.telemetry.glean.private.BooleanMetricType
 import mozilla.telemetry.glean.private.Lifetime
@@ -62,8 +62,7 @@ class GleanDebugActivityTest {
         assertNull(intent.extras)
 
         // Start the activity through our intent.
-        val activity = Robolectric.buildActivity(GleanDebugActivity::class.java, intent)
-        activity.create().start().resume()
+        launch<GleanDebugActivity>(intent)
 
         // Verify that the original configuration and the one after init took place
         // are the same.
@@ -81,8 +80,7 @@ class GleanDebugActivityTest {
         val intent = Intent(ApplicationProvider.getApplicationContext<Context>(),
             GleanDebugActivity::class.java)
         intent.putExtra(GleanDebugActivity.LOG_PINGS_EXTRA_KEY, true)
-        val activity = Robolectric.buildActivity(GleanDebugActivity::class.java, intent)
-        activity.create().start().resume()
+        launch<GleanDebugActivity>(intent)
 
         // Check that the configuration option was correctly flipped.
         assertTrue(Glean.configuration.logPings)
@@ -96,12 +94,13 @@ class GleanDebugActivityTest {
         // Add at least an option, otherwise the activity will be removed.
         intent.putExtra(GleanDebugActivity.LOG_PINGS_EXTRA_KEY, true)
         // Start the activity through our intent.
-        val activity = Robolectric.buildActivity(GleanDebugActivity::class.java, intent)
-        activity.create().start().resume()
+        val scenario = launch<GleanDebugActivity>(intent)
 
         // Check that our main activity was launched.
-        assertEquals(testPackageName,
-            shadowOf(activity.get()).peekNextStartedActivityForResult().intent.`package`!!)
+        scenario.onActivity { activity ->
+            assertEquals(testPackageName,
+                shadowOf(activity).peekNextStartedActivityForResult().intent.`package`!!)
+        }
     }
 
     @Test
@@ -129,8 +128,7 @@ class GleanDebugActivityTest {
         val intent = Intent(ApplicationProvider.getApplicationContext<Context>(),
         GleanDebugActivity::class.java)
         intent.putExtra(GleanDebugActivity.SEND_PING_EXTRA_KEY, "metrics")
-        val activity = Robolectric.buildActivity(GleanDebugActivity::class.java, intent)
-        activity.create().start().resume()
+        launch<GleanDebugActivity>(intent)
 
         // Since we reset the serverEndpoint back to the default for untagged pings, we need to
         // override it here so that the local server we created to intercept the pings will
@@ -175,8 +173,7 @@ class GleanDebugActivityTest {
             GleanDebugActivity::class.java)
         intent.putExtra(GleanDebugActivity.SEND_PING_EXTRA_KEY, "metrics")
         intent.putExtra(GleanDebugActivity.TAG_DEBUG_VIEW_EXTRA_KEY, "inv@lid_id")
-        val activity = Robolectric.buildActivity(GleanDebugActivity::class.java, intent)
-        activity.create().start().resume()
+        launch<GleanDebugActivity>(intent)
 
         // Since a bad tag ID results in resetting the endpoint to the default, verify that
         // has happened.
@@ -231,8 +228,7 @@ class GleanDebugActivityTest {
             GleanDebugActivity::class.java)
         intent.putExtra(GleanDebugActivity.SEND_PING_EXTRA_KEY, "metrics")
         intent.putExtra(GleanDebugActivity.TAG_DEBUG_VIEW_EXTRA_KEY, pingTag)
-        val activity = Robolectric.buildActivity(GleanDebugActivity::class.java, intent)
-        activity.create().start().resume()
+        launch<GleanDebugActivity>(intent)
 
         // This will trigger the call to `fetch()` in the TestPingTagClient which is where the
         // test assertions will occur
