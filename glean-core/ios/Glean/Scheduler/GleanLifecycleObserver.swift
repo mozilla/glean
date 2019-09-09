d08ad05274cfedaa41ed8a7e7b31c7e3ea0d7e2a
Travis Long <tlong@mozilla.com>
2019-09-09 12:16:41 -0500

    1579886 - Add iOS lifecycle observer

---
+++ glean-core/ios/Glean/Scheduler/GleanLifecycleObserver.swift
@@ -0,0 +1,31 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+class GleanLifecycleObserver {
+    init() {
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(GleanLifecycleObserver.appWillEnterForeground(notification:)),
+            name: UIApplication.willEnterForegroundNotification,
+            object: nil
+        )
+
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(GleanLifecycleObserver.appDidEnterBackground(notification:)),
+            name: UIApplication.didEnterBackgroundNotification,
+            object: nil
+        )
+    }
+
+    @objc func appWillEnterForeground(notification: NSNotification) {
+        // TODO start duration
+    }
+
+    @objc func appDidEnterBackground(notification: NSNotification) {
+        // TODO stop duration
+
+        Glean.shared.handleBackgroundEvent()
+    }
+}
