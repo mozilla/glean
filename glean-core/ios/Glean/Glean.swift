3fcdc1ca7d4a7147dae58b64dcf9dcd8252416d4
Jan-Erik Rediger <jrediger@mozilla.com>
2019-09-11 11:39:28 +0200

    Initialize Glean with (mostly) default values

--- glean-core/ios/Glean/Glean.swift
+++ glean-core/ios/Glean/Glean.swift
@@ -12,12 +12,39 @@ public class Glean {
     public static let shared = Glean()
 
     private var initialized: Bool = false
+    private var handle: UInt64 = 0
+    private var uploadEnabled: Bool = true
+
     private init() {
-        initialized = true
+        // intentionally left blank
     }
 
     deinit {
+        self.handle = 0
         self.initialized = false
+    }
+
+    public func initialize(configuration _: Configuration) {
+        var cfg = FfiConfiguration(
+            data_dir: "/tmp",
+            package_name: "ios",
+            upload_enabled: uploadEnabled ? 1 : 0,
+            max_events: nil
+        )
+        handle = glean_initialize(&cfg)
+        initialized = true
+    }
+
+    public func setUploadEnabled(enabled: Bool) {
+        uploadEnabled = enabled
+
+        if isInitialized() {
+            glean_set_upload_enabled(handle, enabled ? 1 : 0)
+        }
+    }
+
+    internal func isInitialized() -> Bool {
+        return handle != 0
     }
 
     /// Handle background event and send appropriate pings
