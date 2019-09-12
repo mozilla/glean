9cc7c2302e1df0fef33ebaf70b450ed385a0f573
Alessio Placitelli <alessio.placitelli@gmail.com>
2019-09-12 19:05:43 +0200

    Make PingUploader a public interface with only `upload`

--- glean-core/android/src/main/java/mozilla/telemetry/glean/net/PingUploader.kt
+++ glean-core/android/src/main/java/mozilla/telemetry/glean/net/PingUploader.kt
@@ -4,122 +4,23 @@
 
 package mozilla.telemetry.glean.net
 
-import android.util.Log
-import mozilla.telemetry.glean.Glean
-import mozilla.telemetry.glean.config.Configuration
-import java.io.BufferedReader
-import java.io.File
-import java.io.FileNotFoundException
-import java.io.FileReader
-import java.io.IOException
-import java.text.SimpleDateFormat
-import java.util.Calendar
-import java.util.Locale
-import java.util.TimeZone
+typealias HeadersList = List<Pair<String, String>>
 
 /**
  * The interface defining how to send pings.
  */
-internal interface PingUploader {
-    companion object {
-        // Since ping file names are UUIDs, this matches UUIDs for filtering purposes
-        private const val FILE_PATTERN = "[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}"
-        private const val LOG_TAG = "glean/PingUploader"
-        internal const val PINGS_DIR = "pings"
-        // A lock to prevent simultaneous writes in the ping queue directory.
-        // In particular, there are issues if the pings are cleared (as part of
-        // disabling telemetry), while the ping uploader is trying to upload queued pings.
-        // Therefore, this lock is held both when uploading pings and when calling
-        // into the Rust code that might clear queued pings (set_upload_enabled).
-        internal val pingQueueLock = Any()
-    }
-
-    fun upload(path: String, data: String, config: Configuration): Boolean
-
-    fun createDateHeaderValue(): String {
-        val calendar = Calendar.getInstance()
-        val dateFormat = SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss z", Locale.US)
-        dateFormat.timeZone = TimeZone.getTimeZone("GMT")
-        return dateFormat.format(calendar.time)
-    }
-
+interface PingUploader {
     /**
-     * Function to deserialize and process all serialized ping files.  This function will ignore
-     * files that don't match the UUID regex and just delete them to prevent files from polluting
-     * the ping storage directory.
+     * Synchronously upload a ping to a server.
      *
-     * @param processingCallback Callback function to do the actual process action on the ping.
-     *                           Typically this will be the [HttpPingUploader.upload] function.
-     * @return Boolean representing the success of the upload task. This may be the value bubbled up
-     *         from the callback, or if there was an error reading the files.
+     * @param url the URL path to upload the data to
+     * @param data the serialized text data to send
+     * @param headers a [HeadersList] containing String to String [Pair] with
+     *        the first entry being the header name and the second its value.
+     *
+     * @return true if the ping was correctly dealt with (sent successfully
+     *         or faced an unrecoverable error), false if there was a recoverable
+     *         error callers can deal with.
      */
-    fun process(): Boolean {
-        // This function is from PingsStorageEngine in glean-ac
-
-        var success = true
-        // TODO: 1551694 Get this directory from the rust side
-        val storageDirectory = File(Glean.getDataDir(), PINGS_DIR)
-
-        Log.d(LOG_TAG, "Processing persisted pings at ${storageDirectory.absolutePath}")
-
-        synchronized(pingQueueLock) {
-            storageDirectory.listFiles()?.forEach { file ->
-                if (file.name.matches(Regex(FILE_PATTERN))) {
-                    Log.d(LOG_TAG, "Processing ping: ${file.name}")
-                    if (!processFile(file)) {
-                        Log.e(LOG_TAG, "Error processing ping file: ${file.name}")
-                        success = false
-                    }
-                } else {
-                    // Delete files that don't match the UUID FILE_PATTERN regex
-                    Log.d(LOG_TAG, "Pattern mismatch. Deleting ${file.name}")
-                    file.delete()
-                }
-            }
-        }
-
-        return success
-    }
-
-    /**
-     * This function encapsulates processing of a single ping file.
-     *
-     * @param file The [File] to process
-     * @param processingCallback the callback that actually processes the file
-     *
-     */
-    @Suppress("ReturnCount")
-    private fun processFile(
-        file: File
-    ): Boolean {
-        // This function is from PingsStorageEngine in glean-ac
-
-        var processed = false
-        BufferedReader(FileReader(file)).use {
-            try {
-                val path = it.readLine()
-                val serializedPing = it.readLine()
-
-                processed = serializedPing == null ||
-                    this.upload(path, serializedPing, Glean.configuration)
-            } catch (e: FileNotFoundException) {
-                // This shouldn't happen after we queried the directory.
-                Log.e(LOG_TAG, "Could not find ping file ${file.name}")
-                return false
-            } catch (e: IOException) {
-                // Something is not right.
-                Log.e(LOG_TAG, "IO Exception when reading file ${file.name}")
-                return false
-            }
-        }
-
-        return if (processed) {
-            val fileWasDeleted = file.delete()
-            Log.d(LOG_TAG, "${file.name} was deleted: $fileWasDeleted")
-            true
-        } else {
-            // The callback couldn't process this file.
-            false
-        }
-    }
+    fun upload(url: String, data: String, headers: HeadersList): Boolean
 }
