9e300f5223319b03e0dbbde53ccdf59317a94e82
Travis Long <tlong@mozilla.com>
2019-09-20 15:25:36 -0500

    Add MPL to top of codefiles

--- samples/ios/app/glean-sample-appUITests/glean_sample_appUITests.swift
+++ samples/ios/app/glean-sample-appUITests/glean_sample_appUITests.swift
@@ -1,10 +1,6 @@
-//
-//  glean_sample_appUITests.swift
-//  glean-sample-appUITests
-//
-//  Created by Jan-Erik Rediger on 28.03.19.
-//  Copyright © 2019 Jan-Erik Rediger. All rights reserved.
-//
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import XCTest
 
