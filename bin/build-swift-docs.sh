be96d4a5d769e3d1d31e35637ea6ff349da89937
Jan-Erik Rediger <jrediger@mozilla.com>
2019-09-27 13:32:39 +0200

    Add script to generate HTML-rendered Swift documentation
    [doc only]

---
+++ bin/build-swift-docs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Build Swift with one command
+# Requires jazzy from https://github.com/realm/jazzy
+
+jazzy --version
+jazzy \
+    --output build/docs/swift \
+    --sdk iphone \
+    --module Glean \
+    --xcodebuild-arguments -workspace,./glean-core/ios/Glean.xcodeproj/project.xcworkspace,-scheme,Glean \
+    --author_url https://mozilla.github.com/glean \
+    --github_url https://github.com/mozilla/glean
