dbda4f1150e441fe2deb46ab61c4e7a6de98e2de
Travis Long <tlong@mozilla.com>
2019-10-01 15:17:18 -0500

    1585341 - Add StringsStorageEngine to AC data migration

---
+++ glean-core/android/src/main/java/mozilla/telemetry/glean/acmigration/engines/StringsStorageEngine.kt
@@ -0,0 +1,56 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package mozilla.telemetry.glean.acmigration.engines
+
+import android.content.Context
+import mozilla.telemetry.glean.private.Lifetime
+import mozilla.telemetry.glean.private.StringMetricType
+
+internal class StringsStorageEngine(
+    applicationContext: Context
+) : GenericStorageEngine<String>() {
+
+    init {
+        this.applicationContext = applicationContext
+    }
+
+    override fun deserializeSingleMetric(metricName: String, value: Any?): String? {
+        return value as? String
+    }
+
+    /**
+     * Perform the data migration.
+     */
+    override fun migrateToGleanCore(lifetime: Lifetime) {
+        super.migrateToGleanCore(lifetime)
+
+        // Get the stored data.
+        val storedData = dataStores[lifetime.ordinal]
+        for ((storeName, data) in storedData) {
+            // Get each storage for the specified lifetime
+            for ((metricId, metricData) in data) {
+                // HACK HACK HACK HACK! Hic sunt leones!
+                // It would be tricky to break apart the category and the name of each metric,
+                // given that categories might contain dots themselves. Just leave the category
+                // blank and provide the full metric identifier through the "name".
+                val metric = StringMetricType(
+                    name = metricId,
+                    category = "",
+                    sendInPings = listOf(storeName),
+                    lifetime = lifetime,
+                    disabled = false
+                )
+
+                if (lifetime == Lifetime.User) {
+                    // User lifetime metrics are migrated very early: we don't want them
+                    // to be batched but, rather, set immediately.
+                    metric.setSync(metricData)
+                } else {
+                    metric.set(metricData)
+                }
+            }
+        }
+    }
+}
