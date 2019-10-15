75b93ca4f540416e7cac0b9a783b09b1197443b8
Beatriz Rizental <beatriz.rizental@gmail.com>
2019-10-15 10:52:26 +0200

    Add instrumented tests to sample app

---
+++ samples/android/app/src/androidTest/java/org/mozilla/samples/gleancore/MainActivityTest.kt
@@ -0,0 +1,50 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.samples.gleancore
+
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
+import mozilla.telemetry.glean.config.Configuration
+import mozilla.telemetry.glean.testing.GleanTestRule
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.mozilla.samples.gleancore.GleanMetrics.Test as GleanTestMetrics
+
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+    @get:Rule
+    val activityRule: ActivityTestRule<MainActivity> = ActivityTestRule(MainActivity::class.java)
+
+    @get:Rule
+    val gleanRule = GleanTestRule(
+        ApplicationProvider.getApplicationContext(),
+        Configuration(serverEndpoint = getPingServerAddress())
+    )
+
+    @Test
+    fun checkGleanClickData() {
+        // Simulate a click on the button.
+        onView(withId(R.id.buttonGenerateData)).perform(click())
+
+        // Use the Glean testing API to check if the expected data was recorded.
+        assertTrue(GleanTestMetrics.counter.testHasValue())
+        assertEquals(1, GleanTestMetrics.counter.testGetValue())
+    }
+
+    @Test
+    fun checkGleanClickDataAgain() {
+        // Repeat the same test as above: this will fail if the GleanTestRule fails to
+        // clear the Glean SDK state.
+        checkGleanClickData()
+    }
+}
--- samples/android/app/src/main/java/org/mozilla/samples/glean/GleanApplication.kt
+++ samples/android/app/src/main/java/org/mozilla/samples/glean/GleanApplication.kt
@@ -7,6 +7,10 @@ package org.mozilla.samples.gleancore
 import android.app.Application
 import android.util.Log
 import mozilla.telemetry.glean.Glean
+import org.mozilla.samples.gleancore.GleanMetrics.Basic
+import org.mozilla.samples.gleancore.GleanMetrics.Test
+import org.mozilla.samples.gleancore.GleanMetrics.Custom
+import org.mozilla.samples.gleancore.GleanMetrics.Pings
 
 private const val TAG = "Glean"
 
@@ -15,9 +19,20 @@ class GleanApplication : Application() {
     override fun onCreate() {
         super.onCreate()
 
+        // Register the sample application's custom pings.
+        Glean.registerPings(Pings)
+
         // Initialize the Glean library. Ideally, this is the first thing that
         // must be done right after enabling logging.
         Glean.initialize(applicationContext)
+
+        Test.timespan.start()
+
+        Custom.counter.add()
+
+        // Set a sample value for a metric.
+        Basic.os.set("Android")
+
         Log.i(TAG, "Glean initialized")
     }
 }
