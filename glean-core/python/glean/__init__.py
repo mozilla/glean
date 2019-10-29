40315f92f66ae710234f87b5d6824475c1b08b70
Michael Droettboom <mdboom@gmail.com>
2019-10-29 15:38:56 -0400

    Fix __version__

--- glean-core/python/glean/__init__.py
+++ glean-core/python/glean/__init__.py
@@ -7,7 +7,7 @@
 from pkg_resources import get_distribution, DistributionNotFound
 
 try:
-    __version__ = get_distribution(__name__).version
+    __version__ = get_distribution("glean-sdk").version
 except DistributionNotFound:
     # package is not installed
     pass
