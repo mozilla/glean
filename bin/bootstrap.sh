7ee2f8e7c55f136dd1f795e409c3bb213dbae841
Jan-Erik Rediger <jrediger@mozilla.com>
2019-11-07 11:50:55 +0100

    Use a private Cartfile to not expose the test-only dependency

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -4,8 +4,4 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-if [ ! -e 'Cartfile' ]; then
-  ln -s tests-only-Cartfile Cartfile
-fi
-
 carthage bootstrap --platform iOS --color auto --cache-builds
