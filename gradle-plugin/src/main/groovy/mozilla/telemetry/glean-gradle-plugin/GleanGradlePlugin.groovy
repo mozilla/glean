ff9e73b764f3b5640491a61fcb85cc5e3dac64f9
Michael Droettboom <mdboom@gmail.com>
2019-11-11 10:16:32 -0500

    20.2.0 release (#465)

--- gradle-plugin/src/main/groovy/mozilla/telemetry/glean-gradle-plugin/GleanGradlePlugin.groovy
+++ gradle-plugin/src/main/groovy/mozilla/telemetry/glean-gradle-plugin/GleanGradlePlugin.groovy
@@ -367,7 +367,7 @@ subprocess.check_call([
     }
 
     void apply(Project project) {
-        project.ext.glean_version = "20.1.0"
+        project.ext.glean_version = "20.2.0"
 
         File condaDir = setupPythonEnvironmentTasks(project)
         project.ext.set("gleanCondaDir", condaDir)
