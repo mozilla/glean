5ec5b8e956bf555fd47cf24d39a13fd2548e78d7
Michael Droettboom <mdboom@gmail.com>
2019-11-13 11:21:40 -0500

    1595486: Make app_id and app_version required (#469)
    * 1595486: Make app_id and app_version required
    
    * glean-python -> glean-python-test

--- glean-core/python/glean/_dispatcher.py
+++ glean-core/python/glean/_dispatcher.py
@@ -7,23 +7,31 @@ import functools
 from typing import Callable, List, Tuple
 
 
-class DispatcherInternal:
+class Dispatcher:
     # This value was chosen in order to allow several tasks to be queued for
     # execution but still be conservative of memory. This queue size is
     # important for cases where setUploadEnabled(false) is not called so that
     # we don't continue to queue tasks and waste memory.
     MAX_QUEUE_SIZE = 100
 
-    def __init__(self):
-        """
-        Dispatcher to manage work that might be set up before
-        `glean.Glean.initialize` is called, but needs to actually be performed
-        afterward.
-        """
-        self._queue_initial_tasks: bool = True
-        self._task_queue: List[Tuple[Callable, tuple, dict]] = []
+    # When True, tasks will be queued for running later, otherwise, they
+    # are run immediately
+    _queue_initial_tasks: bool = True
 
-    def task(self, func: Callable):
+    # The task queue
+    _task_queue: List[Tuple[Callable, tuple, dict]] = []
+
+    @classmethod
+    def reset(cls):
+        """
+        Reset the dispatcher so the queue is cleared, and it is reset into
+        queueing mode.
+        """
+        cls._queue_initial_tasks = True
+        cls._task_queue = []
+
+    @classmethod
+    def task(cls, func: Callable):
         """
         A decorator for coroutines that might either run in the task queue or
         immediately.
@@ -35,16 +43,17 @@ class DispatcherInternal:
 
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
-            if self._queue_initial_tasks:
-                if len(self._task_queue) >= self.MAX_QUEUE_SIZE:
+            if cls._queue_initial_tasks:
+                if len(cls._task_queue) >= cls.MAX_QUEUE_SIZE:
                     return
-                self._task_queue.append((func, args, kwargs))
+                cls._task_queue.append((func, args, kwargs))
             else:
                 func(*args, **kwargs)
 
         return wrapper
 
-    def launch(self, func: Callable):
+    @classmethod
+    def launch(cls, func: Callable):
         """
         Either queue the function for running later, or run immediately,
         depending on the state of `_queue_initial_tasks`.
@@ -68,26 +77,28 @@ class DispatcherInternal:
             Dispatcher.launch(my_task)
         """
 
-        if self._queue_initial_tasks:
-            if len(self._task_queue) >= self.MAX_QUEUE_SIZE:
+        if cls._queue_initial_tasks:
+            if len(cls._task_queue) >= cls.MAX_QUEUE_SIZE:
                 return
-            self._task_queue.append((func, (), {}))
+            cls._task_queue.append((func, (), {}))
         else:
             func()
 
-    def launch_at_front(self, func: Callable):
+    @classmethod
+    def launch_at_front(cls, func: Callable):
         """
         Either queue the function for running later (before all other queued
         tasks), or run immediately, depending on the state of
         `_queue_initial_tasks`.
         """
 
-        if self._queue_initial_tasks:
-            self._task_queue.insert(0, (func, (), {}))
+        if cls._queue_initial_tasks:
+            cls._task_queue.insert(0, (func, (), {}))
         else:
             func()
 
-    def set_task_queueing(self, enabled: bool):
+    @classmethod
+    def set_task_queueing(cls, enabled: bool):
         """
         Enable queueing mode, which causes tasks to be queued until launched by
         calling `DispatcherInternal.flushQueuedInitialTasks`.
@@ -95,16 +106,14 @@ class DispatcherInternal:
         Args:
             enabled (bool): Whether or not to queue tasks.
         """
-        self._queue_initial_tasks = enabled
+        cls._queue_initial_tasks = enabled
 
-    def flush_queued_initial_tasks(self):
+    @classmethod
+    def flush_queued_initial_tasks(cls):
         """
         Stops queueing tasks and processes any tasks in the queue.
         """
-        self.set_task_queueing(False)
-        for (task, args, kwargs) in self._task_queue:
+        cls.set_task_queueing(False)
+        for (task, args, kwargs) in cls._task_queue:
             task(*args, **kwargs)
-        self._task_queue.clear()
-
-
-Dispatcher = DispatcherInternal()
+        cls._task_queue.clear()
