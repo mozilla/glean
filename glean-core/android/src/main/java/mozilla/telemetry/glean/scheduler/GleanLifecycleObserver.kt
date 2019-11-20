711e02385d09bf52f6c090aa4300ec5ac4c4556a
Michael Droettboom <mdboom@gmail.com>
2019-11-20 12:26:24 -0500

    Use the LifecycleEventObserver API (#501)
    * Use the LifecycleEventObserver API
    
    This doesn't require annotations or any Java8 magic, so hopefully will work.
    
    * Add CHANGELOG

--- glean-core/android/src/main/java/mozilla/telemetry/glean/scheduler/GleanLifecycleObserver.kt
+++ glean-core/android/src/main/java/mozilla/telemetry/glean/scheduler/GleanLifecycleObserver.kt
@@ -4,7 +4,8 @@
 
 package mozilla.telemetry.glean.scheduler
 
-import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.LifecycleOwner
 import mozilla.telemetry.glean.Glean
 import mozilla.telemetry.glean.GleanMetrics.GleanBaseline
@@ -13,29 +14,35 @@ import mozilla.telemetry.glean.GleanMetrics.GleanBaseline
  * Connects process lifecycle events from Android to Glean's handleEvent
  * functionality (where the actual work of sending pings is done).
  */
-internal class GleanLifecycleObserver : DefaultLifecycleObserver {
+internal class GleanLifecycleObserver : LifecycleEventObserver {
     /**
-     * Calls the "background" event when entering the background.
+     * Called when lifecycle events are triggered.
      */
-    override fun onStop(owner: LifecycleOwner) {
-        // We're going to background, so store how much time we spent
-        // on foreground.
-        GleanBaseline.duration.stop()
-        Glean.handleBackgroundEvent()
-    }
+    override fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event) {
+        when (event) {
+            Lifecycle.Event.ON_STOP -> {
+                // We're going to background, so store how much time we spent
+                // on foreground.
+                GleanBaseline.duration.stop()
+                Glean.handleBackgroundEvent()
+            }
+            Lifecycle.Event.ON_START -> {
+                // Updates the baseline.duration metric when entering the foreground.
+                // We use ON_START here because we don't want to incorrectly count metrics in
+                // ON_RESUME as pause/resume can happen when interacting with things like the
+                // navigation shade which could lead to incorrectly recording the start of a
+                // duration, etc.
+                //
+                // https://developer.android.com/reference/android/app/Activity.html#onStart()
 
-    /**
-     * Updates the baseline.duration metric when entering the foreground.
-     * We use ON_START here because we don't want to incorrectly count metrics in ON_RESUME as
-     * pause/resume can happen when interacting with things like the navigation shade which could
-     * lead to incorrectly recording the start of a duration, etc.
-     *
-     * https://developer.android.com/reference/android/app/Activity.html#onStart()
-     */
-    override fun onStart(owner: LifecycleOwner) {
-        // Note that this is sending the length of the last foreground session
-        // because it belongs to the baseline ping and that ping is sent every
-        // time the app goes to background.
-        GleanBaseline.duration.start()
+                // Note that this is sending the length of the last foreground session
+                // because it belongs to the baseline ping and that ping is sent every
+                // time the app goes to background.
+                GleanBaseline.duration.start()
+            }
+            else -> {
+                // For other lifecycle events, do nothing
+            }
+        }
     }
 }
