75ccc864a52cb8b0470abc306d604f4bf84cda4a
Alessio Placitelli <alessio.placitelli@gmail.com>
2019-11-22 11:46:01 +0100

    Add license headers to build scripts

--- bin/build-rust-docs.bat
+++ bin/build-rust-docs.bat
@@ -1,3 +1,7 @@
+:: This Source Code Form is subject to the terms of the Mozilla Public
+:: License, v. 2.0. If a copy of the MPL was not distributed with this
+:: file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 :: Build all docs with one command, on Windows.
 :: Documentation will be placed in `build/docs`.
 :: This behaves the same as build-rust-docs.sh.
