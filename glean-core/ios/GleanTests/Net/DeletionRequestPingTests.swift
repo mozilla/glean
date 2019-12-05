afd4c737956f47fadae0cd4a07fc372d753373bf
Jan-Erik Rediger <jrediger@mozilla.com>
2019-12-05 13:55:10 +0100

    Test the deletion_request ping for iOS

---
+++ glean-core/ios/GleanTests/Net/DeletionRequestPingTests.swift
@@ -0,0 +1,105 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+@testable import Glean
+import OHHTTPStubs
+import XCTest
+
+// swiftlint:disable force_cast
+// REASON: Used in a test
+class DeletionRequestPingTests: XCTestCase {
+    var expectation: XCTestExpectation?
+    var lastPingJson: [String: Any]?
+
+    private func setupHttpResponseStub(statusCode: Int32 = 200) {
+        let host = URL(string: Configuration.Constants.defaultTelemetryEndpoint)!.host!
+        stub(condition: isHost(host)) { data in
+            let body = (data as NSURLRequest).ohhttpStubs_HTTPBody()
+            let json = try! JSONSerialization.jsonObject(with: body!, options: []) as? [String: Any]
+            XCTAssert(json != nil)
+            self.lastPingJson = json
+
+            // Fulfill test's expectation once we parsed the incoming data.
+            self.expectation?.fulfill()
+
+            // Ensure a response so that the uploader does its job.
+            return OHHTTPStubsResponse(
+                jsonObject: [],
+                statusCode: statusCode,
+                headers: ["Content-Type": "application/json"]
+            )
+        }
+    }
+
+    override func setUp() {
+        lastPingJson = nil
+        expectation = nil
+    }
+
+    func testDeletionRequestPingsAreSentWhenUploadDisabled() {
+        Glean.shared.resetGlean(clearStores: true)
+
+        setupHttpResponseStub()
+        expectation = expectation(description: "Completed upload")
+
+        Glean.shared.setUploadEnabled(false)
+
+        waitForExpectations(timeout: 5.0) { error in
+            XCTAssertNil(error, "Test timed out waiting for upload: \(error!)")
+        }
+
+        let pingInfo = lastPingJson!["ping_info"] as! [String: Any]
+        XCTAssertEqual(pingInfo["ping_type"] as! String, "deletion_request")
+
+        let clientInfo = lastPingJson!["client_info"] as! [String: Any]
+        let clientId = clientInfo["client_id"] as! String
+        XCTAssertNotEqual(clientId, "c0ffeec0-ffee-c0ff-eec0-ffeec0ffeec0")
+    }
+
+    func testPendingDeletionRequestPingsAreSentOnStartup() {
+        let glean = Glean.shared
+        glean.testDestroyGleanHandle()
+
+        // Create directory for pending deletion_request pings
+        let pendingDeletionRequestDir = getDocumentsDirectory().appendingPathComponent("deletion_request")
+        try! FileManager.default.createDirectory(
+            atPath: pendingDeletionRequestDir.path,
+            withIntermediateDirectories: true,
+            attributes: nil
+        )
+
+        // Write a deletion_request ping file
+        let pingId = "b4e4ede0-8716-4691-a3fa-493c56c5be2d"
+        let submitPath = "/submit/org-mozilla-samples-gleancore/deletion_request/1/\(pingId)"
+        // swiftlint:disable line_length
+        // REASON: This is inline JSON
+        let json = "{\"ping_info\": {\"ping_type\": \"deletion_request\"}, \"client_info\": {\"client_id\": \"test-only\"}}"
+        // swiftlint:enable line_length
+        let content = "\(submitPath)\n\(json)"
+        let pingFile = pendingDeletionRequestDir.appendingPathComponent(pingId)
+        FileManager.default.createFile(
+            atPath: pingFile.relativePath,
+            contents: content.data(using: .utf8),
+            attributes: nil
+        )
+
+        setupHttpResponseStub()
+        expectation = expectation(description: "Completed upload")
+
+        // Init Glean.
+        glean.setUploadEnabled(false)
+        glean.initialize()
+
+        waitForExpectations(timeout: 5.0) { error in
+            XCTAssertNil(error, "Test timed out waiting for upload: \(error!)")
+        }
+
+        let pingInfo = lastPingJson?["ping_info"] as? [String: Any]
+        XCTAssertEqual(pingInfo?["ping_type"] as? String, "deletion_request")
+
+        let clientInfo = lastPingJson!["client_info"] as! [String: Any]
+        let clientId = clientInfo["client_id"] as! String
+        XCTAssertEqual(clientId, "test-only")
+    }
+}
