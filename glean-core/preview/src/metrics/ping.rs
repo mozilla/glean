6be293a6867206d3040b947dcb41db38b5c230da
Jan-Erik Rediger <jrediger@mozilla.com>
2019-12-06 18:42:10 +0100

    Reimplement what FFI does locally
    This avoids the FFI dependency, which might cause problems as it's
    compiled as a cdylib.

--- glean-core/preview/src/metrics/ping.rs
+++ glean-core/preview/src/metrics/ping.rs
@@ -2,9 +2,6 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-use ffi_support::FfiStr;
-use std::ffi::CString;
-
 /// Stores information about a ping.
 ///
 /// This is required so that given metric data queued on disk we can send
@@ -12,7 +9,7 @@ use std::ffi::CString;
 #[derive(Clone, Debug)]
 pub struct PingType {
     pub(crate) name: String,
-    pub(crate) handle: u64,
+    pub(crate) ping_type: glean_core::metrics::PingType,
 }
 
 impl PingType {
@@ -26,13 +23,9 @@ impl PingType {
     /// * `send_if_empty` - Whether the ping should be sent empty or not.
     pub fn new<A: Into<String>>(name: A, include_client_id: bool, send_if_empty: bool) -> Self {
         let name = name.into();
-        let ffi_name = CString::new(name.clone()).unwrap();
-        let handle = glean_ffi::ping_type::glean_new_ping_type(
-            FfiStr::from_cstr(&ffi_name),
-            include_client_id as u8,
-            send_if_empty as u8,
-        );
-        Self { name, handle }
+        let ping_type =
+            glean_core::metrics::PingType::new(name.clone(), include_client_id, send_if_empty);
+        Self { name, ping_type }
     }
 
     /// Send the ping.
