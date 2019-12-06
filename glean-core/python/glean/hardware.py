7e57ce282ec66e6c9d9a73e518333e2c6d697299
Michael Droettboom <mdboom@gmail.com>
2019-12-06 14:48:45 -0500

    Record device info in Python bindings

---
+++ glean-core/python/glean/hardware.py
@@ -0,0 +1,102 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+"""
+This module provides a cross-platform abstraction to get system model and
+manufacturer information.
+"""
+
+import dataclasses
+from pathlib import Path
+import sys
+
+
+# Loosely based on the Java code found here:
+#    https://github.com/oshi/oshi/pull/264
+
+
+@dataclasses.dataclass
+class SystemInformation:
+    """
+    Stores information about the model and manufacturer of the system.
+    """
+
+    model: str
+    """
+    The model name of the current system
+    """
+
+    manufacturer: str
+    """
+    The manufacturer of the current system
+    """
+
+
+if sys.platform.startswith("linux"):
+
+    def get_system_information():
+        def get_value(path):
+            try:
+                with open(dmi_root / path, "rb") as fd:
+                    return fd.read().decode("ascii", "replace").strip()
+            except IOError:
+                return "error"
+
+        dmi_root = Path(b"/sys/devices/virtual/dmi/id")
+
+        model = get_value(b"product_name")
+        manufacturer = get_value(b"sys_vendor")
+
+        return SystemInformation(model=model, manufacturer=manufacturer)
+
+
+elif sys.platform == "darwin":
+
+    def get_system_information():
+        def get_value(name):
+            for line in sysinfo.splitlines():
+                line = line.strip()
+                if line.startswith(name):
+                    return line[len(name) :].decode("ascii", "replace").strip()
+            return "error"
+
+        import subprocess
+
+        try:
+            sysinfo = subprocess.check_output(
+                [b"system_profiler", b"SPHardwareDataType"]
+            )
+        except subprocess.CalledProcessError:
+            sysinfo = ""
+
+        model = get_value(b"Model Identifier: ")
+        manufacturer = "Apple Inc."
+
+        return SystemInformation(model=model, manufacturer=manufacturer)
+
+
+elif sys.platform.startswith("win"):
+
+    def get_system_information():
+        def get_value(name):
+            try:
+                output = subprocess.check_output(["WMIC", "CSPRODUCT", "GET", name])
+            except subprocess.CalledProcessError:
+                return "error"
+            lines = output.splitlines()
+            if lines < 3:
+                return "error"
+            return lines[2].strip().decode("ascii", "replace")
+
+        import subprocess
+
+        model = get_value("NAME")
+        manufacturer = get_value("VENDOR")
+
+        return SystemInformation(model=model, manufacturer=manufacturer)
+
+else:
+
+    def get_system_information():
+        return SystemInformation(model="unknown", manufacturer="unknown")
