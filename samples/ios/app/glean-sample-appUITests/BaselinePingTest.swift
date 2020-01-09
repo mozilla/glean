3c302df07686974525722ac6e663f616ddf05395
Jan-Erik Rediger <jrediger@mozilla.com>
2020-01-09 11:01:56 +0100

    Check that keys exists in the data
    Previously the tests could fail with an opaque "Test crashed" error and
    only the log file will reveal the actual issue, e.g.
    
        Fatal error: Unexpectedly found nil while unwrapping an Optional value: file
        /Users/distiller/project/samples/ios/app/glean-sample-appUITests/BaselinePingTest.swift, line 63
    
    By having explicit assertions and including the data in the message we
    should be able to see the problems right from the visible CI log.

--- samples/ios/app/glean-sample-appUITests/BaselinePingTest.swift
+++ samples/ios/app/glean-sample-appUITests/BaselinePingTest.swift
@@ -60,10 +60,13 @@ class BaselinePingTest: XCTestCase {
 
         // Make sure we have a 'duration' field with a reasonable value: it should be >= 1, since
         // we slept for 1000ms.
+        XCTAssertTrue(metrics.keys.contains("timespan"), "Metrics should have timespans: \(metrics)")
         let timespans = metrics["timespan"] as! [String: Any]
+        XCTAssertTrue(timespans.keys.contains("glean.baseline.duration"),
+                      "Timespans should have baseline duration: \(timespans)")
         let duration = timespans["glean.baseline.duration"] as! [String: Any]
         let durationValue = duration["value"] as! UInt64
-        XCTAssertTrue(durationValue >= 1)
+        XCTAssertTrue(durationValue >= 1, "Duration \(durationValue) should be positive")
 
         // Make sure there's no errors.
         let errors = metrics["labeled_counter"] as? [String: Any]
