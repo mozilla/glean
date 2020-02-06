77ac166c34857da8989add889e74c25bd5cae492
Jan-Erik Rediger <jrediger@mozilla.com>
2020-02-06 13:58:36 +0100

    Update glean_parser to include bug fix and restrict transitive dependency versions

--- glean-core/ios/sdk_generator.sh
+++ glean-core/ios/sdk_generator.sh
@@ -25,7 +25,7 @@
 
 set -e
 
-GLEAN_PARSER_VERSION=1.15.5
+GLEAN_PARSER_VERSION=1.15.6
 
 # CMDNAME is used in the usage text below.
 # shellcheck disable=SC2034
--- glean-core/python/setup.py
+++ glean-core/python/setup.py
@@ -39,7 +39,7 @@ with (ROOT.parent / "Cargo.toml").open() as cargo:
     parsed_toml = toml.load(cargo)
     version = parsed_toml["package"]["version"]
 
-requirements = ["cffi==1.13.1", "glean_parser==1.15.5", "inflection==0.3.1"]
+requirements = ["cffi==1.13.1", "glean_parser==1.15.6", "inflection==0.3.1"]
 
 setup_requirements = []
 
