22f927935174a45d8f422e6ac445c6915dc58626
Beatriz Rizental <beatriz.rizental@gmail.com>
2020-03-05 12:30:33 -0300

    Make it easier to consume FfiPingUploadTask in Kotlin

--- glean-core/android/src/main/java/mozilla/telemetry/glean/upload/Upload.kt
+++ glean-core/android/src/main/java/mozilla/telemetry/glean/upload/Upload.kt
@@ -4,9 +4,12 @@
 
 package mozilla.telemetry.glean.upload
 
+import org.json.JSONObject
 import com.sun.jna.Structure
 import com.sun.jna.Pointer
 import com.sun.jna.Union
+import mozilla.telemetry.glean.net.HeadersList
+import mozilla.telemetry.glean.rust.getRustString
 
 @Structure.FieldOrder("tag", "uuid", "path", "body", "headers")
 internal class Upload_Body(
@@ -33,4 +36,63 @@ internal open class FfiPingUploadTask(
     @JvmField var upload: Upload_Body = Upload_Body()
 ) : Union() {
     class ByValue : FfiPingUploadTask(), Structure.ByValue
+
+    fun toPingUploadTask(): PingUploadTask {
+        return when (this.tag.toInt()) {
+            0 -> PingUploadTask.Wait
+            1 -> {
+                this.readField("upload")
+                PingUploadTask.Upload(this.upload.toPingRequest())
+            }
+            else -> PingUploadTask.Done
+        }
+    }
+}
+
+/**
+ * The PingUploadTask makes it easier to consume Upload_Body.
+ */
+internal class PingRequest(
+    val path: String,
+    val body: String,
+    headers: String
+) {
+    val headers: HeadersList = headersFromJSONString(headers)
+
+    companion object {
+        fun headersFromJSONString(str: String): HeadersList {
+            val jsonHeaders = JSONObject(str)
+            val headers: MutableList<Pair<String, String>> = mutableListOf()
+            for (key in jsonHeaders.keys()) {
+                headers.add(Pair(key, jsonHeaders.get(key).toString()))
+            }
+            return headers
+        }
+    }
+}
+
+/**
+ * The PingUploadTask makes it easier to consume FfiPingUploadTask.
+ */
+internal sealed class PingUploadTask {
+    /**
+     * A PingRequest popped from the front of the queue.
+     */
+    class Upload(val request: PingRequest) : PingUploadTask()
+
+    /**
+     * A flag signaling that the pending pings directories are not done being processed,
+     * thus the requester should wait and come back later.
+     */
+    object Wait : PingUploadTask()
+
+    /**
+     * A flag signaling that the pending pings queue is empty and requester is done.
+     */
+    object Done : PingUploadTask()
+
+    fun request(): PingRequest? = when (this) {
+        is Upload -> this.request
+        else -> null
+    }
 }
