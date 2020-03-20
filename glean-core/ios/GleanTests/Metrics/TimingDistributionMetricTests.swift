2ca74bf6550cbeea8e4555f29643595d9112cd14
Travis Long <tlong@mozilla.com>
2020-03-20 15:32:25 -0500

    iOS: Add convenience `measure` function to `TimingDistributionMetricType`

--- glean-core/ios/GleanTests/Metrics/TimingDistributionMetricTests.swift
+++ glean-core/ios/GleanTests/Metrics/TimingDistributionMetricTests.swift
@@ -130,4 +130,36 @@ class TimingDistributionTypeTests: XCTestCase {
 
         XCTAssertEqual(1, metric.testGetNumRecordedErrors(.invalidState))
     }
+    
+    func testMeasureFunctionCorrectlyStoresValues() {
+        let metric = TimingDistributionMetricType(
+            category: "telemetry",
+            name: "timing_distribution",
+            sendInPings: ["store1"],
+            lifetime: .ping,
+            disabled: false,
+            timeUnit: .millisecond
+        )
+        
+        func testFunc(value: Bool) -> Bool {
+            return value
+        }
+
+        // Accumulate a few values
+        for _ in 1 ... 3 {
+            let testValue = metric.measure {
+                testFunc(value: true)
+            }
+            
+            // Make sure that the `measure` function returns the value from
+            // the measured function
+            XCTAssertTrue(testValue)
+        }
+
+        // Check that data was properly recorded.
+        // We can only check the count, as we don't control the time.
+        XCTAssert(metric.testHasValue())
+        let snapshot = try! metric.testGetValue()
+        XCTAssertEqual(3, snapshot.count)
+    }
 }
