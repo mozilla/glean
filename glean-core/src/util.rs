184a0373480b7e08649108868c711ef0c5cc5321
Michael Droettboom <mdboom@gmail.com>
2020-03-26 09:45:08 -0400

    Python Windows testing (#779)
    * Python: Testing on Windows
    
    * Fix floating point on x86_64 Windows
    
    * Update comment

--- glean-core/src/util.rs
+++ glean-core/src/util.rs
@@ -112,13 +112,14 @@ pub(crate) fn truncate_string_at_boundary_with_error<S: Into<String>>(
 }
 
 // On i686 on Windows, the CPython interpreter sets the FPU precision control
-// flag to 53 bits of precision, rather than the 64 bit default. This causes
-// different floating point results than on other architectures. This context
-// manager makes it easy to set the precision to 64-bits around floating point
-// operations.
+// flag to 53 bits of precision, rather than the 64 bit default. On x86_64 on
+// Windows, the CPython interpreter changes the rounding control settings. This
+// causes different floating point results than on other architectures. This
+// context manager makes it easy to set the correct precision and rounding control
+// to match our other targets and platforms.
 //
 // See https://bugzilla.mozilla.org/show_bug.cgi?id=1623335 for additional context.
-#[cfg(all(target_arch = "x86", target_os = "windows"))]
+#[cfg(target_os = "windows")]
 pub mod floating_point_context {
     use libc::size_t;
 
@@ -130,6 +131,10 @@ pub mod floating_point_context {
         fn _controlfp_s(current: *mut size_t, new: size_t, mask: size_t) -> size_t;
     }
 
+    // Rounding control mask
+    const MCW_RC: size_t = 0x00000300;
+    // Round by truncation
+    const RC_CHOP: size_t = 0x00000300;
     // Precision control mask
     const MCW_PC: size_t = 0x00030000;
     // Values for 64-bit precision
@@ -142,7 +147,7 @@ pub mod floating_point_context {
     impl FloatingPointContext {
         pub fn new() -> Self {
             let mut current: size_t = 0;
-            let _err = unsafe { _controlfp_s(&mut current, PC_64, MCW_PC) };
+            let _err = unsafe { _controlfp_s(&mut current, PC_64 | RC_CHOP, MCW_PC | MCW_RC) };
 
             FloatingPointContext {
                 original_value: current,
@@ -153,12 +158,12 @@ pub mod floating_point_context {
     impl Drop for FloatingPointContext {
         fn drop(&mut self) {
             let mut current: size_t = 0;
-            let _err = unsafe { _controlfp_s(&mut current, self.original_value, MCW_PC) };
+            let _err = unsafe { _controlfp_s(&mut current, self.original_value, MCW_PC | MCW_RC) };
         }
     }
 }
 
-#[cfg(not(all(target_arch = "x86", target_os = "windows")))]
+#[cfg(not(target_os = "windows"))]
 pub mod floating_point_context {
     pub struct FloatingPointContext {}
 
