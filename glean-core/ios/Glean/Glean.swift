1173de207e778f9a111ce29fe8ff29454e93560b
Jan-Erik Rediger <jrediger@mozilla.com>
2020-04-06 11:23:32 +0200

    Move lint marker out of the way so docs can get parsed

--- glean-core/ios/Glean/Glean.swift
+++ glean-core/ios/Glean/Glean.swift
@@ -50,6 +50,7 @@ public class Glean {
         self.initialized = false
     }
 
+    // swiftlint:disable function_body_length
     /// Initialize the Glean SDK.
     ///
     /// This should only be initialized once by the application, and not by
@@ -65,7 +66,6 @@ public class Glean {
     ///       If disabled, all persisted metrics, events and queued pings (except
     ///       first_run_date) are cleared.
     ///     * configuration: A Glean `Configuration` object with global settings.
-    // swiftlint:disable function_body_length
     public func initialize(uploadEnabled: Bool,
                            configuration: Configuration = Configuration()) {
         if self.isInitialized() {
