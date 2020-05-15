8edd3fbc28cb3be50bcd2ce94523e85e60ff9df5
Jan-Erik Rediger <jrediger@mozilla.com>
2020-05-15 11:42:21 +0200

    iOS (cleanup): Add UI tests to main testing scheme of sample app
    For some reason this was missing.
    With this added one can run the UI tests from the Xcode GUI

--- bin/run-ios-sample-app-test.sh
+++ bin/run-ios-sample-app-test.sh
@@ -9,7 +9,7 @@ set -euvx
 set -o pipefail && \
 xcodebuild \
   -workspace ./samples/ios/app/glean-sample-app.xcodeproj/project.xcworkspace \
-  -scheme glean-sample-appUITests \
+  -scheme glean-sample-app \
   -sdk iphonesimulator \
   -destination 'platform=iOS Simulator,name=iPhone 11' \
   test | \
