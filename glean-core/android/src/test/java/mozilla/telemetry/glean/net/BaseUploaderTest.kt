b83d0a0b4294e62b50b6370b86ef2295d27c72d3
Jan-Erik Rediger <jrediger@mozilla.com>
2020-05-20 11:33:39 +0200

    Remove ping logging from the Kotlin uploader

--- glean-core/android/src/test/java/mozilla/telemetry/glean/net/BaseUploaderTest.kt
+++ glean-core/android/src/test/java/mozilla/telemetry/glean/net/BaseUploaderTest.kt
@@ -4,11 +4,9 @@
 
 package mozilla.telemetry.glean.net
 
-import com.google.common.base.Strings
 import mozilla.telemetry.glean.config.Configuration
 import mozilla.components.support.test.any
 import mozilla.components.support.test.eq
-import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mockito.spy
@@ -40,60 +38,5 @@ class BaseUploaderTest {
         val expectedUrl = testDefaultConfig.serverEndpoint + testPath
         uploader.doUpload(testPath, testPing.toByteArray(Charsets.UTF_8), testHeaders, testDefaultConfig)
         verify(uploader).upload(eq(expectedUrl), any(), any())
-    }
-
-    @Test
-    fun `splitPingForLog() correctly divides large pings`() {
-        // This makes 10 full chunks plus a chunk with 10 bytes (1 message) to test breaking out
-        // a smaller last chunk
-        val testString = "*TestData*"
-        val testData = Strings.repeat(testString, BaseUploader.MAX_LOG_PAYLOAD_SIZE_BYTES + 1)
-        val testPath = "test/Glean"
-        val testChunk = Strings.repeat(
-            testString,
-            BaseUploader.MAX_LOG_PAYLOAD_SIZE_BYTES / testString.length
-        )
-
-        val chunks = BaseUploader.splitPingForLog(testData, testPath)
-
-        assertEquals("Must have correct number of chunks", 11, chunks.size)
-        var curChunk = 0
-        for (chunk in chunks) {
-            val calculatedHeaderMsg =
-                "Glean ping to URL: $testPath [Part ${curChunk + 1} of ${chunks.size}]\n"
-            // Need to re-add the /n here since `lines()` strips it
-            val actualHeaderMsg = "${chunk.lines()[0]}\n"
-            assertEquals(
-                "First line must contain correct header message",
-                calculatedHeaderMsg,
-                actualHeaderMsg
-            )
-
-            if (chunk === chunks.last()) {
-                assertEquals(
-                    "Must have correct size of chunk",
-                    testString.length + calculatedHeaderMsg.length,
-                    chunk.length
-                )
-                assertEquals(
-                    "Must have correct content",
-                    "$calculatedHeaderMsg$testString",
-                    chunk
-                )
-            } else {
-                assertEquals(
-                    "Must have correct size of chunk",
-                    BaseUploader.MAX_LOG_PAYLOAD_SIZE_BYTES + calculatedHeaderMsg.length,
-                    chunk.length
-                )
-                assertEquals(
-                    "Must have correct content",
-                    "$calculatedHeaderMsg$testChunk",
-                    chunk
-                )
-            }
-
-            curChunk += 1
-        }
     }
 }
