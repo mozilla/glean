01c16ff613eb64e6ba0112a04c68957ac423e9dd
brizental <beatriz.rizental@gmail.com>
2020-06-09 11:20:09 +0200

    Create metric to record upload failures

--- glean-core/src/upload/result.rs
+++ glean-core/src/upload/result.rs
@@ -64,3 +64,18 @@ impl From<u32> for UploadResult {
         }
     }
 }
+
+impl UploadResult {
+    /// Failures are recorded in the `ping_upload_failure` metric.
+    /// This metric is defined in the `metrics.yaml` for documentation purposes.
+    pub fn get_label(&self) -> Option<&str> {
+        match self {
+            UploadResult::HttpStatus(200..=299) => None,
+            UploadResult::HttpStatus(400..=499) => Some("status_code_4xx"),
+            UploadResult::HttpStatus(500..=599) => Some("status_code_5xx"),
+            UploadResult::HttpStatus(_) => Some("status_code_unknown"),
+            UploadResult::UnrecoverableFailure => Some("unrecoverable"),
+            UploadResult::RecoverableFailure => Some("recoverable"),
+        }
+    }
+}
