5f5012ebc39bcd524776117d943ba3463b3d666c
Travis Long <tlong@mozilla.com>
2020-06-18 10:13:47 -0500

    Move task scheduler to it's own file and address other change requests.

--- glean-core/csharp/Glean/Dispatchers.cs
+++ glean-core/csharp/Glean/Dispatchers.cs
@@ -4,11 +4,11 @@
 
 using System;
 using System.Collections.Concurrent;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Threading.Tasks.Schedulers;
 
 namespace Mozilla.Glean
 {
@@ -23,7 +23,7 @@ namespace Mozilla.Glean
         /// This is the number of milliseconds that are allowed for the initial
         /// tasks queue to process all of the queued tasks.
         /// </summary>
-        private const int QueueProcessingTimeout = 5000;
+        private const int QueueProcessingTimeoutMs = 5000;
 
         /// <summary>
         /// This is the maximum number of tasks that will be queued before
@@ -119,7 +119,7 @@ namespace Mozilla.Glean
                 {
                     // If we are in testing mode, then go ahead and await
                     // the task to ensure synchronous execution.
-                    factory.StartNew(action).Wait();
+                    action.Invoke();
                 }
             }
 
@@ -161,9 +161,9 @@ namespace Mozilla.Glean
             }
             else
             {
-                // This is not invoked on the apiScheduler to ensure that it
-                // gets executed now, ahead of anything on the queue. The task
-                // is returned so that it can be awaited if needed.
+                // **NOTE** This does not ensure that this task is executed at
+                // the front of the apiScheduler. This will be addressed with
+                // https://bugzilla.mozilla.org/show_bug.cgi?id=1646750
                 return factory.StartNew(action);
             }
         }
@@ -209,136 +209,6 @@ namespace Mozilla.Glean
         public static void AssertInTestingMode()
         {
             Debug.Assert(TestingMode);
-        }
-
-        // Provides a task scheduler that ensures a maximum concurrency level while
-        // running on top of the thread pool.  Copied from example provided here:
-        // https://docs.microsoft.com/en-us/dotnet/api/system.threading.tasks.taskscheduler?view=netcore-3.1
-        private class LimitedConcurrencyLevelTaskScheduler : TaskScheduler
-        {
-            // Indicates whether the current thread is processing work items.
-            [ThreadStatic]
-            private static bool _currentThreadIsProcessingItems;
-
-            // The list of tasks to be executed
-            // protected by lock(_tasks)
-            private readonly LinkedList<Task> _tasks = new LinkedList<Task>(); 
-
-            // The maximum concurrency level allowed by this scheduler.
-            private readonly int _maxDegreeOfParallelism;
-
-            // Indicates whether the scheduler is currently processing work items.
-            private int _delegatesQueuedOrRunning = 0;
-
-            // Creates a new instance with the specified degree of parallelism.
-            public LimitedConcurrencyLevelTaskScheduler(int maxDegreeOfParallelism)
-            {
-                if (maxDegreeOfParallelism < 1)
-                {
-                    throw new ArgumentOutOfRangeException("maxDegreeOfParallelism");
-                }
-                _maxDegreeOfParallelism = maxDegreeOfParallelism;
-            }
-
-            // Queues a task to the scheduler.
-            protected sealed override void QueueTask(Task task)
-            {
-                // Add the task to the list of tasks to be processed.  If there aren't enough
-                // delegates currently queued or running to process tasks, schedule another.
-                lock (_tasks)
-                {
-                    _tasks.AddLast(task);
-                    if (_delegatesQueuedOrRunning < _maxDegreeOfParallelism)
-                    {
-                        ++_delegatesQueuedOrRunning;
-                        NotifyThreadPoolOfPendingWork();
-                    }
-                }
-            }
-
-            // Inform the ThreadPool that there's work to be executed for this scheduler.
-            private void NotifyThreadPoolOfPendingWork()
-            {
-                ThreadPool.UnsafeQueueUserWorkItem(_ =>
-                {
-                    // Note that the current thread is now processing work items.
-                    // This is necessary to enable inlining of tasks into this thread.
-                    _currentThreadIsProcessingItems = true;
-                    try
-                    {
-                        // Process all available items in the queue.
-                        while (true)
-                        {
-                            Task item;
-                            lock (_tasks)
-                            {
-                                // When there are no more items to be processed,
-                                // note that we're done processing, and get out.
-                                if (_tasks.Count == 0)
-                                {
-                                    --_delegatesQueuedOrRunning;
-                                    break;
-                                }
-
-                                // Get the next item from the queue
-                                item = _tasks.First.Value;
-                                _tasks.RemoveFirst();
-                            }
-
-                            // Execute the task we pulled out of the queue
-                            base.TryExecuteTask(item);
-                        }
-                    }
-                    // We're done processing items on the current thread
-                    finally { _currentThreadIsProcessingItems = false; }
-                }, null);
-            }
-
-            // Attempts to execute the specified task on the current thread.
-            protected sealed override bool TryExecuteTaskInline(Task task,
-                bool taskWasPreviouslyQueued)
-            {
-                // If this thread isn't already processing a task, we don't support inlining
-                if (!_currentThreadIsProcessingItems) return false;
-
-                // If the task was previously queued, remove it from the queue
-                if (taskWasPreviouslyQueued)
-                    // Try to run the task.
-                    if (TryDequeue(task))
-                        return base.TryExecuteTask(task);
-                    else
-                        return false;
-                else
-                    return base.TryExecuteTask(task);
-            }
-
-            // Attempt to remove a previously scheduled task from the scheduler.
-            protected sealed override bool TryDequeue(Task task)
-            {
-                lock (_tasks) return _tasks.Remove(task);
-            }
-
-            // Gets the maximum concurrency level supported by this scheduler.
-            public sealed override int MaximumConcurrencyLevel
-            {
-                get { return _maxDegreeOfParallelism; }
-            }
-
-            // Gets an enumerable of the tasks currently scheduled on this scheduler.
-            protected sealed override IEnumerable<Task> GetScheduledTasks()
-            {
-                bool lockTaken = false;
-                try
-                {
-                    Monitor.TryEnter(_tasks, ref lockTaken);
-                    if (lockTaken) return _tasks;
-                    else throw new NotSupportedException();
-                }
-                finally
-                {
-                    if (lockTaken) Monitor.Exit(_tasks);
-                }
-            }
-        }
+        }        
     }
 }
