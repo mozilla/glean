b5acc4227250dfdf9253abe794858a1dd787fc44
Alessio Placitelli <alessio.placitelli@gmail.com>
2020-06-23 18:25:26 +0200

    Manually implement the C# core metrics

--- glean-core/csharp/Glean/ManualMetrics.cs
+++ glean-core/csharp/Glean/ManualMetrics.cs
@@ -32,6 +32,56 @@ namespace Mozilla.Glean.GleanMetrics
         // operations as `architecture.<operation>()` instead
         // of `architecture.Value.<operation>()`. 
         public StringMetricType architecture => architectureLazy.Value;
+
+        private readonly Lazy<StringMetricType> osVersionLazy = new Lazy<StringMetricType>(() => new StringMetricType(
+            category: "",
+            disabled: false,
+            lifetime: Lifetime.Application,
+            name: "os_version",
+            sendInPings: new string[] { "glean_client_info" }
+        ));
+
+        public StringMetricType osVersion => osVersionLazy.Value;
+
+        private readonly Lazy<StringMetricType> localeLazy = new Lazy<StringMetricType>(() => new StringMetricType(
+            category: "",
+            disabled: false,
+            lifetime: Lifetime.Application,
+            name: "locale",
+            sendInPings: new string[] { "glean_client_info" }
+        ));
+
+        public StringMetricType locale => localeLazy.Value;
+
+        private readonly Lazy<StringMetricType> appChannelLazy = new Lazy<StringMetricType>(() => new StringMetricType(
+            category: "",
+            disabled: false,
+            lifetime: Lifetime.Application,
+            name: "app_channel",
+            sendInPings: new string[] { "glean_client_info" }
+        ));
+
+        public StringMetricType appChannel => appChannelLazy.Value;
+
+        private readonly Lazy<StringMetricType> appBuildLazy = new Lazy<StringMetricType>(() => new StringMetricType(
+            category: "",
+            disabled: false,
+            lifetime: Lifetime.Application,
+            name: "app_build",
+            sendInPings: new string[] { "glean_client_info" }
+        ));
+
+        public StringMetricType appBuild => appBuildLazy.Value;
+
+        private readonly Lazy<StringMetricType> appDisplayversionLazy = new Lazy<StringMetricType>(() => new StringMetricType(
+            category: "",
+            disabled: false,
+            lifetime: Lifetime.Application,
+            name: "app_display_version",
+            sendInPings: new string[] { "glean_client_info" }
+        ));
+
+        public StringMetricType appDisplayVersion => appDisplayversionLazy.Value;
 #pragma warning restore IDE1006 // Naming Styles
     }
 }
