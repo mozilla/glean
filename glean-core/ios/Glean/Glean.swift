4273672b823217ad0dd1496d9ef37e6d7997b745
brizental <beatriz.rizental@gmail.com>
2020-07-20 11:53:15 +0200

    Implement JWE metric type for Swift

--- glean-core/ios/Glean/Glean.swift
+++ glean-core/ios/Glean/Glean.swift
@@ -117,7 +117,7 @@ public class Glean {
             }
 
             if let debugViewTag = self.debugViewTag {
-                self.setDebugViewTag(debugViewTag)
+                _ = self.setDebugViewTag(debugViewTag)
             }
 
             if self.logPings {
