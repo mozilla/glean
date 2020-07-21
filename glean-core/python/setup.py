5da3c619a0b3830a9a07b6909247d4799c7f8577
Michael Droettboom <mdboom@gmail.com>
2020-07-21 19:22:41 -0400

    Fix Python bindings

--- glean-core/python/setup.py
+++ glean-core/python/setup.py
@@ -48,7 +48,7 @@ with (ROOT.parent / "Cargo.toml").open() as cargo:
 
 requirements = [
     "cffi>=1",
-    "glean_parser==1.23.0",
+    "glean_parser==1.27.0",
     "iso8601>=0.1.10; python_version<='3.6'",
 ]
 
