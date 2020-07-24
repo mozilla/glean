6f85a261fb74a02cb51de4bfa414a9eceac4e422
Travis Long <tlong@mozilla.com>
2020-07-24 11:56:25 -0500

    Bumped version to 31.6.0

--- gradle-plugin/src/main/groovy/mozilla/telemetry/glean-gradle-plugin/GleanGradlePlugin.groovy
+++ gradle-plugin/src/main/groovy/mozilla/telemetry/glean-gradle-plugin/GleanGradlePlugin.groovy
@@ -374,7 +374,7 @@ except:
     }
 
     void apply(Project project) {
-        project.ext.glean_version = "31.5.0"
+        project.ext.glean_version = "31.6.0"
 
         File condaDir = setupPythonEnvironmentTasks(project)
         project.ext.set("gleanCondaDir", condaDir)
