57a195ddcdf57a11ba110307d2cf8f4dc2908730
Travis Long <tlong@mozilla.com>
2020-08-12 09:25:15 -0500

    Bug 1655100 - Capture device logs to diagnose intermittent test failure

--- glean-core/ios/Glean/Utils/Logger.swift
+++ glean-core/ios/Glean/Utils/Logger.swift
@@ -2,28 +2,21 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-/// This is a simple Logger class to unify and simplify log messaging
-/// across the application
-class Logger {
-    // This holds the log tag for reuse by the instance
-    private let tag: String
+import Foundation
+import os.log
 
-    // Represents the different supported log levels and the raw value
-    // that is used as a start character for the log message in order
-    // to be consistent with the Android log output.
-    enum LogLevel: String {
-        case debug = "D"
-        case info = "I"
-        case warn = "W"
-        case error = "E"
-    }
+class Logger {
+    private let log: OSLog
 
     /// Creates a new logger instance with the specified tag value
     ///
     /// - parameters:
     ///     * tag: `String` value used to tag log messages
     init(tag: String) {
-        self.tag = tag
+        self.log = OSLog(
+            subsystem: Bundle.main.bundleIdentifier!,
+            category: tag
+        )
     }
 
     /// Output a debug log message
@@ -31,7 +24,7 @@ class Logger {
     /// - parameters:
     ///     * message: The message to log
     func debug(_ message: String) {
-        log(message: message, level: .debug)
+        log(message, type: .debug)
     }
 
     /// Output an info log message
@@ -39,15 +32,7 @@ class Logger {
     /// - parameters:
     ///     * message: The message to log
     func info(_ message: String) {
-        log(message: message, level: .info)
-    }
-
-    /// Output a warning log message
-    ///
-    /// - parameters:
-    ///     * message: The message to log
-    func warn(_ message: String) {
-        log(message: message, level: .warn)
+        log(message, type: .info)
     }
 
     /// Output an error log message
@@ -55,25 +40,15 @@ class Logger {
     /// - parameters:
     ///     * message: The message to log
     func error(_ message: String) {
-        log(message: message, level: .error)
+        log(message, type: .error)
     }
 
-    /// Private function that formats and outputs the log message based on the level
+    /// Private function that calls os_log with the proper parameters
     ///
     /// - parameters:
     ///     * message: The message to log
     ///     * level: The `LogLevel` at which to output the message
-    private func log(message: String, level: LogLevel) {
-        let formattedMessage = "\(level.rawValue)/\(tag): \(message)"
-
-        // Since logging via `NSLog` has overhead, we only want to output via `NSLog`
-        // for critical messages such as errors.  Everything else can be outputted
-        // using `print()` so that it does not impact the log on release builds.
-        switch level {
-        case .error:
-            NSLog(formattedMessage)
-        default:
-            print(formattedMessage)
-        }
+    private func log(_ message: String, type: OSLogType) {
+        os_log("%@", log: self.log, type: type, message)
     }
 }
