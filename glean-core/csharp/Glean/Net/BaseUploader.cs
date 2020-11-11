mozilla/glean

commit 2261845761251d91b6968f29846dc3aabbc0cc45

Bug 1661240 - Have same throttle backoff time for all bindings (#1240)

glean-core/csharp/Glean/Net/BaseUploader.cs
@@ -19,6 +19,7 @@ namespace Mozilla.Glean.Net
     /// </summary>
     internal class BaseUploader
     {
+        internal const int THROTTLED_BACKOFF_MS = 60_000;
         private readonly IPingUploader uploader;
 
         /// <summary>
@@ -88,8 +89,6 @@ namespace Mozilla.Glean.Net
             // TODO: must not work like this, it should work off the main thread.
             // FOR TESTING Implement the upload worker here and call this from Glean.cs
 
-            int waitAttempts = 0;
-
             // Limits are enforced by glean-core to avoid an inifinite loop here.
             // Whenever a limit is reached, this binding will receive `UploadTaskTag.Done` and step out.
             while (true)
@@ -126,7 +125,7 @@ namespace Mozilla.Glean.Net
                         }
                         break;
                     case UploadTaskTag.Wait:
-                        Thread.Sleep(100);
+                        Thread.Sleep(THROTTLED_BACKOFF_MS);
                         break;
                     case UploadTaskTag.Done:
                         // Nothing to do here, break out of the loop.

glean-core/ios/Glean/Net/HttpPingUploader.swift
@@ -13,6 +13,7 @@ public class HttpPingUploader {
         // Since ping file names are UUIDs, this matches UUIDs for filtering purposes
         static let logTag = "glean/HttpPingUploader"
         static let connectionTimeout = 10000
+        static let throttleBackoffMs: UInt32 = 60_000
 
         // For this error, the ping will be retried later
         static let recoverableErrorStatusCode: UInt16 = 500
@@ -117,6 +118,7 @@ public class HttpPingUploader {
                     glean_process_ping_upload_response(&incomingTask, result.toFfi())
                 }
             case .wait:
+                sleep(Constants.throttleBackoffMs)
                 continue
             case .done:
                 return

glean-core/python/glean/glean.py
@@ -230,6 +230,9 @@ class Glean:
         The temporary directory will be destroyed when Glean is initialized
         again or at process shutdown.
         """
+        # Lower throttle backoff time for testing
+        PingUploadWorker._throttle_backoff_time = 1
+
         actual_data_dir = Path(tempfile.TemporaryDirectory().name)
         cls.initialize(
             application_id,

glean-core/python/glean/net/ping_upload_worker.py
@@ -23,6 +23,8 @@ log = logging.getLogger(__name__)
 
 
 class PingUploadWorker:
+    _throttle_backoff_time = 60_000
+
     @classmethod
     def process(cls):
         """
@@ -43,7 +45,13 @@ class PingUploadWorker:
         from .. import Glean
 
         return ProcessDispatcher.dispatch(
-            _process, (Glean._data_dir, Glean._application_id, Glean._configuration)
+            _process,
+            (
+                Glean._data_dir,
+                Glean._application_id,
+                Glean._configuration,
+                cls._throttle_backoff_time,
+            ),
         )
 
     @classmethod
@@ -94,7 +102,9 @@ def _parse_ping_headers(
     return headers
 
 
-def _process(data_dir: Path, application_id: str, configuration) -> bool:
+def _process(
+    data_dir: Path, application_id: str, configuration, throttle_backoff_time: int
+) -> bool:
 
     # Import here to avoid cyclical import
     from ..glean import Glean
@@ -144,7 +154,7 @@ def _process(data_dir: Path, application_id: str, configuration) -> bool:
                 incoming_task, upload_result.to_ffi()
             )
         elif tag == UploadTaskTag.WAIT:
-            time.sleep(1)
+            time.sleep(throttle_backoff_time)
         elif tag == UploadTaskTag.DONE:
             return True
 

glean-core/python/tests/test_glean.py
@@ -37,7 +37,6 @@ from glean.metrics import (
 from glean.net import PingUploadWorker
 from glean.testing import _RecordingUploader
 
-
 GLEAN_APP_ID = "glean-python-test"
 
 

commit 01642f5b9e60fbb5a204b9e9be8d1c37f6c81d28

Bug 1676712: Reduce throttle backoff time to 1s and fixes it for python and swift
Fixes regression introduced by PR #1240 :
Python and Swift "sleep" functions are expecting "seconds" and not ms.

glean-core/csharp/Glean/Net/BaseUploader.cs
@@ -19,7 +19,7 @@ namespace Mozilla.Glean.Net
     /// </summary>
     internal class BaseUploader
     {
-        internal const int THROTTLED_BACKOFF_MS = 60_000;
+        internal const int THROTTLED_BACKOFF_S = 1;
         private readonly IPingUploader uploader;
 
         /// <summary>
@@ -125,7 +125,7 @@ namespace Mozilla.Glean.Net
                         }
                         break;
                     case UploadTaskTag.Wait:
-                        Thread.Sleep(THROTTLED_BACKOFF_MS);
+                        Thread.Sleep(THROTTLED_BACKOFF_S * 1000);
                         break;
                     case UploadTaskTag.Done:
                         // Nothing to do here, break out of the loop.

glean-core/ios/Glean/Net/HttpPingUploader.swift
@@ -13,7 +13,7 @@ public class HttpPingUploader {
         // Since ping file names are UUIDs, this matches UUIDs for filtering purposes
         static let logTag = "glean/HttpPingUploader"
         static let connectionTimeout = 10000
-        static let throttleBackoffMs: UInt32 = 60_000
+        static let throttleBackoffS: UInt32 = 1
 
         // For this error, the ping will be retried later
         static let recoverableErrorStatusCode: UInt16 = 500
@@ -118,7 +118,7 @@ public class HttpPingUploader {
                     glean_process_ping_upload_response(&incomingTask, result.toFfi())
                 }
             case .wait:
-                sleep(Constants.throttleBackoffMs)
+                sleep(Constants.throttleBackoffS)
                 continue
             case .done:
                 return

glean-core/python/glean/glean.py
@@ -230,9 +230,6 @@ class Glean:
         The temporary directory will be destroyed when Glean is initialized
         again or at process shutdown.
         """
-        # Lower throttle backoff time for testing
-        PingUploadWorker._throttle_backoff_time = 1
-
         actual_data_dir = Path(tempfile.TemporaryDirectory().name)
         cls.initialize(
             application_id,

glean-core/python/glean/net/ping_upload_worker.py
@@ -23,7 +23,8 @@ log = logging.getLogger(__name__)
 
 
 class PingUploadWorker:
-    _throttle_backoff_time = 60_000
+    # In seconds
+    _throttle_backoff_time = 1.0
 
     @classmethod
     def process(cls):