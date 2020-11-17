298061cf6927f5b37d5ea50cd44a315e0f8b76c0
Alessio Placitelli <alessio.placitelli@gmail.com>
2020-11-17 13:08:17 +0100

    Don't require `get_submetric` in the `Labeled` trait
    This is really an implementation detail of the type
    and not something we want to enforce in the API.

--- glean-core/src/traits/labeled.rs
+++ glean-core/src/traits/labeled.rs
@@ -2,7 +2,6 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-use crate::metrics::MetricType;
 use crate::ErrorType;
 
 /// A description for the `LabeledMetric` type.
@@ -11,7 +10,7 @@ use crate::ErrorType;
 /// implemented in the related type in `../metrics/`.
 pub trait Labeled<T>
 where
-    T: MetricType + Clone,
+    T: Clone,
 {
     /// Gets a specific metric for a given label.
     ///
@@ -25,12 +24,6 @@ where
     /// Labels must be `snake_case` and less than 30 characters.
     /// If an invalid label is used, the metric will be recorded in the special `OTHER_LABEL` label.
     fn get(&self, label: &str) -> T;
-
-    /// Gets the template submetric.
-    ///
-    /// The template submetric is the actual metric that is cloned and modified
-    /// to record for a specific label.
-    fn get_submetric(&self) -> &T;
 
     /// **Exported for test purposes.**
     ///
