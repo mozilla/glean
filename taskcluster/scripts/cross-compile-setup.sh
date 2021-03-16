54591ba3cf368bf7fba79acf44b493343096c155
Jan-Erik Rediger <jrediger@mozilla.com>
2021-03-16 09:45:13 +0100

    Pick clang 11 and cctools from m-c build
    clang and cctools selected from the following treeherder run:
    https://treeherder.mozilla.org/jobs?repo=autoland&revision=11810f926917ae7e79b7fa156239b2cab7b0327c
    
    This was picked from checking what artifacts another cross-compile build
    on Treeherder picked.
    
    This contains clang 11 and corresponding cctools.
    This supports both x86_64 and aarch64 darwin. We only need the former
    for now (but might use the latter eventually).
    
    It's not clear how stable these URLs are long-term, but we'll work on
    getting them into tooltool (or make sure we cache them in the Docker
    container instead of installing at runtime)

--- taskcluster/scripts/cross-compile-setup.sh
+++ taskcluster/scripts/cross-compile-setup.sh
@@ -2,12 +2,12 @@
 export PATH=$PATH:/tmp/clang/bin
 export ORG_GRADLE_PROJECT_RUST_ANDROID_GRADLE_TARGET_X86_64_APPLE_DARWIN_CC=/tmp/clang/bin/clang
 export ORG_GRADLE_PROJECT_RUST_ANDROID_GRADLE_TARGET_X86_64_APPLE_DARWIN_TOOLCHAIN_PREFIX=/tmp/cctools/bin
-export ORG_GRADLE_PROJECT_RUST_ANDROID_GRADLE_TARGET_X86_64_APPLE_DARWIN_AR=/tmp/cctools/bin/x86_64-darwin11-ar
-export ORG_GRADLE_PROJECT_RUST_ANDROID_GRADLE_TARGET_X86_64_APPLE_DARWIN_RANLIB=/tmp/cctools/bin/x86_64-darwin11-ranlib
+export ORG_GRADLE_PROJECT_RUST_ANDROID_GRADLE_TARGET_X86_64_APPLE_DARWIN_AR=/tmp/cctools/bin/x86_64-apple-darwin-ar
+export ORG_GRADLE_PROJECT_RUST_ANDROID_GRADLE_TARGET_X86_64_APPLE_DARWIN_RANLIB=/tmp/cctools/bin/x86_64-apple-darwin-ranlib
 export ORG_GRADLE_PROJECT_RUST_ANDROID_GRADLE_TARGET_X86_64_APPLE_DARWIN_LD_LIBRARY_PATH=/tmp/clang/lib
-export ORG_GRADLE_PROJECT_RUST_ANDROID_GRADLE_TARGET_X86_64_APPLE_DARWIN_RUSTFLAGS="-C linker=/tmp/clang/bin/clang -C link-arg=-B -C link-arg=/tmp/cctools/bin -C link-arg=-target -C link-arg=x86_64-darwin11 -C link-arg=-isysroot -C link-arg=/tmp/MacOSX10.12.sdk -C link-arg=-Wl,-syslibroot,/tmp/MacOSX10.12.sdk -C link-arg=-Wl,-dead_strip"
+export ORG_GRADLE_PROJECT_RUST_ANDROID_GRADLE_TARGET_X86_64_APPLE_DARWIN_RUSTFLAGS="-C linker=/tmp/clang/bin/clang -C link-arg=-B -C link-arg=/tmp/cctools/bin -C link-arg=-target -C link-arg=x86_64-apple-darwin -C link-arg=-isysroot -C link-arg=/tmp/MacOSX10.12.sdk -C link-arg=-Wl,-syslibroot,/tmp/MacOSX10.12.sdk -C link-arg=-Wl,-dead_strip"
 # For ring's use of `cc`.
-export ORG_GRADLE_PROJECT_RUST_ANDROID_GRADLE_TARGET_X86_64_APPLE_DARWIN_CFLAGS_x86_64_apple_darwin="-B /tmp/cctools/bin -target x86_64-darwin11 -isysroot /tmp/MacOSX10.12.sdk -Wl,-syslibroot,/tmp/MacOSX10.12.sdk -Wl,-dead_strip"
+export ORG_GRADLE_PROJECT_RUST_ANDROID_GRADLE_TARGET_X86_64_APPLE_DARWIN_CFLAGS_x86_64_apple_darwin="-B /tmp/cctools/bin -target x86_64-apple-darwin -isysroot /tmp/MacOSX10.12.sdk -Wl,-syslibroot,/tmp/MacOSX10.12.sdk -Wl,-dead_strip"
 # The wrong linker gets used otherwise: https://github.com/rust-lang/rust/issues/33465.
 export ORG_GRADLE_PROJECT_RUST_ANDROID_GRADLE_TARGET_X86_64_PC_WINDOWS_GNU_RUSTFLAGS="-C linker=x86_64-w64-mingw32-gcc"
 
@@ -25,6 +25,14 @@ tooltool.py \
   --url=http://taskcluster/tooltool.mozilla-releng.net/ \
   --manifest="/builds/worker/checkouts/src/taskcluster/scripts/macos-cc-tools.manifest" \
   fetch
+
+curl -sfSL --retry 5 --retry-delay 10 https://firefox-ci-tc.services.mozilla.com/api/queue/v1/task/NcaljhkvQOSKxGlBSWJl_w/runs/0/artifacts/public/build/cctools.tar.xz > cctools.tar.xz
+tar -xf cctools.tar.xz
+ls -l /tmp/cctools
+curl -sfSL --retry 5 --retry-delay 10 https://firefox-ci-tc.services.mozilla.com/api/queue/v1/task/eJJotfnPSt2weVG0sZlfeQ/runs/0/artifacts/public/build/clang.tar.zst > clang.tar.zst
+tar -I zstd -xf clang.tar.zst
+ls -l /tmp/clang
+ls -l /tmp/clang/bin
 
 rustup target add x86_64-apple-darwin
 rustup target add x86_64-pc-windows-gnu
