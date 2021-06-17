0dab4526c4142b095c0554db2e44498f5c6a1a37
Jan-Erik Rediger <jrediger@mozilla.com>
2021-06-17 12:14:18 +0200

    Extend counter to actually store data into the database

--- glean-core/uniffi/src/private/counter.rs
+++ glean-core/uniffi/src/private/counter.rs
@@ -34,21 +34,48 @@ impl CounterMetric {
     }
 
     /// Internal only, synchronous API for incremeting a counter
-    pub(crate) fn add_sync(&self, _glean: &Glean, amount: i32) {
+    pub(crate) fn add_sync(&self, glean: &Glean, amount: i32) {
         log::info!("Counter({:?}).add({})", self.meta, amount);
-        self.count.fetch_add(amount, Ordering::SeqCst);
+
+        if !self.should_record(glean) {
+            return;
+        }
+
+        if amount <= 0 {
+            record_error(
+                glean,
+                &self.meta,
+                ErrorType::InvalidValue,
+                format!("Added negative or zero value {}", amount),
+                None,
+            );
+            return;
+        }
+
+        glean
+            .storage()
+            .record_with(glean, &self.meta, |old_value| match old_value {
+                Some(Metric::Counter(old_value)) => {
+                    Metric::Counter(old_value.saturating_add(amount))
+                }
+                _ => Metric::Counter(amount),
+            })
     }
 
     pub fn add(&self, amount: i32) {
         crate::core::with_glean(|glean| self.add_sync(glean, amount))
     }
 
-    pub fn test_get_value(&self, _ping_name: Option<String>) -> Option<i32> {
+    pub(crate) fn get_value(&self, _glean: &Glean, _ping_name: Option<&str>) -> Option<i32> {
         let val = self.count.load(Ordering::SeqCst);
         if val > 0 {
             Some(val)
         } else {
             None
         }
+    }
+
+    pub fn test_get_value(&self, ping_name: Option<String>) -> Option<i32> {
+        crate::core::with_glean(|glean| self.get_value(glean, ping_name.as_deref()))
     }
 }
