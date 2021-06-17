f514edbcfddfbf5b650f675abd7a7255fd248403
Jan-Erik Rediger <jrediger@mozilla.com>
2021-06-17 12:14:18 +0200

    Bring back CommonMetricData to be equivalent to the core SDK

--- glean-core/uniffi/src/common_metric_data.rs
+++ glean-core/uniffi/src/common_metric_data.rs
@@ -4,6 +4,10 @@
 
 use std::convert::TryFrom;
 
+use crate::error::{Error, ErrorKind};
+use crate::private::labeled::validate_dynamic_label;
+use crate::Glean;
+
 /// The supported metrics' lifetimes.
 ///
 /// A metric's lifetime determines when its stored data gets reset.
@@ -36,14 +40,14 @@ impl Lifetime {
 }
 
 impl TryFrom<i32> for Lifetime {
-    type Error = ();
+    type Error = Error;
 
     fn try_from(value: i32) -> Result<Lifetime, Self::Error> {
         match value {
             0 => Ok(Lifetime::Ping),
             1 => Ok(Lifetime::Application),
             2 => Ok(Lifetime::User),
-            _e => Err(()),
+            e => Err(ErrorKind::Lifetime(e).into()),
         }
     }
 }
@@ -84,6 +88,32 @@ impl CommonMetricData {
             category: category.into(),
             send_in_pings: vec![ping_name.into()],
             ..Default::default()
+        }
+    }
+
+    /// The metric's base identifier, including the category and name, but not the label.
+    ///
+    /// If `category` is empty, it's ommitted.
+    /// Otherwise, it's the combination of the metric's `category` and `name`.
+    pub(crate) fn base_identifier(&self) -> String {
+        if self.category.is_empty() {
+            self.name.clone()
+        } else {
+            format!("{}.{}", self.category, self.name)
+        }
+    }
+
+    /// The metric's unique identifier, including the category, name and label.
+    ///
+    /// If `category` is empty, it's ommitted.
+    /// Otherwise, it's the combination of the metric's `category`, `name` and `label`.
+    pub(crate) fn identifier(&self, glean: &Glean) -> String {
+        let base_identifier = self.base_identifier();
+
+        if let Some(label) = &self.dynamic_label {
+            validate_dynamic_label(glean, self, &base_identifier, label)
+        } else {
+            base_identifier
         }
     }
 
