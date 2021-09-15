feb42833ba64be930c2008ffd2167791b79cd9d5
Chris H-C <chutten@mozilla.com>
2021-09-15 10:20:47 -0400

    bug 1729723 - Expose persist_ping_lifetime_data in the RLB.

--- glean-core/rlb/src/lib.rs
+++ glean-core/rlb/src/lib.rs
@@ -784,5 +784,22 @@ pub fn get_timestamp_ms() -> u64 {
     glean_core::get_timestamp_ms()
 }
 
+/// Asks the database to persist ping-lifetime data to disk. Probably expensive to call.
+/// Only has effect when Glean is configured with `delay_ping_lifetime_io: true`.
+/// If Glean hasn't been initialized this will dispatch and return Ok(()),
+/// otherwise it will block until the persist is done and return its Result.
+pub fn persist_ping_lifetime_data() -> Result<()> {
+    if !was_initialize_called() {
+        crate::launch_with_glean(|glean| {
+            // This is async, we can't get the Error back to the caller.
+            let _ = glean.persist_ping_lifetime_data();
+        });
+        Ok(())
+    } else {
+        block_on_dispatcher();
+        with_glean(|glean| glean.persist_ping_lifetime_data())
+    }
+}
+
 #[cfg(test)]
 mod test;
