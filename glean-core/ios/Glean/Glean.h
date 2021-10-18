3c1faf09811ed1f3974d60dfd6820c8f8f305104
Jan-Erik Rediger <jrediger@mozilla.com>
2021-10-18 15:16:12 +0200

    Run `uniffi-bindgen` as part of the Xcode build

--- glean-core/ios/Glean/Glean.h
+++ glean-core/ios/Glean/Glean.h
@@ -12,4 +12,4 @@ FOUNDATION_EXPORT const unsigned char GleanVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Glean/PublicHeader.h>
 
-#import "GleanFfi.h"
+#import "gleanFFI.h"
