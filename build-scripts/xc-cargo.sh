dbf9dd76c8362f670f92d09dc9d84df99c7f7b68
Jan-Erik Rediger <jrediger@mozilla.com>
2021-10-18 15:16:12 +0200

    Run `uniffi-bindgen` as part of the Xcode build

---
+++ build-scripts/xc-cargo.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+#
+# This is a small wrapper for running `cargo` inside of an XCode build,
+# which unfortunately doesn't seem to work quite right out-of-the-box.
+set -eEuvx
+
+# Xcode tries to be helpful and overwrites the PATH. Reset that.
+export PATH="${HOME}/.cargo/bin:$PATH"
+
+"${@}"
