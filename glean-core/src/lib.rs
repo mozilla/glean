9465806d144316a76494f9593ae9b4f75b176790
Jan-Erik Rediger <jrediger@mozilla.com>
2021-11-04 14:40:10 +0100

    [Rust] Cleanup: Move the macro to where it is used

--- glean-core/src/lib.rs
+++ glean-core/src/lib.rs
@@ -22,10 +22,6 @@ use once_cell::sync::OnceCell;
 use std::sync::Mutex;
 use uuid::Uuid;
 
-// This needs to be included first, and the space below prevents rustfmt from
-// alphabetizing it.
-mod macros;
-
 mod common_metric_data;
 mod coverage;
 mod database;
