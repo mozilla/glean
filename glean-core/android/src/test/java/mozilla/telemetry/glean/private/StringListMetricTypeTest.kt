064a0fc03a25ed65342d6feec6241f1113c1415c
Jan-Erik Rediger <jrediger@mozilla.com>
2021-11-24 10:24:28 +0100

    [UniFFI] Migrate string list metric implementation

--- glean-core/android/src/test/java/mozilla/telemetry/glean/private/StringListMetricTypeTest.kt
+++ glean-core/android/src/test/java/mozilla/telemetry/glean/private/StringListMetricTypeTest.kt
@@ -18,6 +18,7 @@ import mozilla.telemetry.glean.testing.GleanTestRule
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
+import org.junit.Assert.assertNull
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -31,13 +32,13 @@ class StringListMetricTypeTest {
     @Test
     fun `The API saves to its storage engine by first adding then setting`() {
         // Define a 'stringListMetric' string list metric, which will be stored in "store1"
-        val stringListMetric = StringListMetricType(
+        val stringListMetric = StringListMetricType(CommonMetricData(
             disabled = false,
             category = "telemetry",
-            lifetime = Lifetime.Application,
+            lifetime = Lifetime.APPLICATION,
             name = "string_list_metric",
             sendInPings = listOf("store1")
-        )
+        ))
 
         // Record two lists using add and set
         stringListMetric.add("value1")
@@ -45,7 +46,7 @@ class StringListMetricTypeTest {
         stringListMetric.add("value3")
 
         // Check that data was properly recorded.
-        val snapshot = stringListMetric.testGetValue()
+        val snapshot = stringListMetric.testGetValue()!!
         assertEquals(3, snapshot.size)
         assertTrue(stringListMetric.testHasValue())
         assertEquals("value1", snapshot[0])
@@ -55,7 +56,7 @@ class StringListMetricTypeTest {
         // Use set() to see that the first list is replaced by the new list
         stringListMetric.set(listOf("other1", "other2", "other3"))
         // Check that data was properly recorded.
-        val snapshot2 = stringListMetric.testGetValue()
+        val snapshot2 = stringListMetric.testGetValue()!!
         assertEquals(3, snapshot2.size)
         assertTrue(stringListMetric.testHasValue())
         assertEquals("other1", snapshot2[0])
@@ -66,19 +67,19 @@ class StringListMetricTypeTest {
     @Test
     fun `The API saves to its storage engine by first setting then adding`() {
         // Define a 'stringListMetric' string list metric, which will be stored in "store1"
-        val stringListMetric = StringListMetricType(
+        val stringListMetric = StringListMetricType(CommonMetricData(
                 disabled = false,
                 category = "telemetry",
-                lifetime = Lifetime.Application,
+                lifetime = Lifetime.APPLICATION,
                 name = "string_list_metric",
                 sendInPings = listOf("store1")
-        )
+        ))
 
         // Record two lists using set and add
         stringListMetric.set(listOf("value1", "value2", "value3"))
 
         // Check that data was properly recorded.
-        val snapshot = stringListMetric.testGetValue()
+        val snapshot = stringListMetric.testGetValue()!!
         assertEquals(3, snapshot.size)
         assertTrue(stringListMetric.testHasValue())
         assertEquals("value1", snapshot[0])
@@ -88,7 +89,7 @@ class StringListMetricTypeTest {
         // Use add() to see that the list is appended to
         stringListMetric.add("added1")
         // Check that data was properly recorded.
-        val snapshot2 = stringListMetric.testGetValue()
+        val snapshot2 = stringListMetric.testGetValue()!!
         assertEquals(4, snapshot2.size)
         assertTrue(stringListMetric.testHasValue())
         assertEquals("value1", snapshot2[0])
@@ -101,13 +102,13 @@ class StringListMetricTypeTest {
     fun `disabled lists must not record data`() {
         // Define a string list metric which will be stored in "store1".
         // It's disabled so it should not record anything.
-        val stringListMetric = StringListMetricType(
+        val stringListMetric = StringListMetricType(CommonMetricData(
             disabled = true,
             category = "telemetry",
-            lifetime = Lifetime.Application,
+            lifetime = Lifetime.APPLICATION,
             name = "string_list_metric",
             sendInPings = listOf("store1")
-        )
+        ))
 
         // Attempt to store the string list using set.
         stringListMetric.set(listOf("value1", "value2", "value3"))
@@ -122,28 +123,28 @@ class StringListMetricTypeTest {
             stringListMetric.testHasValue())
     }
 
-    @Test(expected = NullPointerException::class)
+    @Test
     fun `testGetValue() throws NullPointerException if nothing is stored`() {
-        val stringListMetric = StringListMetricType(
+        val stringListMetric = StringListMetricType(CommonMetricData(
             disabled = true,
             category = "telemetry",
-            lifetime = Lifetime.Application,
+            lifetime = Lifetime.APPLICATION,
             name = "string_list_metric",
             sendInPings = listOf("store1")
-        )
-        stringListMetric.testGetValue()
+        ))
+        assertNull(stringListMetric.testGetValue())
     }
 
     @Test
     fun `The API saves to secondary pings`() {
         // Define a 'stringListMetric' string list metric, which will be stored in "store1" and "store2"
-        val stringListMetric = StringListMetricType(
+        val stringListMetric = StringListMetricType(CommonMetricData(
             disabled = false,
             category = "telemetry",
-            lifetime = Lifetime.Application,
+            lifetime = Lifetime.APPLICATION,
             name = "string_list_metric",
             sendInPings = listOf("store1", "store2")
-        )
+        ))
 
         // Record two lists using add and set
         stringListMetric.add("value1")
@@ -152,7 +153,7 @@ class StringListMetricTypeTest {
 
         // Check that data was properly recorded in the second ping.
         assertTrue(stringListMetric.testHasValue("store2"))
-        val snapshot = stringListMetric.testGetValue("store2")
+        val snapshot = stringListMetric.testGetValue("store2")!!
         assertEquals(3, snapshot.size)
         assertEquals("value1", snapshot[0])
         assertEquals("value2", snapshot[1])
@@ -162,7 +163,7 @@ class StringListMetricTypeTest {
         stringListMetric.set(listOf("other1", "other2", "other3"))
         // Check that data was properly recorded in the second ping.
         assertTrue(stringListMetric.testHasValue("store2"))
-        val snapshot2 = stringListMetric.testGetValue("store2")
+        val snapshot2 = stringListMetric.testGetValue("store2")!!
         assertEquals(3, snapshot2.size)
         assertEquals("other1", snapshot2[0])
         assertEquals("other2", snapshot2[1])
@@ -172,21 +173,21 @@ class StringListMetricTypeTest {
     @Test
     fun `Long string lists are truncated`() {
         // Define a 'stringListMetric' string list metric, which will be stored in "store1"
-        val stringListMetric = StringListMetricType(
+        val stringListMetric = StringListMetricType(CommonMetricData(
             disabled = false,
             category = "telemetry",
-            lifetime = Lifetime.Application,
+            lifetime = Lifetime.APPLICATION,
             name = "string_list_metric",
             sendInPings = listOf("store1")
-        )
+        ))
 
         for (x in 0..20) {
             stringListMetric.add("value$x")
         }
 
-        val snapshot = stringListMetric.testGetValue("store1")
+        val snapshot = stringListMetric.testGetValue("store1")!!
         assertEquals(20, snapshot.size)
 
-        assertEquals(1, stringListMetric.testGetNumRecordedErrors(ErrorType.InvalidValue))
+        assertEquals(1, stringListMetric.testGetNumRecordedErrors(ErrorType.INVALID_VALUE))
     }
 }
