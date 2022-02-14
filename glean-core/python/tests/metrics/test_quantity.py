21919c399818850bc8b1924f6768d604dac742ae
Jan-Erik Rediger <jrediger@mozilla.com>
2022-02-14 11:52:13 +0100

    [Python] Migrate quantity metric to UniFFI

--- glean-core/python/tests/metrics/test_quantity.py
+++ glean-core/python/tests/metrics/test_quantity.py
@@ -2,113 +2,117 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-
-import pytest
-
-
 from glean import metrics
-from glean.metrics import Lifetime
+from glean.metrics import Lifetime, CommonMetricData
 from glean import testing
 
 
 def test_the_api_saves_to_its_storage_engine():
     # Define a quantity metric, which will be stored in "store1"
     quantity_metric = metrics.QuantityMetricType(
-        disabled=False,
-        category="telemetry",
-        lifetime=Lifetime.APPLICATION,
-        name="quantity_metric",
-        send_in_pings=["store1"],
+        CommonMetricData(
+            disabled=False,
+            category="telemetry",
+            lifetime=Lifetime.APPLICATION,
+            name="quantity_metric",
+            send_in_pings=["store1"],
+            dynamic_label=None,
+        )
     )
 
-    assert quantity_metric.test_has_value() is False
+    assert quantity_metric.test_get_value() is None
 
     quantity_metric.set(1)
 
     # Check that the metric was properly recorded
-    assert quantity_metric.test_has_value() is True
     assert 1 == quantity_metric.test_get_value()
 
     quantity_metric.set(10)
 
     # Check that the metric was properly overwritten
-    assert quantity_metric.test_has_value() is True
     assert 10 == quantity_metric.test_get_value()
 
 
 def test_disabled_quantities_must_not_record_data():
     # Define a quantity metric, which will be stored in "store1"
     quantity_metric = metrics.QuantityMetricType(
-        disabled=True,
-        category="telemetry",
-        lifetime=Lifetime.APPLICATION,
-        name="quantity_metric",
-        send_in_pings=["store1"],
+        CommonMetricData(
+            disabled=True,
+            category="telemetry",
+            lifetime=Lifetime.APPLICATION,
+            name="quantity_metric",
+            send_in_pings=["store1"],
+            dynamic_label=None,
+        )
     )
 
     # Attempt to increment the quantity
     quantity_metric.set(1)
     # Check that nothing was recorded
-    assert quantity_metric.test_has_value() is False
+    assert quantity_metric.test_get_value() is None
 
 
 def test_get_value_throws_value_error_if_nothing_is_stored():
     # Define a quantity metric, which will be stored in "store1"
     quantity_metric = metrics.QuantityMetricType(
-        disabled=True,
-        category="telemetry",
-        lifetime=Lifetime.APPLICATION,
-        name="quantity_metric",
-        send_in_pings=["store1"],
+        CommonMetricData(
+            disabled=True,
+            category="telemetry",
+            lifetime=Lifetime.APPLICATION,
+            name="quantity_metric",
+            send_in_pings=["store1"],
+            dynamic_label=None,
+        )
     )
 
-    with pytest.raises(ValueError):
-        quantity_metric.test_get_value()
+    assert not quantity_metric.test_get_value()
 
 
 def test_api_saves_to_secondary_pings():
     # Define a quantity metric, which will be stored in "store1" and "store2"
     quantity_metric = metrics.QuantityMetricType(
-        disabled=False,
-        category="telemetry",
-        lifetime=Lifetime.APPLICATION,
-        name="quantity_metric",
-        send_in_pings=["store1", "store2"],
+        CommonMetricData(
+            disabled=False,
+            category="telemetry",
+            lifetime=Lifetime.APPLICATION,
+            name="quantity_metric",
+            send_in_pings=["store1", "store2"],
+            dynamic_label=None,
+        )
     )
 
     quantity_metric.set(1)
 
     # Check that the metric was properly recorded on the second ping
-    assert quantity_metric.test_has_value("store2")
     assert 1 == quantity_metric.test_get_value("store2")
 
     quantity_metric.set(10)
 
     # Check that the metric was properly overwritten on the second ping
-    assert quantity_metric.test_has_value("store2")
     assert 10 == quantity_metric.test_get_value("store2")
 
 
 def test_negative_values_are_not_counted():
     # Define a quantity metric, which will be stored in "store1"
     quantity_metric = metrics.QuantityMetricType(
-        disabled=False,
-        category="telemetry",
-        lifetime=Lifetime.APPLICATION,
-        name="quantity_metric",
-        send_in_pings=["store1"],
+        CommonMetricData(
+            disabled=False,
+            category="telemetry",
+            lifetime=Lifetime.APPLICATION,
+            name="quantity_metric",
+            send_in_pings=["store1"],
+            dynamic_label=None,
+        )
     )
 
     quantity_metric.set(1)
 
     # Check that the metric was properly recorded
-    assert quantity_metric.test_has_value("store1")
     assert 1 == quantity_metric.test_get_value("store1")
 
     quantity_metric.set(-10)
 
     # Check that the quantity was NOT recorded
-    assert quantity_metric.test_has_value("store1")
     assert 1 == quantity_metric.test_get_value("store1")
     assert 1 == quantity_metric.test_get_num_recorded_errors(
         testing.ErrorType.INVALID_VALUE
