d2ebe3e3fde4dc2b865376d3b2ff8b4121253d47
Jan-Erik Rediger <jrediger@mozilla.com>
2022-02-14 11:52:13 +0100

    [Python] Re-export all metric types again from metrics module

--- glean-core/python/glean/metrics/__init__.py
+++ glean-core/python/glean/metrics/__init__.py
@@ -7,14 +7,58 @@
 This module contains all of the metric types.
 """
 
-
-from .._uniffi import CounterMetric as CounterMetricType
+# Re-export utilities
 from .._uniffi import CommonMetricData
 from .._uniffi import Lifetime
+from .._uniffi import MemoryUnit
+from .._uniffi import TimeUnit
+from .._uniffi import RecordedExperiment
+
+# Re-export some metrics directly
+from .._uniffi import BooleanMetric as BooleanMetricType
+from .._uniffi import CounterMetric as CounterMetricType
+from .._uniffi import MemoryDistributionMetric as MemoryDistributionMetricType
+from .._uniffi import QuantityMetric as QuantityMetricType
+from .._uniffi import StringListMetric as StringListMetricType
+
+# Export wrapper implementations for metric types
+from .datetime import DatetimeMetricType
+from .event import EventMetricType, EventExtras, RecordedEvent
+from .labeled import (
+    LabeledBooleanMetricType,
+    LabeledCounterMetricType,
+    LabeledStringMetricType,
+)
+from .ping import PingType
+from .string import StringMetricType
+from .timespan import TimespanMetricType
+from .timing_distribution import TimingDistributionMetricType
+from .url import UrlMetricType
+from .uuid import UuidMetricType
 
 
 __all__ = [
+    "BooleanMetricType",
     "CommonMetricData",
     "CounterMetricType",
+    "DatetimeMetricType",
+    "EventMetricType",
+    "QuantityMetricType",
+    "LabeledBooleanMetricType",
+    "LabeledCounterMetricType",
+    "LabeledStringMetricType",
     "Lifetime",
+    "MemoryDistributionMetricType",
+    "MemoryUnit",
+    "PingType",
+    "RecordedEvent",
+    "EventExtras",
+    "RecordedExperiment",
+    "StringMetricType",
+    "StringListMetricType",
+    "TimespanMetricType",
+    "TimeUnit",
+    "TimingDistributionMetricType",
+    "UrlMetricType",
+    "UuidMetricType",
 ]
