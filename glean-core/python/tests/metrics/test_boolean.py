58e0774e9fa9d9993af5ad4b9e8679e4c276f315
Jan-Erik Rediger <jrediger@mozilla.com>
2022-02-14 11:52:13 +0100

    [Python] Migrate boolean metric to UniFFI

--- glean-core/python/tests/metrics/test_boolean.py
+++ glean-core/python/tests/metrics/test_boolean.py
@@ -2,76 +2,79 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-
-import pytest
-
-
 from glean import metrics
-from glean.metrics import Lifetime
+from glean.metrics import Lifetime, CommonMetricData
 
 
 def test_the_api_saves_to_its_storage_engine():
     boolean_metric = metrics.BooleanMetricType(
-        disabled=False,
-        category="telemetry",
-        lifetime=Lifetime.APPLICATION,
-        name="boolean_metric",
-        send_in_pings=["store1"],
+        CommonMetricData(
+            disabled=False,
+            category="telemetry",
+            lifetime=Lifetime.APPLICATION,
+            name="boolean_metric",
+            send_in_pings=["store1"],
+            dynamic_label=None,
+        )
     )
 
     boolean_metric.set(True)
 
-    assert boolean_metric.test_has_value()
     assert True is boolean_metric.test_get_value()
 
     boolean_metric.set(False)
 
-    assert boolean_metric.test_has_value()
     assert False is boolean_metric.test_get_value()
 
 
 def test_disabled_booleans_must_not_record_data():
     boolean_metric = metrics.BooleanMetricType(
-        disabled=True,
-        category="telemetry",
-        lifetime=Lifetime.APPLICATION,
-        name="boolean_metric",
-        send_in_pings=["store1"],
+        CommonMetricData(
+            disabled=True,
+            category="telemetry",
+            lifetime=Lifetime.APPLICATION,
+            name="boolean_metric",
+            send_in_pings=["store1"],
+            dynamic_label=None,
+        )
     )
 
     boolean_metric.set(True)
 
-    assert not boolean_metric.test_has_value()
+    assert not boolean_metric.test_get_value()
 
 
 def test_get_value_throws_if_nothing_is_stored():
     boolean_metric = metrics.BooleanMetricType(
-        disabled=True,
-        category="telemetry",
-        lifetime=Lifetime.APPLICATION,
-        name="boolean_metric",
-        send_in_pings=["store1"],
+        CommonMetricData(
+            disabled=True,
+            category="telemetry",
+            lifetime=Lifetime.APPLICATION,
+            name="boolean_metric",
+            send_in_pings=["store1"],
+            dynamic_label=None,
+        )
     )
 
-    with pytest.raises(ValueError):
-        boolean_metric.test_get_value()
+    assert not boolean_metric.test_get_value()
 
 
 def test_the_api_saves_to_secondary_pings():
     boolean_metric = metrics.BooleanMetricType(
-        disabled=False,
-        category="telemetry",
-        lifetime=Lifetime.APPLICATION,
-        name="boolean_metric",
-        send_in_pings=["store1", "store2"],
+        CommonMetricData(
+            disabled=False,
+            category="telemetry",
+            lifetime=Lifetime.APPLICATION,
+            name="boolean_metric",
+            send_in_pings=["store1", "store2"],
+            dynamic_label=None,
+        )
     )
 
     boolean_metric.set(True)
 
-    assert boolean_metric.test_has_value("store2")
     assert True is boolean_metric.test_get_value("store2")
 
     boolean_metric.set(False)
 
-    assert boolean_metric.test_has_value("store2")
     assert False is boolean_metric.test_get_value("store2")
