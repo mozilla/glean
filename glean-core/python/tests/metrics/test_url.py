9e56af678186ded1d0940aef2dad93930b365b36
Jan-Erik Rediger <jrediger@mozilla.com>
2022-02-14 11:52:13 +0100

    [Python] Migrate url metric to UniFFI

--- glean-core/python/tests/metrics/test_url.py
+++ glean-core/python/tests/metrics/test_url.py
@@ -4,71 +4,79 @@
 
 
 from glean import metrics
-from glean.metrics import Lifetime
+from glean.metrics import Lifetime, CommonMetricData
 from glean import testing
 
 
 def test_the_api_saves_to_its_storage_engine():
     url_metric = metrics.UrlMetricType(
-        disabled=False,
-        category="telemetry",
-        lifetime=Lifetime.APPLICATION,
-        name="url_metric",
-        send_in_pings=["store1"],
+        CommonMetricData(
+            disabled=False,
+            category="telemetry",
+            lifetime=Lifetime.APPLICATION,
+            name="url_metric",
+            send_in_pings=["store1"],
+            dynamic_label=None,
+        )
     )
 
     url_metric.set("glean://testing")
 
-    assert url_metric.test_has_value()
     assert "glean://testing" == url_metric.test_get_value()
 
     url_metric.set("glean://overriddenValue")
 
-    assert url_metric.test_has_value()
     assert "glean://overriddenValue" == url_metric.test_get_value()
 
 
 def test_disabled_urls_must_not_record_data():
     url_metric = metrics.UrlMetricType(
-        disabled=True,
-        category="telemetry",
-        lifetime=Lifetime.APPLICATION,
-        name="url_metric",
-        send_in_pings=["store1"],
+        CommonMetricData(
+            disabled=True,
+            category="telemetry",
+            lifetime=Lifetime.APPLICATION,
+            name="url_metric",
+            send_in_pings=["store1"],
+            dynamic_label=None,
+        )
     )
 
     url_metric.set("glean://testing")
 
-    assert not url_metric.test_has_value()
+    assert not url_metric.test_get_value()
 
 
 def test_the_api_saves_to_secondary_pings():
     url_metric = metrics.UrlMetricType(
-        disabled=False,
-        category="telemetry",
-        lifetime=Lifetime.APPLICATION,
-        name="url_metric",
-        send_in_pings=["store1", "store2"],
+        CommonMetricData(
+            disabled=False,
+            category="telemetry",
+            lifetime=Lifetime.APPLICATION,
+            name="url_metric",
+            send_in_pings=["store1", "store2"],
+            dynamic_label=None,
+        )
     )
 
     url_metric.set("glean://value")
 
-    assert url_metric.test_has_value("store2")
     assert "glean://value" == url_metric.test_get_value("store2")
 
     url_metric.set("glean://overriddenValue")
 
-    assert url_metric.test_has_value("store2")
     assert "glean://overriddenValue" == url_metric.test_get_value("store2")
 
 
 def test_setting_a_long_url_records_an_error():
     url_metric = metrics.UrlMetricType(
-        disabled=False,
-        category="telemetry",
-        lifetime=Lifetime.APPLICATION,
-        name="url_metric",
-        send_in_pings=["store1", "store2"],
+        CommonMetricData(
+            disabled=False,
+            category="telemetry",
+            lifetime=Lifetime.APPLICATION,
+            name="url_metric",
+            send_in_pings=["store1", "store2"],
+            dynamic_label=None,
+        )
     )
 
     url_metric.set("glean://" + "testing" * 2000)
@@ -80,13 +88,16 @@ def test_setting_a_long_url_records_an_error():
 
 def test_setting_a_url_as_none():
     url_metric = metrics.UrlMetricType(
-        disabled=False,
-        category="telemetry",
-        lifetime=Lifetime.APPLICATION,
-        name="url_metric",
-        send_in_pings=["store1", "store2"],
+        CommonMetricData(
+            disabled=False,
+            category="telemetry",
+            lifetime=Lifetime.APPLICATION,
+            name="url_metric",
+            send_in_pings=["store1", "store2"],
+            dynamic_label=None,
+        )
     )
 
     url_metric.set(None)
 
-    assert not url_metric.test_has_value()
+    assert not url_metric.test_get_value()
