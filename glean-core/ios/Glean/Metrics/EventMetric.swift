9441e7debeb2a17b605740275ec910e79811ea14
Jan-Erik Rediger <jrediger@mozilla.com>
2022-02-21 12:56:23 +0100

    [Swift] Migrate event metric to UniFFI

--- glean-core/ios/Glean/Metrics/EventMetric.swift
+++ glean-core/ios/Glean/Metrics/EventMetric.swift
@@ -2,35 +2,13 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import Foundation
-
-/// Deserialized event data.
-public struct RecordedEventData {
-    /// The event's category, part of the full identifier
-    let category: String
-    /// The event's name, part of the full identifier
-    let name: String
-    /// The event's timestamp
-    let timestamp: UInt64
-    /// Any extra data recorded for the event
-    let extra: [String: String]?
-
-    var identifier: String {
-        if category.isEmpty {
-            return name
-        } else {
-            return "\(category).\(name)"
-        }
-    }
-}
-
-/// Extra keys for events.
-/// Extra keys can be of any type, but need to adhere to a protocol.
+/// The ability to convert an enum key back to its string representation.
+/// This is necessary because Glean-generated enum values are camelCased,
+/// so an enum's `name` property doesn't match the allowed key.
 ///
-/// For user-defined `EventMetricType`s these will be defined as `enums`.
-public protocol ExtraKeys: Hashable {
-    /// The index of the extra key, used to index the string array passed at `EventMetricType` instantiation.
-    func index() -> Int32
+/// This is automatically implemented for generated enums.
+public protocol EventExtraKey: Hashable {
+    func keyName() throws -> String
 }
 
 /// Extra keys for events by name.
@@ -43,16 +21,16 @@ public protocol EventExtras {
     /// 1. The list of extra key indices.
     ///    Unset keys will be skipped.
     /// 2. The list of extra values.
-    func toFfiExtra() -> ([Int32], [String])
+    func toExtraRecord() -> [String: String]
 }
 
 /// Default of no extra keys for events.
 ///
 /// An enum with no values for convenient use as the default set of extra keys
 /// that an `EventMetricType` can accept.
-public enum NoExtraKeys: ExtraKeys {
-    public func index() -> Int32 {
-        return 0
+public enum NoExtraKeys: EventExtraKey {
+    public func keyName() throws -> String {
+        throw "can't serialize this key"
     }
 }
 
@@ -61,25 +39,21 @@ public enum NoExtraKeys: ExtraKeys {
 /// An empty class for convenient use as the default set of extra keys
 /// that an `EventMetricType` can accept.
 public class NoExtras: EventExtras {
-    public func toFfiExtra() -> ([Int32], [String]) {
-        return ([Int32](), [String]())
+    public func toExtraRecord() -> [String: String] {
+        return [String: String]()
     }
 }
 
 /// Default-implement `EventExtras` for a map of ExtraKeys to Strings.
-extension Dictionary: EventExtras where Key: ExtraKeys, Value == String {
-    public func toFfiExtra() -> ([Int32], [String]) {
-        var keys = [Int32]()
-        var values = [String]()
-        keys.reserveCapacity(self.count)
-        values.reserveCapacity(self.count)
+extension Dictionary: EventExtras where Key: EventExtraKey, Value == String {
+    public func toExtraRecord() -> [String: String] {
+        var record = [String: String]()
 
         for (key, value) in self {
-            keys.append(key.index())
-            values.append(value)
+            record[try! key.keyName()] = value
         }
 
-        return (keys, values)
+        return record
     }
 }
 
@@ -90,43 +64,12 @@ extension Dictionary: EventExtras where Key: ExtraKeys, Value == String {
 ///
 /// The Events API only exposes the `EventMetricType.record(extra:)` method, which takes care of validating the input
 /// data and making sure that limits are enforced.
-public class EventMetricType<ExtraKeysEnum: ExtraKeys, ExtraObject: EventExtras> {
-    let handle: UInt64
-    let disabled: Bool
-    let sendInPings: [String]
+public class EventMetricType<ExtraKeysEnum: EventExtraKey, ExtraObject: EventExtras> {
+    let inner: EventMetric
 
     /// The public constructor used by automatically generated metrics.
-    public init(
-        category: String,
-        name: String,
-        sendInPings: [String],
-        lifetime: Lifetime,
-        disabled: Bool,
-        allowedExtraKeys: [String]? = nil
-    ) {
-        self.disabled = disabled
-        self.sendInPings = sendInPings
-        self.handle = withArrayOfCStrings(sendInPings) { pingArray in
-            withArrayOfCStrings(allowedExtraKeys) { allowedExtraKeys in
-                glean_new_event_metric(
-                    category,
-                    name,
-                    pingArray,
-                    Int32(sendInPings.count),
-                    lifetime.rawValue,
-                    disabled ? 1 : 0,
-                    allowedExtraKeys,
-                    Int32(allowedExtraKeys?.count ?? 0)
-                )
-            }
-        }
-    }
-
-    /// Destroy this metric.
-    deinit {
-        if self.handle != 0 {
-            glean_destroy_event_metric(self.handle)
-        }
+    public init(_ meta: CommonMetricData, _ allowedExtraKeys: [String]? = nil) {
+        self.inner = EventMetric(meta, allowedExtraKeys ?? [])
     }
 
     /// Record an event by using the information provided by the instance of this class.
@@ -138,130 +81,27 @@ public class EventMetricType<ExtraKeysEnum: ExtraKeys, ExtraObject: EventExtras>
     ///              The maximum length for values is defined by `MAX_LENGTH_EXTRA_KEY_VALUE`.
     @available(*, deprecated, message: "Specify types for your event extras. See the reference for details.")
     public func record(extra: [ExtraKeysEnum: String]) {
-        let timestamp = glean_get_timestamp_ms()
-        self.recordInternal(timestamp: timestamp, extras: extra.toFfiExtra())
+        self.recordInternal(extras: extra.toExtraRecord())
     }
 
     public func record(_ properties: ExtraObject? = nil) {
-        // We capture the event time now, since we don't know when the async code below
-        // might get executed.
-        let timestamp = glean_get_timestamp_ms()
-        self.recordInternal(timestamp: timestamp, extras: properties.map { $0.toFfiExtra() })
+        self.recordInternal(extras: properties.map { $0.toExtraRecord() } ?? [:])
     }
 
-    func recordInternal(timestamp: UInt64, extras: ([Int32], [String])?) {
-        guard !self.disabled else { return }
-
-        Dispatchers.shared.launchAPI {
-            // The map is sent over FFI as a pair of arrays, one containing the
-            // keys, and the other containing the values.
-            var len = 0
-            var keys: [Int32]?
-            var values: [String]?
-
-            if let extra = extras {
-                keys = extra.0
-                values = extra.1
-                len = extra.0.count
-            }
-
-            withArrayOfCStrings(values) { values in
-                glean_event_record(
-                    self.handle,
-                    timestamp,
-                    keys,
-                    values,
-                    Int32(len)
-                )
-            }
-        }
-    }
-
-    /// Tests whether a value is stored for the metric for testing purposes only. This function will
-    /// attempt to await the last task (if any) writing to the the metric's storage engine before
-    /// returning a value.
-    ///
-    /// - parameters:
-    ///     * pingName: represents the name of the ping to retrieve the metric for.
-    ///                 Defaults to the first value in `sendInPings`.
-    /// - returns: true if metric value exists, otherwise false
-    public func testHasValue(_ pingName: String? = nil) -> Bool {
-        Dispatchers.shared.assertInTestingMode()
-
-        let pingName = pingName ?? self.sendInPings[0]
-        return glean_event_test_has_value(self.handle, pingName).toBool()
-    }
-
-    /// Deserializes an event in JSON into a RecordedEventData object.
-    ///
-    /// - parameters:
-    ///     * jsonContent: The JSONObject containing the data for the event. It is in
-    ///       the same format as an event sent in a ping, and has the following entries:
-    ///         - timestamp (Int)
-    ///         - category (String): The category of the event metric
-    ///         - name (String): The name of the event metric
-    ///         - extra ([String: String]?): Map of extra key/value pairs
-    /// - returns: `RecordedEventData` representing the event data
-    private func deserializeEvent(_ jsonContent: [String: Any]) -> RecordedEventData? {
-        guard let category = jsonContent["category"] as? String else { return nil }
-        guard let name = jsonContent["name"] as? String else { return nil }
-        guard let timestamp = jsonContent["timestamp"] as? UInt64 else { return nil }
-        var extra: [String: String]?
-
-        if let extraObj = jsonContent["extra"] {
-            let extraObj = extraObj as? [String: String]
-            extra = extraObj
-        }
-
-        return RecordedEventData(category: category, name: name, timestamp: timestamp, extra: extra)
+    func recordInternal(extras: [String: String]) {
+        self.inner.record(extras)
     }
 
     /// Returns the stored value for testing purposes only. This function will attempt to await the
     /// last task (if any) writing to the the metric's storage engine before returning a value.
     ///
-    /// Throws a "Missing value" exception if no value is stored
-    ///
     /// - parameters:
     ///     * pingName: represents the name of the ping to retrieve the metric for.
     ///                 Defaults to the first value in `sendInPings`.
     ///
-    /// - returns:  value of the stored metric
-    public func testGetValue(_ pingName: String? = nil) throws -> [RecordedEventData] {
-        Dispatchers.shared.assertInTestingMode()
-
-        let pingName = pingName ?? self.sendInPings[0]
-
-        if !testHasValue(pingName) {
-            throw "Missing value"
-        }
-
-        let res = String(
-            freeingGleanString: glean_event_test_get_value_as_json_string(self.handle, pingName)
-        )
-
-        do {
-            let data = res.data(using: .utf8)!
-            // swiftlint:disable force_cast
-            let jsonRes = try JSONSerialization.jsonObject(with: data, options: []) as! [Any]
-            if jsonRes.isEmpty {
-                throw "Missing value"
-            }
-
-            var result = [RecordedEventData]()
-            for element in jsonRes {
-                if let event = element as? [String: Any] {
-                    if let event = deserializeEvent(event) {
-                        result.append(event)
-                    } else {
-                        throw "Missing value"
-                    }
-                }
-            }
-
-            return result
-        } catch {
-            throw "Missing value"
-        }
+    /// - returns:  value of the stored metric or `nil` if nothing was recorded.
+    public func testGetValue(_ pingName: String? = nil) -> [RecordedEvent]? {
+        return self.inner.testGetValue()
     }
 
     /// Returns the number of errors recorded for the given metric.
@@ -273,14 +113,6 @@ public class EventMetricType<ExtraKeysEnum: ExtraKeys, ExtraObject: EventExtras>
     ///
     /// - returns: The number of errors recorded for the metric for the given error type.
     public func testGetNumRecordedErrors(_ errorType: ErrorType, pingName: String? = nil) -> Int32 {
-        Dispatchers.shared.assertInTestingMode()
-
-        let pingName = pingName ?? self.sendInPings[0]
-
-        return glean_event_test_get_num_recorded_errors(
-            self.handle,
-            errorType.rawValue,
-            pingName
-        )
+        return self.inner.testGetNumRecordedErrors(errorType, pingName)
     }
 }
