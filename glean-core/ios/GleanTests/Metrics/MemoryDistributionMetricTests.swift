01f64f4193ff73a9ce970ae330348098d4dc66f5
Jan-Erik Rediger <jrediger@mozilla.com>
2022-02-21 12:56:24 +0100

    [Swift] Migrate memory distribution metric to UniFFI

--- glean-core/ios/GleanTests/Metrics/MemoryDistributionMetricTests.swift
+++ glean-core/ios/GleanTests/Metrics/MemoryDistributionMetricTests.swift
@@ -5,8 +5,6 @@
 @testable import Glean
 import XCTest
 
-// swiftlint:disable force_cast
-// REASON: Used in a test
 class MemoryDistributionTypeTests: XCTestCase {
     override func setUp() {
         resetGleanDiscardingInitialPings(testCase: self, tag: "MemoryDistributionTypeTests")
@@ -17,129 +15,125 @@ class MemoryDistributionTypeTests: XCTestCase {
     }
 
     func testTiminingDistributionSavesToStorage() {
-        let metric = MemoryDistributionMetricType(
+        let metric = MemoryDistributionMetricType(CommonMetricData(
             category: "telemetry",
             name: "memory_distribution",
             sendInPings: ["store1"],
             lifetime: .ping,
-            disabled: false,
-            memoryUnit: .kilobyte
+            disabled: false
+            ), .kilobyte
         )
 
         // Accumulate a few values
-        for i in UInt64(1) ... 3 {
+        for i in Int64(1) ... 3 {
             metric.accumulate(i)
         }
 
-        let kb = UInt64(1024)
+        let kb = Int64(1024)
 
         // Check that data was properly recorded.
         // We can only check the count, as we don't control the time.
-        XCTAssert(metric.testHasValue())
-        let snapshot = try! metric.testGetValue()
-        XCTAssertEqual(3, snapshot.count)
+        let snapshot = metric.testGetValue()!
+        let sum = snapshot.values.values.reduce(0, +)
+        XCTAssertEqual(3, sum)
 
         // Check the sum
         XCTAssertEqual(1 * kb + 2 * kb + 3 * kb, snapshot.sum)
         // Check that the 1L fell into the first value bucket
-        XCTAssertEqual(1, snapshot.values[1023])
+        XCTAssertEqual(1, snapshot.values["1023"])
         // Check that the 2L fell into the second value bucket
-        XCTAssertEqual(1, snapshot.values[2047])
+        XCTAssertEqual(1, snapshot.values["2047"])
         // Check that the 3L fell into the third value bucket
-        XCTAssertEqual(1, snapshot.values[3024])
+        XCTAssertEqual(1, snapshot.values["3024"])
     }
 
     func testMemoryDistributionValuesAreTruncatedTo1Tb() {
-        let metric = MemoryDistributionMetricType(
+        let metric = MemoryDistributionMetricType(CommonMetricData(
             category: "telemetry",
             name: "memory_distribution",
             sendInPings: ["store1"],
             lifetime: .ping,
-            disabled: false,
-            memoryUnit: .gigabyte
+            disabled: false
+            ), .gigabyte
         )
 
         metric.accumulate(2048)
 
-        let snapshot = try! metric.testGetValue()
+        let snapshot = metric.testGetValue()!
 
         // Check the sum
         XCTAssertEqual(1 << 40, snapshot.sum)
         // Check that the 1L fell into the first value bucket
-        XCTAssertEqual(1, snapshot.values[(1 << 40) - 1])
+        XCTAssertEqual(1, snapshot.values["\((1 << 40) - 1)"])
         // Check that an error was recorded
         XCTAssertEqual(1, metric.testGetNumRecordedErrors(.invalidValue))
     }
 
     func testMemoryDistributionMustNotRecordIfDisabled() {
-        let metric = MemoryDistributionMetricType(
+        let metric = MemoryDistributionMetricType(CommonMetricData(
             category: "telemetry",
             name: "memory_distribution",
             sendInPings: ["store1"],
             lifetime: .ping,
-            disabled: true,
-            memoryUnit: .kilobyte
+            disabled: true
+            ), .kilobyte
         )
 
         metric.accumulate(1)
-        XCTAssertFalse(metric.testHasValue())
+        XCTAssertNil(metric.testGetValue())
     }
 
     func testMemoryDistributionGetValueThrowsExceptionIfNothingIsStored() {
-        let metric = MemoryDistributionMetricType(
+        let metric = MemoryDistributionMetricType(CommonMetricData(
             category: "telemetry",
             name: "memory_distribution",
             sendInPings: ["store1"],
             lifetime: .application,
-            disabled: false,
-            memoryUnit: .kilobyte
+            disabled: false
+            ), .kilobyte
         )
 
-        XCTAssertThrowsError(try metric.testGetValue()) { error in
-            XCTAssertEqual(error as! String, "Missing value")
-        }
+        XCTAssertNil(metric.testGetValue())
     }
 
     func testMemoryDistributionSavesToSecondaryPings() {
         // Define a memory distribution metric which will be stored in multiple stores
-        let metric = MemoryDistributionMetricType(
+        let metric = MemoryDistributionMetricType(CommonMetricData(
             category: "telemetry",
             name: "memory_distribution",
             sendInPings: ["store1", "store2", "store3"],
             lifetime: .application,
-            disabled: false,
-            memoryUnit: .kilobyte
+            disabled: false
+            ), .kilobyte
         )
 
         // Accumulate a few values
-        for i in UInt64(1) ... 3 {
+        for i in Int64(1) ... 3 {
             metric.accumulate(i)
         }
 
         // Check that data was properly recorded in the second ping.
-        XCTAssert(metric.testHasValue("store2"))
-        var snapshot = try! metric.testGetValue("store2")
+        var snapshot = metric.testGetValue("store2")!
 
         // Check the sum
         XCTAssertEqual(6144, snapshot.sum)
         // Check that the 1L fell into the first value bucket
-        XCTAssertEqual(1, snapshot.values[1023])
+        XCTAssertEqual(1, snapshot.values["1023"])
         // Check that the 2L fell into the second value bucket
-        XCTAssertEqual(1, snapshot.values[2047])
+        XCTAssertEqual(1, snapshot.values["2047"])
         // Check that the 3L fell into the third value bucket
-        XCTAssertEqual(1, snapshot.values[3024])
+        XCTAssertEqual(1, snapshot.values["3024"])
 
         // Check that data was properly recorded in the second ping.
-        XCTAssert(metric.testHasValue("store3"))
-        snapshot = try! metric.testGetValue("store3")
+        snapshot = metric.testGetValue("store3")!
 
         // Check the sum
         XCTAssertEqual(6144, snapshot.sum)
         // Check that the 1L fell into the first value bucket
-        XCTAssertEqual(1, snapshot.values[1023])
+        XCTAssertEqual(1, snapshot.values["1023"])
         // Check that the 2L fell into the second value bucket
-        XCTAssertEqual(1, snapshot.values[2047])
+        XCTAssertEqual(1, snapshot.values["2047"])
         // Check that the 3L fell into the third value bucket
-        XCTAssertEqual(1, snapshot.values[3024])
+        XCTAssertEqual(1, snapshot.values["3024"])
     }
 }
