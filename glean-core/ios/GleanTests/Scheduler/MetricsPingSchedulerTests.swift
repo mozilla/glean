3904ffbdd409fdc930d4be22e8963b277e9b30d1
Jan-Erik Rediger <jrediger@mozilla.com>
2022-02-21 12:56:24 +0100

    glean-core/ios/GleanTests/Scheduler/MetricsPingSchedulerTests.swift: unfinished

--- glean-core/ios/GleanTests/Scheduler/MetricsPingSchedulerTests.swift
+++ glean-core/ios/GleanTests/Scheduler/MetricsPingSchedulerTests.swift
@@ -2,6 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+/*
 @testable import Glean
 import XCTest
 
@@ -356,3 +357,4 @@ class MetricsPingSchedulerTests: XCTestCase {
         waitForExpectations(timeout: 10.0)
     }
 }
+*/
