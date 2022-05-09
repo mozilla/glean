e2a153cf8b06f5664777d00e52899731ff1d6499
Jan-Erik Rediger <jrediger@mozilla.com>
2022-05-09 19:15:21 +0200

    Reformat

--- glean-core/rlb/src/private/mod.rs
+++ glean-core/rlb/src/private/mod.rs
@@ -7,18 +7,14 @@
 mod event;
 mod ping;
 
+pub use event::EventMetric;
 pub use glean_core::BooleanMetric;
 pub use glean_core::CounterMetric;
 pub use glean_core::CustomDistributionMetric;
-pub use glean_core::UuidMetric;
-pub use glean_core::{Datetime, DatetimeMetric};
 pub use glean_core::DenominatorMetric;
-pub use event::EventMetric;
 pub use glean_core::MemoryDistributionMetric;
-pub use glean_core::{AllowLabeled, LabeledMetric};
 pub use glean_core::NumeratorMetric;
 pub use glean_core::QuantityMetric;
-pub use ping::PingType;
 pub use glean_core::RateMetric;
 pub use glean_core::RecordedExperiment;
 pub use glean_core::StringListMetric;
@@ -26,3 +22,7 @@ pub use glean_core::StringMetric;
 pub use glean_core::TimespanMetric;
 pub use glean_core::TimingDistributionMetric;
 pub use glean_core::UrlMetric;
+pub use glean_core::UuidMetric;
+pub use glean_core::{AllowLabeled, LabeledMetric};
+pub use glean_core::{Datetime, DatetimeMetric};
+pub use ping::PingType;
