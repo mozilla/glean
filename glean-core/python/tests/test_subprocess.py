27387560d4683a7a590906fdb092ad794a1141da
Tooru Fujisawa <arai_a@mac.com>
2022-05-24 04:43:02 +0900

    Bug 1770791 - Part 1: Do not create FdLogger instance if already created.

---
+++ glean-core/python/tests/test_subprocess.py
@@ -0,0 +1,12 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
+def test_subprocess_works():
+    import glean  # noqa: F401
+    import subprocess
+
+    # Importing glean shouldn't affect subprocess.
+    output = subprocess.check_output(["echo", "hello"]).decode().strip()
+    assert output == "hello"
