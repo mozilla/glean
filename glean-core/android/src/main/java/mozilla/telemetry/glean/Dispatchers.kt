75ff90e3536d7d2e484a848c06b6a3fc7e376754
Jan-Erik Rediger <jrediger@mozilla.com>
2022-06-23 22:14:46 +0200

    Put Glean initialization on a async Kotlin task
    This should delay loading the bundled library, which can be quite time
    consuming itself.

---
+++ glean-core/android/src/main/java/mozilla/telemetry/glean/Dispatchers.kt
@@ -0,0 +1,62 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package mozilla.telemetry.glean
+
+import androidx.annotation.VisibleForTesting
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.ObsoleteCoroutinesApi
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.newSingleThreadContext
+import kotlinx.coroutines.runBlocking
+
+@ObsoleteCoroutinesApi
+internal object Dispatchers {
+    class WaitableCoroutineScope(private val coroutineScope: CoroutineScope) {
+        // When true, jobs will be run synchronously
+        internal var testingMode = false
+
+        /**
+         * Enable testing mode, which makes all of the Glean SDK public API
+         * synchronous.
+         *
+         * @param enabled whether or not to enable the testing mode
+         */
+        @VisibleForTesting(otherwise = VisibleForTesting.NONE)
+        fun setTestingMode(enabled: Boolean) {
+            testingMode = enabled
+        }
+
+        /**
+         * Helper function to execute the task as an asynchronous operation.
+         */
+        internal fun executeTask(block: suspend CoroutineScope.() -> Unit): Job? {
+            return when {
+                testingMode -> {
+                    runBlocking {
+                        block()
+                    }
+                    null
+                }
+                else -> coroutineScope.launch(block = block)
+            }
+        }
+    }
+
+    // This job is used to make sure the API `CoroutineContext` does not cancel
+    // children jobs when exceptions are thrown in children coroutines.
+    private val supervisorJob = SupervisorJob()
+
+    /**
+     * A coroutine scope to make it easy to dispatch API calls off the main thread.
+     * This needs to be a `var` so that our tests can override this.
+     */
+    var API = WaitableCoroutineScope(
+        CoroutineScope(
+            newSingleThreadContext("GleanAPIPool") + supervisorJob
+        )
+    )
+}
