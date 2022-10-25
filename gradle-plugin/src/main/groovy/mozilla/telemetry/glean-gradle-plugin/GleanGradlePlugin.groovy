2fd4d79bfa13423ac5b1df00f1d83463a7fb0057
Travis Long <tlong@mozilla.com>
2022-10-25 13:07:51 -0500

    Bumped version to 51.7.0

--- gradle-plugin/src/main/groovy/mozilla/telemetry/glean-gradle-plugin/GleanGradlePlugin.groovy
+++ gradle-plugin/src/main/groovy/mozilla/telemetry/glean-gradle-plugin/GleanGradlePlugin.groovy
@@ -555,7 +555,7 @@ except:
     void apply(Project project) {
         isOffline = project.gradle.startParameter.offline
 
-        project.ext.glean_version = "51.6.0"
+        project.ext.glean_version = "51.7.0"
         def parserVersion = gleanParserVersion(project)
 
         // Print the required glean_parser version to the console. This is
