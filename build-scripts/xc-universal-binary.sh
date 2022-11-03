8e03275983de6e6fd7079cc26298d9ca350cb90e
Jan-Erik Rediger <jrediger@mozilla.com>
2022-11-03 18:38:20 +0100

    iOS: Use the right `SDK_DIR`

--- build-scripts/xc-universal-binary.sh
+++ build-scripts/xc-universal-binary.sh
@@ -23,12 +23,12 @@ fi
 
 set -euvx
 
-if [[ -n "${DEVELOPER_SDK_DIR:-}" ]]; then
+if [[ -n "${SDK_DIR:-}" ]]; then
   # Assume we're in Xcode, which means we're probably cross-compiling.
   # In this case, we need to add an extra library search path for build scripts and proc-macros,
   # which run on the host instead of the target.
   # (macOS Big Sur does not have linkable libraries in /usr/lib/.)
-  export LIBRARY_PATH="${DEVELOPER_SDK_DIR}/MacOSX.sdk/usr/lib:${LIBRARY_PATH:-}"
+  export LIBRARY_PATH="${SDK_DIR}/usr/lib:${LIBRARY_PATH:-}"
 fi
 
 IS_SIMULATOR=0
