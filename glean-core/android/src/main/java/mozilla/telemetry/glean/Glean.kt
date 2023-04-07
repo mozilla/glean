7a4f55bf94b0cd63de4e09118e52762f3bd34509
Travis Long <tlong@mozilla.com>
2023-04-07 09:15:13 -0500

    Addressing change requests, adding clarifying comments

--- glean-core/android/src/main/java/mozilla/telemetry/glean/Glean.kt
+++ glean-core/android/src/main/java/mozilla/telemetry/glean/Glean.kt
@@ -447,6 +447,11 @@ open class GleanInternalAPI internal constructor() {
      */
     fun setMetricsDisabledConfig(json: String) {
         // Let's convert this to the new API for backwards compatibility
+        // In order to convert to the new API we need to flip all of the boolean values that
+        // are contained in the map contained in the supplied JSON string. We do this by
+        // parsing the string and then iterating through the keys to create a new object with
+        // the boolean values inverted. Finally, we turn this back into a string to pass into
+        // the `setMetricsEnabledConfig` function.
         val jsonData = JSONObject(json)
         var invertedJsonData = JSONObject()
         jsonData.keys().forEach {
