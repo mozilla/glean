8371ddc01812b61aa7cf3c3740f37d57599f2970
Travis Long <tlong@mozilla.com>
2024-03-15 12:48:08 -0500

    Bug 1877830 - Keep iOS supported platform version up to date

--- bin/publish-glean-swift.sh
+++ bin/publish-glean-swift.sh
@@ -9,8 +9,16 @@ set -xe
 # Strip any `v` prefix from `vX.Y.Z`
 NEW_VERSION=$(echo "$1" | sed 's/^v//')
 
+# Get the supported iOS platform version
+# See https://developer.apple.com/documentation/packagedescription/supportedplatform/iosversion for valid iOS versions
+WORKSPACE_ROOT="$( cd "$(dirname "$0")/.." ; pwd -P )"
+FILE=glean-core/ios/Glean.xcodeproj/project.pbxproj
+IOS_PLATFORM_VERSION=v$(grep -m 1 \
+    -Po "(?<=IPHONEOS_DEPLOYMENT_TARGET \= )[0-9]." \
+    "${WORKSPACE_ROOT}/${FILE}")
+
 git clone https://github.com/mozilla/glean-swift glean-swift
 cd glean-swift
-bin/update.sh "$NEW_VERSION"
+bin/update.sh "$NEW_VERSION" --ios-version "$IOS_PLATFORM_VERSION"
 git push -q https://${GLEAN_SWIFT_GITHUB_TOKEN}@github.com/mozilla/glean-swift.git main
 git push -q https://${GLEAN_SWIFT_GITHUB_TOKEN}@github.com/mozilla/glean-swift.git "$NEW_VERSION"
