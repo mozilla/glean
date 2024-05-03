190918160248acfbb1cb0eca2a2d2f85ace32221
Jan-Erik Rediger <jrediger@mozilla.com>
2024-05-03 16:11:36 +0200

    Docker container to run a Python Windows build equivalent to CI

---
+++ tools/docker-winbuild/init.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+git clone --depth=1 https://github.com/mozilla/glean ~/project
+
+PATH=$HOME/.cargo/bin:$PATH \
+  rustc tools/patches/bcryptprimitives.rs -Copt-level=3 -Clto=fat --out-dir project/wine_shims --target x86_64-pc-windows-gnu
+
+exec bash -l
