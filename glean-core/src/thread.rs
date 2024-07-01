3368b98ab3b89d6892720031fc31c799c44e6a04
Jan-Erik Rediger <jrediger@mozilla.com>
2024-07-01 16:32:03 +0200

    Register threads with the Gecko profiler (feature-gated)
    Restricted to non-Android and behind a feature flag to enable it only
    for FOG.
    On Android we are part of libxul, but potentially run before libxul has
    run its early setup.

--- glean-core/src/thread.rs
+++ glean-core/src/thread.rs
@@ -1,6 +1,37 @@
 use std::io;
 use std::thread::{self, JoinHandle};
 
+#[cfg(all(feature = "gecko", not(target_os = "android")))]
+mod bindings {
+    extern "C" {
+        pub fn gecko_profiler_register_thread(name: *const std::ffi::c_char);
+        pub fn gecko_profiler_unregister_thread();
+    }
+}
+
+/// Register a thread with the Gecko Profiler.
+#[cfg(all(feature = "gecko", not(target_os = "android")))]
+fn register_thread(thread_name: &str) {
+    let name = std::ffi::CString::new(thread_name).unwrap();
+    unsafe {
+        // gecko_profiler_register_thread copies the passed name here.
+        bindings::gecko_profiler_register_thread(name.as_ptr());
+    }
+}
+
+#[cfg(any(not(feature = "gecko"), target_os = "android"))]
+fn register_thread(_thread_name: &str) {}
+
+/// Unregister a thread with the Gecko Profiler.
+#[cfg(all(feature = "gecko", not(target_os = "android")))]
+fn unregister_thread() {
+    unsafe {
+        bindings::gecko_profiler_unregister_thread();
+    }
+}
+#[cfg(any(not(feature = "gecko"), target_os = "android"))]
+fn unregister_thread() {}
+
 /// Spawns a new thread, returning a [`JoinHandle`] for it.
 ///
 /// Wrapper around [`std::thread::spawn`], but automatically naming the thread.
@@ -9,5 +40,12 @@ where
     F: FnOnce() -> T + Send + 'static,
     T: Send + 'static,
 {
-    thread::Builder::new().name(name.to_string()).spawn(f)
+    thread::Builder::new()
+        .name(name.to_string())
+        .spawn(move || {
+            register_thread(name);
+            let res = f();
+            unregister_thread();
+            res
+        })
 }
