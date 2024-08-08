3bad927a0ec7fde6640f3eae37045c1d48096715
Jan-Erik Rediger <jrediger@mozilla.com>
2024-08-08 12:36:51 +0200

    Kotlin: Add a (blocked) task queue and unblock it after initializing
    Note that while Glean is initializing (in glean-core on a separate thread),
    we run all tasks from the early queue.
    These then just add to the queue from Glean.
    
    Now, in theory, this could slightly change the order in which certain
    metric recordings are executed. I think. I can't come up with any sort
    of proof or test or so on whether that's problematic.

--- glean-core/android/src/main/java/mozilla/telemetry/glean/Dispatchers.kt
+++ glean-core/android/src/main/java/mozilla/telemetry/glean/Dispatchers.kt
@@ -11,6 +11,8 @@ import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.newSingleThreadContext
 import kotlinx.coroutines.runBlocking
+import java.util.concurrent.ConcurrentLinkedQueue
+import java.util.concurrent.atomic.AtomicBoolean
 
 internal object Dispatchers {
     class WaitableCoroutineScope(private val coroutineScope: CoroutineScope) {
@@ -44,6 +46,68 @@ internal object Dispatchers {
         }
     }
 
+    class DelayedTaskQueue {
+        // When true, jobs will be queued and not ran until triggered by calling
+        // flushQueuedInitialTasks()
+        private var queueInitialTasks = AtomicBoolean(true)
+
+        // Use a [ConcurrentLinkedQueue] to take advantage of its thread safety and no locking
+        internal val taskQueue: ConcurrentLinkedQueue<() -> Unit> = ConcurrentLinkedQueue()
+
+        /**
+         * Launch a block of work synchronously or queue it if not yet unblocked.
+         *
+         * If [queueInitialTasks] true
+         * then the work will be queued and executed when [flushQueuedInitialTasks] is called.
+         * If [queueInitialTasks] is false the block is executed synchronously.
+         */
+        fun launch(
+            block: () -> Unit,
+        ) {
+            if (queueInitialTasks.get()) {
+                addTaskToQueue(block)
+            } else {
+                block()
+            }
+        }
+
+        /**
+         * Stops queueing tasks and processes any tasks in the queue.
+         *
+         * Processing happens on the current thread synchronously.
+         *
+         * Since [queueInitialTasks] is set to false prior to processing the queue,
+         * newly launched tasks should be executed immediately rather than added to the queue.
+         */
+        internal fun flushQueuedInitialTasks() {
+            val dispatcherObject = this
+
+            queueInitialTasks.set(false)
+
+            // Nothing should be added to this list. However, the flush could get called
+            // while a `addTaskToQueue` is being executed. For this reason, we need synchronized
+            // access to the queue. However, we can't simply wrap the task execution in a sync
+            // block: suspending functions are not allowed inside critical sections.
+            val queueCopy: ConcurrentLinkedQueue<() -> Unit> = ConcurrentLinkedQueue()
+            synchronized(dispatcherObject) {
+                queueCopy.addAll(taskQueue)
+                taskQueue.clear()
+            }
+
+            queueCopy.forEach { task ->
+                task()
+            }
+        }
+
+        /**
+         * Helper function to add task to queue.
+         */
+        @Synchronized
+        private fun addTaskToQueue(block: () -> Unit) {
+            taskQueue.add(block)
+        }
+    }
+
     // This job is used to make sure the API `CoroutineContext` does not cancel
     // children jobs when exceptions are thrown in children coroutines.
     private val supervisorJob = SupervisorJob()
@@ -58,4 +122,7 @@ internal object Dispatchers {
             newSingleThreadContext("GleanAPIPool") + supervisorJob,
         ),
     )
+
+    @OptIn(kotlinx.coroutines.DelicateCoroutinesApi::class, kotlinx.coroutines.ExperimentalCoroutinesApi::class)
+    var Delayed = DelayedTaskQueue()
 }
