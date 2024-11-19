872828abffd39a5954e72dac9e3ff30881bbd3c2
Jan-Erik Rediger <jrediger@mozilla.com>
2024-11-19 16:50:02 +0100

    Python: Replace deprecated pkg_resources usage with importlib_resources

--- glean-core/python/glean/_builtins.py
+++ glean-core/python/glean/_builtins.py
@@ -7,16 +7,26 @@
 This module loads the built-in metrics and pings.
 """
 
-from pkg_resources import resource_filename
+import sys
 
+if sys.version_info >= (3, 9):
+    import importlib.resources as importlib_resources
+else:
+    import importlib_resources
 
 from ._loader import load_metrics, load_pings
 
+# Python <3.12 makes this something like `glean._builtin`,
+# above that it's just `glean`.
+pkg_name = __name__.split(".")[0]
 
-metrics = load_metrics(resource_filename(__name__, "metrics.yaml"), config={"allow_reserved": True})
+ref = importlib_resources.files(pkg_name) / "metrics.yaml"
+with importlib_resources.as_file(ref) as path:
+    metrics = load_metrics(path, config={"allow_reserved": True})
 
-
-pings = load_pings(resource_filename(__name__, "pings.yaml"), config={"allow_reserved": True})
+ref = importlib_resources.files(pkg_name) / "pings.yaml"
+with importlib_resources.as_file(ref) as path:
+    pings = load_pings(path, config={"allow_reserved": True})
 
 
 __all__ = ["metrics", "pings"]
