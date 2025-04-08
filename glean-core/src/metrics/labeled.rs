1a62321a3f94febb86cdba18303636f1da1e3e83
Jan-Erik Rediger <jrediger@mozilla.com>
2025-04-08 11:49:32 +0200

    Correct length limits for metric categories, names and labels everywhere
    [doc only]

--- glean-core/src/metrics/labeled.rs
+++ glean-core/src/metrics/labeled.rs
@@ -265,7 +265,7 @@ where
     /// only the first 16 unique labels will be used.
     /// After that, any additional labels will be recorded under the special `OTHER_LABEL` label.
     ///
-    /// Labels must be `snake_case` and less than 30 characters.
+    /// Labels must have a maximum of 71 characters, and may comprise any printable ASCII characters.
     /// If an invalid label is used, the metric will be recorded in the special `OTHER_LABEL` label.
     pub fn get<S: AsRef<str>>(&self, label: S) -> Arc<T> {
         let label = label.as_ref();
