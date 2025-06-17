cac13aeff3a896008ea60ee66d46230fc731db5b
CircleCI docs-deploy job <jrediger@mozilla.com>
2025-06-17 14:15:21 +0000

    [skip ci] Updates

---
+++ docs/trait.impl/core/ops/deref/trait.Deref.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["glean_core",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.82.0/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"enum\" href=\"glean_core/metrics/enum.DynamicLabelType.html\" title=\"enum glean_core::metrics::DynamicLabelType\">DynamicLabelType</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[319]}
