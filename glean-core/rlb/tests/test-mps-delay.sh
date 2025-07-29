ffb07d294417274cbd8bf896482252efc290ea47
Jan-Erik Rediger <jrediger@mozilla.com>
2025-07-29 11:32:43 +0200

    (backported) MPS: Properly calculate rescheduled duration using local time and avoid re-scheduling anything immediately
    This code changed with the recent chrono update and can cause rapid
    rescheduling of metric pings due to wrong date and time math, that mixes
    local, naive and UTC times.
    
    We now do the time math _always_ in the local timezone, ensuring to aim
    for a 4am submission of the metrics ping (in case it gets rescheduled).
    We also avoid any 0-duration reschedule (immediate). If somehow our math
    doesn't work out we schedule the metrics ping 24 hours into the future.

---
+++ glean-core/rlb/tests/test-mps-delay.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Test harness for testing the RLB processes from the outside.
+#
+# Some behavior can only be observed when properly exiting the process running Glean,
+# e.g. when an uploader runs in another thread.
+# On exit the threads will be killed, regardless of their state.
+
+# Remove the temporary data path on all exit conditions
+cleanup() {
+  if [ -n "$datapath" ]; then
+    rm -r "$datapath"
+  fi
+}
+trap cleanup INT ABRT TERM EXIT
+
+WORKSPACE_ROOT="$( cd "$(dirname "$0")/../../.." ; pwd -P )"
+cd "$WORKSPACE_ROOT"
+
+tmp="${TMPDIR:-/tmp}"
+datapath=$(mktemp -d "${tmp}/glean_mps_delay.XXXX")
+
+# Build it once
+cargo build -p glean --example mps-delay || exit 1
+
+cmd="target/debug/examples/mps-delay $datapath"
+
+# Set a timezone (Los Angeles = -07:00)
+export TZ=America/Los_Angeles
+export RUST_LOG=debug
+
+timeout 5s faketime --exclude-monotonic -f "2025-07-27 04:05:00" $cmd init
+count=$(ls -1q "$datapath/sent_pings" | wc -l)
+if [[ "$count" -ne 0 ]]; then
+  echo "1/3 test result: FAILED. Expected 0, got $count pings"
+  exit 101
+fi
+
+timeout 5s faketime --exclude-monotonic -f "2025-07-28 22:27:00" $cmd second
+count=$(ls -1q "$datapath/sent_pings" | wc -l)
+if [[ "$count" -ne 1 ]]; then
+  echo "2/3 test result: FAILED. Expected 1, got $count pings"
+  exit 101
+fi
+
+timeout 5s faketime --exclude-monotonic -f "2025-07-28 22:30:00" $cmd third
+count=$(ls -1q "$datapath/sent_pings" | wc -l)
+if [[ "$count" -ne 1 ]]; then
+  echo "3/3 test result: FAILED. Expected 1, got $count pings"
+  exit 101
+fi
+
+echo "test result: PASSED."
+exit 0
