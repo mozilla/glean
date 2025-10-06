57eaeffde56b778bc5019f9ade24181088d47bb1
CircleCI docs-deploy job <jrediger@mozilla.com>
2025-10-06 13:40:38 +0000

    [skip ci] Updates

--- docs/trait.impl/glean_core/metrics/trait.TestGetValue.js
+++ docs/trait.impl/glean_core/metrics/trait.TestGetValue.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["glean",[["impl&lt;K&gt; <a class=\"trait\" href=\"glean/trait.TestGetValue.html\" title=\"trait glean::TestGetValue\">TestGetValue</a>&lt;<a class=\"enum\" href=\"https://docs.rs/serde_json/1.0.138/serde_json/value/enum.Value.html\" title=\"enum serde_json::value::Value\">Value</a>&gt; for <a class=\"struct\" href=\"glean/private/struct.ObjectMetric.html\" title=\"struct glean::private::ObjectMetric\">ObjectMetric</a>&lt;K&gt;"],["impl&lt;K&gt; <a class=\"trait\" href=\"glean/trait.TestGetValue.html\" title=\"trait glean::TestGetValue\">TestGetValue</a>&lt;<a class=\"struct\" href=\"https://doc.rust-lang.org/1.82.0/alloc/vec/struct.Vec.html\" title=\"struct alloc::vec::Vec\">Vec</a>&lt;<a class=\"struct\" href=\"glean/struct.RecordedEvent.html\" title=\"struct glean::RecordedEvent\">RecordedEvent</a>&gt;&gt; for <a class=\"struct\" href=\"glean/private/struct.EventMetric.html\" title=\"struct glean::private::EventMetric\">EventMetric</a>&lt;K&gt;"]]],["glean_core",[]]]);
+    var implementors = Object.fromEntries([["glean",[["impl&lt;K&gt; <a class=\"trait\" href=\"glean/trait.TestGetValue.html\" title=\"trait glean::TestGetValue\">TestGetValue</a> for <a class=\"struct\" href=\"glean/private/struct.EventMetric.html\" title=\"struct glean::private::EventMetric\">EventMetric</a>&lt;K&gt;"],["impl&lt;K&gt; <a class=\"trait\" href=\"glean/trait.TestGetValue.html\" title=\"trait glean::TestGetValue\">TestGetValue</a> for <a class=\"struct\" href=\"glean/private/struct.ObjectMetric.html\" title=\"struct glean::private::ObjectMetric\">ObjectMetric</a>&lt;K&gt;"]]],["glean_core",[]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[966,18]}
+//{"start":57,"fragment_lengths":[554,18]}
