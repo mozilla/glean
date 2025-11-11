4c03f572dd5a4b1e0fc421bc242e4afeba4d4a81
Jan-Erik Rediger <jrediger@mozilla.com>
2025-11-11 12:52:04 +0100

    A first benchmark: Putting things on the dispatcher
    This sets up criterion, a statistics-driven micro-benchmarking tool.
    It also adds a first simple benchmark: Launching tasks on the global
    dispatcher.
    
    Also included is a helper script to run a benchmark comparison.

---
+++ bin/benchmark-compare.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+# Run a benchmark comparison between a previous commit and the current one.
+
+set -eo pipefail
+
+run() {
+    [ "${VERB:-0}" != 0 ] && echo "+ $*"
+    "$@"
+}
+
+WORKSPACE_ROOT="$( cd "$(dirname "$0")/.." ; pwd -P )"
+
+if [ -z "$1" ]; then
+    echo "Usage: $(basename "$0") <start commit> [end commit]"
+    echo
+    echo "Run a benchmark on both the <start commit> and [end commit] for comparison."
+    echo "[end commit] defaults to HEAD"
+    exit 1
+fi
+
+START_COMMIT="$1"
+START_COMMIT=$(git rev-parse "$START_COMMIT")
+END_COMMIT="${2:-HEAD}"
+END_COMMIT=$(git rev-parse "$END_COMMIT")
+
+BASELINE_NAME="bn-${END_COMMIT}"
+
+stash_commit=$(git stash create)
+
+run git checkout "$START_COMMIT"
+run cargo bench -p glean-core --features benchmark -- --save-baseline "$BASELINE_NAME"
+
+run git checkout "$END_COMMIT"
+run cargo bench -p glean-core --features benchmark -- --baseline "$BASELINE_NAME"
+
+if [[ -n "$stash_commit" ]]; then
+  git stash apply "$stash_commit"
+fi
+
+run echo "You can rerun the benchmark against the baseline using:"
+run echo "cargo bench -p glean-core -- --baseline $BASELINE_NAME"
