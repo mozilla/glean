d078034f2d877d662f2197cd2588d7dbe6f8a903
Jan-Erik Rediger <jrediger@mozilla.com>
2025-12-15 12:03:00 +0100

    Update to `glean_parser` v18.2.0

--- glean-core/python/glean/__init__.py
+++ glean-core/python/glean/__init__.py
@@ -30,7 +30,7 @@ __author__ = "The Glean Team"
 __email__ = "glean-team@mozilla.com"
 
 
-GLEAN_PARSER_VERSION = "18.0.6"
+GLEAN_PARSER_VERSION = "18.2.0"
 parser_version = VersionInfo.parse(GLEAN_PARSER_VERSION)
 parser_version_next_major = parser_version.bump_major()
 
