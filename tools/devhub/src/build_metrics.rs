3a274be81370b2e4061388b626e4c8f88fe9dc25
Jan-Erik Rediger <jrediger@mozilla.com>
2026-01-08 14:39:48 +0100

    Add the `devhub` command: Gather metrics and upload them to a devhub database

---
+++ tools/devhub/src/build_metrics.rs
@@ -0,0 +1,72 @@
+use std::collections::HashMap;
+
+use serde::Deserialize;
+use xshell::{Shell, cmd};
+
+use super::{Metric, MetricRecorder, Result};
+
+type Tokei = HashMap<String, TokeiStats>;
+
+#[derive(Deserialize)]
+struct TokeiStats {
+    code: u64,
+}
+
+/// Count the lines of code.
+pub struct Codesize;
+
+impl MetricRecorder for Codesize {
+    fn name(&self) -> &'static str {
+        "loc"
+    }
+
+    fn description(&self) -> &'static str {
+        "Lines of Code (by language)"
+    }
+
+    fn record(&self, sh: &Shell) -> Result<Vec<Metric>> {
+        let tokei_out = cmd!(
+            sh,
+            "tokei -o json -e glean-core/android/build -t rust,kotlin,swift,python glean-core"
+        )
+        .read()?;
+        let tokei_json: Tokei = serde_json::from_str(&tokei_out)?;
+
+        let mut metrics = Vec::new();
+        for (lang, stats) in tokei_json {
+            let metric = Metric {
+                name: format!("Lines of code - {lang}"),
+                unit: String::from(""),
+                value: stats.code,
+            };
+            metrics.push(metric);
+        }
+
+        Ok(metrics)
+    }
+}
+
+/// Count the number of built-in metrics
+pub struct MetricCount;
+
+impl MetricRecorder for MetricCount {
+    fn name(&self) -> &'static str {
+        "metrics"
+    }
+
+    fn description(&self) -> &'static str {
+        "Number of built-in metrics"
+    }
+
+    fn record(&self, sh: &Shell) -> Result<Vec<Metric>> {
+        let metric_count = cmd!(sh, "rg -c '^  [a-z]' glean-core/metrics.yaml")
+            .read()?
+            .parse()?;
+        let metric = Metric {
+            name: String::from("Number of built-in metrics"),
+            unit: String::from(""),
+            value: metric_count,
+        };
+        Ok(vec![metric])
+    }
+}
---
+++ tools/devhub/src/deps.rs
@@ -0,0 +1,39 @@
+use std::collections::HashSet;
+
+use xshell::{Shell, cmd};
+
+use super::{Metric, MetricRecorder, Result};
+
+/// Number of dependencies.
+pub struct Dependencies(String);
+
+impl Dependencies {
+    pub fn new<S: Into<String>>(krate: S) -> Dependencies {
+        Dependencies(krate.into())
+    }
+}
+
+impl MetricRecorder for Dependencies {
+    fn name(&self) -> &'static str {
+        "deps"
+    }
+
+    fn description(&self) -> &'static str {
+        "Number of dependencies"
+    }
+
+    fn record(&self, sh: &Shell) -> Result<Vec<Metric>> {
+        let mut metrics = Vec::new();
+
+        let krate = &self.0;
+        let dep_count = cmd!(sh, "cargo tree --locked --offline -p {krate} --edges no-dev --charset ascii --prefix none --format '{p}'").read()?;
+        let dep_count = dep_count.lines().collect::<HashSet<_>>().len() as u64;
+        metrics.push(Metric {
+            name: format!("Number of dependencies for {}", self.0),
+            unit: String::from(""),
+            value: dep_count,
+        });
+
+        Ok(metrics)
+    }
+}
