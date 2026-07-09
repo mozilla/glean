1824e55f577edc695dc84818901be8efc4fd0e61
Jan-Erik Rediger <jrediger@mozilla.com>
2026-07-09 12:57:05 +0200

    Wire mozbuild through to link against sqlite

--- glean-core/build.rs
+++ glean-core/build.rs
@@ -1,3 +1,7 @@
 fn main() {
     uniffi::generate_scaffolding("./src/glean.udl").unwrap();
+    #[cfg(feature = "mozbuild-rustlib")]
+    if std::env::var_os("MOZ_TOPOBJDIR").is_some() {
+        mozbuild::link_sqlite();
+    }
 }
