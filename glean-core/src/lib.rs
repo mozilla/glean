a66526972c94b70742aa54c61210f7b67566d20f
Jan-Erik Rediger <jrediger@mozilla.com>
2026-08-11 13:36:40 +0200

    Close the database on shutdown

--- glean-core/src/lib.rs
+++ glean-core/src/lib.rs
@@ -787,7 +787,7 @@ pub fn shutdown() {
     uploader_shutdown();
 
     // Be sure to call this _after_ draining the dispatcher
-    core::with_glean(|glean| {
+    core::with_glean_mut(|glean| {
         if let Err(e) = glean.persist_ping_lifetime_data() {
             log::info!("Can't persist ping lifetime data: {:?}", e);
         }
@@ -797,6 +797,8 @@ pub fn shutdown() {
                 log::info!("Can't run database maintenance on shutdown: {:?}", e);
             }
         }
+
+        glean.close_db();
     });
 }
 
@@ -1344,7 +1346,7 @@ pub fn glean_test_destroy_glean(clear_stores: bool, data_path: Option<String>) {
                 if clear_stores {
                     glean.test_clear_all_stores()
                 }
-                glean.destroy_db()
+                glean.close_db()
             });
         }
 
