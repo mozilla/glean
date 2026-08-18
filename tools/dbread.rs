06dc1b19fc4ef6a621a5b80fcdee364dbf9a7ccc
Jan-Erik Rediger <jrediger@mozilla.com>
2026-08-18 10:21:54 +0200

    Update dbread tool to parse SQLite databases
    [doc only]

--- tools/dbread.rs
+++ tools/dbread.rs
@@ -1,101 +1,83 @@
 #!/usr/bin/env -S cargo +nightly -q -Zscript
 ---cargo
+package.edition = "2024"
 [dependencies]
-bincode = "1.0"
-serde = {version = "1.0.144", features = ["derive"]}
-bitflags = {version = "2.4.1", features = ["serde"]}
+rmp-serde = "1.3.1"
+rusqlite = { version = "0.37.0", features = ["bundled"] }
+serde_json = "1.0.44"
 tabwriter = "1.1.0"
+glean-core = { path = "../glean-core" }
 ---
 
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-//! A tiny tool to read Rkv-based (safe-mode) databases as produced by Glean.
+//! A tiny tool to read SQLite-based databases as produced by Glean.
 //!
-//! Displays the metrics in every store, more complex metric types (e.g. distributions)
-//! are not displayed in full.
+//! Displays the metrics for every lifetime.
 
 use std::env;
 use std::io::{self, Write};
-use std::collections::{HashMap, BTreeMap};
 
-use bitflags::bitflags;
-use serde::Deserialize;
+use glean_core::metrics::Metric;
+use rusqlite::{Connection, OpenFlags, named_params};
+use serde_json::json;
 use tabwriter::TabWriter;
 
-bitflags! {
-    #[derive(Default, Deserialize, PartialEq, Eq, Debug, Clone, Copy)]
-    pub struct DatabaseFlagsImpl: u32 {
-        const NIL = 0b0000_0000;
-    }
-}
-
-type Key = Box<[u8]>;
-type Value = Box<[u8]>;
-
-#[derive(Debug, Clone, Deserialize)]
-pub struct Snapshot {
-    _flags: DatabaseFlagsImpl,
-    map: BTreeMap<Key, Value>,
-}
-
-#[derive(Debug, Deserialize)]
-pub struct Database {
-    snapshot: Snapshot,
-}
-
-/// Reduced duplicate of `enum Metric` in `glean-core/src/metrics/mod.rs`
-#[derive(Deserialize, Clone, Debug, PartialEq)]
-pub enum Metric {
-    Boolean(bool),
-    Counter(i32),
-    CustomDistributionExponential,
-    CustomDistributionLinear,
-    Datetime,
-    Experiment,
-    Quantity(i64),
-    String(String),
-    StringList(Vec<String>),
-    Uuid(String),
-    Timespan,
-    TimingDistribution,
-    MemoryDistribution,
-    Jwe(String),
-    Rate(i32, i32),
-    Url(String),
-    Text(String),
-    Object(String),
+struct MetricRow {
+    id: String,
+    ping: String,
+    labels: String,
+    value: Metric,
 }
 
 fn main() {
     let mut args = env::args().skip(1);
-    let path = args.next().expect("/path/to/db");
-    let content = std::fs::read(path).unwrap();
+    let path = args.next().expect("REQUIRED: /path/to/db");
+    let flags = OpenFlags::SQLITE_OPEN_NO_MUTEX
+        | OpenFlags::SQLITE_OPEN_EXRESCODE
+        | OpenFlags::SQLITE_OPEN_READ_ONLY;
 
-    let data: HashMap<Option<String>, Database> = bincode::deserialize(&content).unwrap();
-    let mut data: Vec<_> = data.into_iter().collect();
-    data.sort_by(|a, b| a.0.cmp(&b.0));
+    let conn = Connection::open_with_flags(path, flags).unwrap();
+
     let stdout = io::stdout();
 
-    for (name, db) in data {
+    let mut stmt = conn.prepare("SELECT id, ping, labels, value FROM telemetry WHERE lifetime = :lifetime ORDER BY ping ASC, id ASC").unwrap();
+    for lifetime in &["user", "app", "ping"] {
+        let metric_iter = stmt
+            .query_map(named_params! { ":lifetime": lifetime }, |row| {
+                let id = row.get(0)?;
+                let ping = row.get(1)?;
+                let labels = row.get(2)?;
+                let value: Vec<u8> = row.get(3)?;
+                Ok(MetricRow {
+                    id,
+                    ping,
+                    labels,
+                    value: rmp_serde::from_slice(&value).unwrap(),
+                })
+            })
+            .unwrap();
+
         let handle = stdout.lock();
         let mut tw = TabWriter::new(handle);
-        let name = name.unwrap_or(String::from(""));
-        writeln!(&mut tw, "DB: {name}").unwrap();
-        writeln!(&mut tw, "Key\tValue").unwrap();
-        writeln!(&mut tw, "===\t=====").unwrap();
+        writeln!(&mut tw, "Lifetime: {lifetime}").unwrap();
+        writeln!(&mut tw, "Ping\tKey\tLabels\tValue").unwrap();
+        writeln!(&mut tw, "====\t===\t======\t=====").unwrap();
 
-        for (k, v) in db.snapshot.map {
-            let k = str::from_utf8(&k).unwrap();
-            if v[0] == 9 {
-                let d: Vec<u8> = bincode::deserialize(&v[1..]).unwrap();
-                let m: Metric = bincode::deserialize(&d).unwrap();
-                writeln!(&mut tw, "{k}\t{m:?}").ok();
-            } else {
-                writeln!(&mut tw, "{k}\t{:?}..", &v[..4]).ok();
-            }
+        for row in metric_iter {
+            let row = row.unwrap();
+            let value = json!({ row.value.ping_section(): row.value.as_json() });
+            let value = serde_json::to_string(&value).unwrap();
+            writeln!(
+                &mut tw,
+                "{}\t{}\t{}\t{}",
+                row.ping, row.id, row.labels, value
+            )
+            .ok();
         }
+
         writeln!(&mut tw, "").ok();
         tw.flush().unwrap();
     }
