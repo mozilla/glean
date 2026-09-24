9fe8c92c7643f094fb549298302dcc2e587dd082
Jan-Erik Rediger <jrediger@mozilla.com>
2026-09-24 15:08:05 +0200

    glean-sym: Ensure UniFFI symbols are always included
    In no-op mode we stub out the library loading, but we do generate the
    public API, which itself uses UniFFI tagging, so we should keep the
    constants around for UniFFI to parse out.

--- glean-core/glean-sym/src/lib.rs
+++ glean-core/glean-sym/src/lib.rs
@@ -129,14 +129,12 @@ pub struct UniFfiTag;
 /// The namespace glean-sym FFI items are placed in.
 /// Required by UniFFI to match up items.
 #[allow(clippy::large_const_arrays)] // this is moved into the `static` below, we can't make it a static itself.
-#[cfg(feature = "active")]
 const UNIFFI_META_CONST_NAMESPACE_GLEAN_SYM: ::uniffi::MetadataBuffer =
     ::uniffi::MetadataBuffer::from_code(::uniffi::metadata::codes::NAMESPACE)
         .concat_str("glean_sym")
         .concat_str("glean_sym");
 #[doc(hidden)]
 #[unsafe(no_mangle)]
-#[cfg(feature = "active")]
 pub static UNIFFI_META_NAMESPACE_GLEAN_SYM: [::std::primitive::u8;
     UNIFFI_META_CONST_NAMESPACE_GLEAN_SYM.size] =
     UNIFFI_META_CONST_NAMESPACE_GLEAN_SYM.into_array();
